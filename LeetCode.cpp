7703a285820ccb6065e85133f5d76f002669f13a
Danil Pinchuk <512kb@proton.me>
2022-06-03 01:05:34 +0400

    4. Median of Two Sorted Arrays

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <vector>
+#include <algorithm>
 
 #include "LeetCode.h"
 
@@ -9,27 +10,27 @@ using namespace std;
 
 class Solution {
 public:
-	static vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-		if (matrix.empty())
-			return vector<vector<int>>();
+    double findMedianSortedArrays(vector<int>&& nums1, vector<int>&& nums2) {
+        vector<int> result;
+        result.reserve(nums1.size() + nums2.size());
 
-		vector<vector<int>> result;
-		int colsNumber = matrix.front().size();
-		result.reserve(colsNumber);
+        merge(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), back_inserter(result));
 
-		for (int colI = 0; colI < colsNumber; ++colI) {
-			result.push_back(vector<int>());
-			result.back().reserve(matrix.size());
-			for (const auto& row : matrix) {
-				result[colI].push_back(row[colI]);
-			}
-		}
+        double medianIndex = (result.size() + 1) / 2.;
+        double frac = modf(medianIndex, &medianIndex);
+        
+        --medianIndex;
+        frac = ceil(frac);
 
-		return result;
-	}
+        return !result.empty() ? (result[medianIndex] + result[medianIndex + frac]) / 2. : 0;
+    }
 };
 
 int main()
 {
+    Solution sol;
+    cout << sol.findMedianSortedArrays({ }, { }) << endl;
+    cout << sol.findMedianSortedArrays({ 1, 3 }, { 2 }) << endl;
+    cout << sol.findMedianSortedArrays({ 1, 2 }, { 3, 4 }) << endl;
 	return 0;
 }
