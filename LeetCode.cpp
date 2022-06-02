dd88528f0d7e92e4e3985a48b1f7f1b5c82d5112
Danil Pinchuk <512kb@proton.me>
2022-06-03 00:36:24 +0400

    867. Transpose Matrix

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -1,12 +1,35 @@
 ﻿// LeetCode.cpp : Defines the entry point for the application.
 //
 
+#include <vector>
+
 #include "LeetCode.h"
 
 using namespace std;
 
+class Solution {
+public:
+	static vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+		if (matrix.empty())
+			return vector<vector<int>>();
+
+		vector<vector<int>> result;
+		int colsNumber = matrix.front().size();
+		result.reserve(colsNumber);
+
+		for (int colI = 0; colI < colsNumber; ++colI) {
+			result.push_back(vector<int>());
+			result.back().reserve(matrix.size());
+			for (const auto& row : matrix) {
+				result[colI].push_back(row[colI]);
+			}
+		}
+
+		return result;
+	}
+};
+
 int main()
 {
-	cout << "Hello CMake." << endl;
 	return 0;
 }
