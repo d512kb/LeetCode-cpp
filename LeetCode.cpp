6e348e9b54bec79650d269a0cfd7c68e9f23eed6
Danil Pinchuk <512kb@proton.me>
2022-06-03 04:33:32 +0400

    4. Median of Two Sorted Arrays
    No algorithms usage

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -10,27 +10,46 @@ using namespace std;
 
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>&& nums1, vector<int>&& nums2) {
-        vector<int> result;
-        result.reserve(nums1.size() + nums2.size());
+	double findMedianSortedArrays(vector<int>&& nums1, vector<int>&& nums2) {
+		if (nums1.empty() && nums2.empty())
+			return 0;
+		
+		auto nums1Iter = nums1.cbegin();
+		auto nums2Iter = nums2.cbegin();
+		auto lastIter = nums1.empty() ? nums2.cbegin() : nums1.cbegin();
+		auto medianIndex = (nums1.size() + nums2.size()) / 2;
 
-        merge(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), back_inserter(result));
+		for (int i = 0; i < medianIndex; ++i) {
+			if (nums1Iter == nums1.cend()) {
+				lastIter = nums2Iter++;
+			} else if (nums2Iter == nums2.cend()) {
+				lastIter = nums1Iter++;
+			} else if (*nums1Iter <= *nums2Iter) {
+				lastIter = nums1Iter++;
+			} else {
+				lastIter = nums2Iter++;
+			}
+		}
 
-        double medianIndex = (result.size() + 1) / 2.;
-        double frac = modf(medianIndex, &medianIndex);
-        
-        --medianIndex;
-        frac = ceil(frac);
+		auto minMedian = min(nums1Iter == nums1.cend() ? *nums2Iter : *nums1Iter,
+							 nums2Iter == nums2.cend() ? *nums1Iter : *nums2Iter);
 
-        return !result.empty() ? (result[medianIndex] + result[medianIndex + frac]) / 2. : 0;
-    }
+		if ((nums1.size() + nums2.size()) % 2) { // odd number of elements, exact one median	
+			return minMedian;
+		} else { // find median by calculating mean of two numbers
+			return (*lastIter + minMedian) / 2.0;
+		}
+	}
 };
 
 int main()
 {
-    Solution sol;
-    cout << sol.findMedianSortedArrays({ }, { }) << endl;
-    cout << sol.findMedianSortedArrays({ 1, 3 }, { 2 }) << endl;
-    cout << sol.findMedianSortedArrays({ 1, 2 }, { 3, 4 }) << endl;
+	Solution sol;
+	cout << sol.findMedianSortedArrays({ 1, 3,4,5,6,7 }, { 2 }) << endl;
+	cout << sol.findMedianSortedArrays({ 1, 2, 5 }, { 3, 4 }) << endl;
+	cout << sol.findMedianSortedArrays({ 3 }, { 1, 2, 3, 4, 5 }) << endl;
+	cout << sol.findMedianSortedArrays({  }, { 3, 4, 5, 6 }) << endl;
+	cout << sol.findMedianSortedArrays({ 1, 2 }, { 3,4 }) << endl;
+	cout << sol.findMedianSortedArrays({ 3, 4 }, { 1, 2 }) << endl;
 	return 0;
 }
