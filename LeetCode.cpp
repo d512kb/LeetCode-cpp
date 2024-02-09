993710e8e3b9b03623cc603d5e0e063121a4d18c
Danil Pinchuk <512kb@proton.me>
2024-02-08 23:04:02 -0500

    274. H-Index

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -9,30 +9,23 @@ using namespace std;
 
 class Solution {
 public:
-    int jump(vector<int>& nums) {
-        int jumpCount = 0;
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
 
-        for (int i = 0; i < nums.size() - 1; ++i) {
-            if (i + nums[i] >= nums.size() - 1) {
-                return jumpCount + 1; // last jump
-            }
+        int targetHIndex = citations.size();
+        int hIndex = 0;
 
-            int maxJumpIndex = i + 1;
-            int maxJump = nums[maxJumpIndex] + 1; // count an extra step
-            int jumpMaxIndex = i + nums[i];
-
-            for (int j = maxJumpIndex, k = 1; j <= jumpMaxIndex; ++j, ++k) {
-                if (nums[j] + k >= maxJump) {
-                    maxJumpIndex = j;
-                    maxJump = nums[maxJumpIndex] + k;
+        for (int i = citations.size() - 1; i >= 0; --i, ++hIndex) {
+            if (citations[i] <= targetHIndex) {
+                if (hIndex < targetHIndex && hIndex < citations[i]) {
+                    targetHIndex = citations[i];
+                } else {
+                    return hIndex;
                 }
             }
-
-            i = maxJumpIndex - 1;
-            ++jumpCount;
         }
 
-        return jumpCount;
+        return hIndex;
     }
 };
 
@@ -40,9 +33,6 @@ int main() {
     Solution sol;
 
     INIT_TIME(timer);
-
-    vector<int> v{ 2,3,1,1,4 };
-    sol.jump(v);
 
     PRINT_ELAPSED(timer);
     return 0;
