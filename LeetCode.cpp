b529da5b26eaa9d13bcdffb871cdab2969c7c5b6
Danil Pinchuk <512kb@proton.me>
2024-02-09 02:38:10 -0500

    380. Insert Delete GetRandom O(1)

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -3,35 +3,59 @@
 
 #include "LeetCode.h"
 #include <map>
+#include <unordered_set>
 #include <stack>
 
 using namespace std;
 
-class Solution {
+class RandomizedSet {
 public:
-    int hIndex(vector<int>& citations) {
-        sort(citations.begin(), citations.end());
+    RandomizedSet() :
+        m_rndEngine(random_device()())
+    {
+    }
 
-        int targetHIndex = citations.size();
-        int hIndex = 0;
+    bool insert(int val) {
+        auto result = m_indexes.insert({ val, 0 });
 
-        for (int i = citations.size() - 1; i >= 0; --i, ++hIndex) {
-            if (citations[i] <= targetHIndex) {
-                if (hIndex < targetHIndex && hIndex < citations[i]) {
-                    targetHIndex = citations[i];
-                } else {
-                    return hIndex;
-                }
-            }
+        if (result.second) {
+            m_data.push_back(val);
+            m_indexes[val] = m_data.size() - 1;
+            m_intDistribution.param(decltype(m_intDistribution.param())(0, m_data.size() - 1));
         }
 
-        return hIndex;
+        return result.second;
     }
+
+    bool remove(int val) {
+        auto iter = m_indexes.find(val);
+
+        if (iter != m_indexes.end()) {
+            m_data[iter->second] = m_data.back();
+            m_indexes[m_data.back()] = iter->second;
+            m_data.pop_back();
+            m_indexes.erase(iter);
+            m_intDistribution.param(decltype(m_intDistribution.param())(0, m_data.size() - 1));
+
+            return true;
+        }
+
+        return false;
+    }
+
+    int getRandom()
+    {
+        return m_data[m_intDistribution(m_rndEngine)];
+    }
+
+private:
+    unordered_map<int, int> m_indexes;
+    vector<int> m_data;
+    default_random_engine m_rndEngine;
+    uniform_int_distribution<int> m_intDistribution;
 };
 
 int main() {
-    Solution sol;
-
     INIT_TIME(timer);
 
     PRINT_ELAPSED(timer);
