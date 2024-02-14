ad8413a56ef054bf28c529c66ec5f5f917f820b6
Danil Pinchuk <512kb@proton.me>
2024-02-14 04:19:41 -0500

    151. Reverse Words in a String

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -10,21 +10,36 @@ using namespace std;
 
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        auto firstStr = strs[0];
-        long prefixLen = firstStr.size();
+    string reverseWords(string s) {
+        string result(s.size()+1, ' '); // extra space to allow iterator increment
 
-        for (int i = 1; i < strs.size(); ++i) {
-            auto mis = mismatch(firstStr.begin(), firstStr.begin()+prefixLen, strs[i].begin());
-            prefixLen = min(prefixLen, static_cast<long>(distance(firstStr.begin(), mis.first)));
+        auto outIter = result.begin();
+        auto firstChar = s.rbegin();
+        auto firstSpace = s.rbegin();
+        
+        while (true) {
+            firstChar = find_if_not(firstSpace, s.rend(), ::isspace);
+            firstSpace = find(firstChar, s.rend(), ' ');
+
+            if (firstChar == s.rend())
+                break;
+
+            outIter = reverse_copy(firstChar, firstSpace, outIter);
+            ++outIter;
         }
 
-        return firstStr.substr(0, prefixLen);
+        result.resize(distance(result.begin(), outIter)-1);
+        
+        return result;
     }
 };
 
 int main() {
     INIT_TIME(timer);
+
+    Solution sol;
+
+    cout << sol.reverseWords("the sky is blue");
 
     PRINT_ELAPSED(timer);
     return 0;
