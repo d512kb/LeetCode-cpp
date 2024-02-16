5aba88d4210391c13ed91d3d6463cea5f052bad2
Danil Pinchuk <512kb@proton.me>
2024-02-16 07:30:20 -0500

    167. Two Sum II - Input Array Is Sorted (lower/upper bounds)

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -10,16 +10,25 @@ using namespace std;
 
 class Solution {
 public:
-    bool isSubsequence(string s, string t) {
-        int sIndex = 0;
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        auto b = upper_bound(numbers.begin(), numbers.end(), target);
 
-        for (int i = 0; i < t.size() && sIndex < s.size(); ++i) {
-            if (t[i] == s[sIndex]) {
-                ++sIndex;
-            }
+        if (b == numbers.end())
+            b = numbers.end() - 1;
+        else if (b == numbers.begin())
+            ++b;
+
+        auto a = lower_bound(numbers.begin(), numbers.end(), target - *b);
+
+        while (*a + *b != target) {
+            --b;
+            a = lower_bound(numbers.begin(), numbers.end(), target - *b);
         }
 
-        return sIndex == s.size();
+        int aVal = static_cast<int>(distance(numbers.begin(), a) + 1);
+        int bVal = static_cast<int>(distance(numbers.begin(), b) + 1);
+
+        return { min(aVal, bVal), max(aVal, bVal) };
     }
 };
 
