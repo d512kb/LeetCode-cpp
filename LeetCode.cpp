ca7e88487694224e398f0709821f9679a62038f8
Danil Pinchuk <512kb@proton.me>
2024-02-16 07:50:40 -0500

    167. Two Sum II - Input Array Is Sorted (two pointers)

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -11,24 +11,23 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        auto b = upper_bound(numbers.begin(), numbers.end(), target);
+        int a = 0;
+        int b = numbers.size() - 1;
+        int sum = 0;
 
-        if (b == numbers.end())
-            b = numbers.end() - 1;
-        else if (b == numbers.begin())
-            ++b;
-
-        auto a = lower_bound(numbers.begin(), numbers.end(), target - *b);
-
-        while (*a + *b != target) {
-            --b;
-            a = lower_bound(numbers.begin(), numbers.end(), target - *b);
+        while (a < b) {
+            sum = numbers[a] + numbers[b];
+            
+            if (sum < target) {
+                ++a;
+            } else if (sum > target) {
+                --b;
+            } else {
+                break;
+            }
         }
 
-        int aVal = static_cast<int>(distance(numbers.begin(), a) + 1);
-        int bVal = static_cast<int>(distance(numbers.begin(), b) + 1);
-
-        return { min(aVal, bVal), max(aVal, bVal) };
+        return { a + 1, b + 1 };
     }
 };
 
