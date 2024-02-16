e210d2e40774b24ca842c6f69fc459626f3b0296
Danil Pinchuk <512kb@proton.me>
2024-02-16 03:22:28 -0500

    28. Find the Index of the First Occurrence in a String

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -10,36 +10,67 @@ using namespace std;
 
 class Solution {
 public:
-    string reverseWords(string s) {
-        string result(s.size()+1, ' '); // extra space to allow iterator increment
+    int strStr(string haystack, string needle) {
+        if (needle.size() > haystack.size())
+            return -1;
 
-        auto outIter = result.begin();
-        auto firstChar = s.rbegin();
-        auto firstSpace = s.rbegin();
-        
-        while (true) {
-            firstChar = find_if_not(firstSpace, s.rend(), ::isspace);
-            firstSpace = find(firstChar, s.rend(), ' ');
+        if (needle.size() <= 1) {
+            auto pos = find(haystack.begin(), haystack.end(), needle[0]);
 
-            if (firstChar == s.rend())
-                break;
-
-            outIter = reverse_copy(firstChar, firstSpace, outIter);
-            ++outIter;
+            return pos == haystack.end() ? -1 : distance(haystack.begin(), pos);
         }
 
-        result.resize(distance(result.begin(), outIter)-1);
-        
-        return result;
+        uint32_t k = needle.size();
+        uint64_t a = 3;
+        uint64_t an = 1;
+        uint64_t n = 101;
+        uint32_t haystackHash = haystack[k - 1] % n;
+        uint32_t needleHash = needle[k-1] % n;
+
+        for (int i = k - 2; i >= 0; --i) {
+            an *= a;
+            an %= n;
+            
+            haystackHash = (haystackHash + haystack[i] * an) % n;
+            needleHash = (needleHash + needle[i] * an) % n;
+        }
+
+        int i = k;
+
+        if (haystackHash == needleHash) {
+            if (mismatch(needle.begin(), needle.end(), haystack.begin() + i - k).first == needle.end()) {
+                return i - k;
+            }
+        }
+
+        while (i < haystack.size()) {
+            haystackHash = (haystackHash + n - (haystack[i - k] * an) % n) % n;
+            haystackHash = (haystackHash * a) % n;
+            haystackHash = (haystackHash + haystack[i]) % n;
+            
+            ++i;
+
+            if (haystackHash == needleHash) {
+                if (mismatch(needle.begin(), needle.end(), haystack.begin() + i - k).first == needle.end()) {
+                    return i-k;
+                }
+            }
+        }
+
+        return -1;
     }
 };
 
 int main() {
-    INIT_TIME(timer);
-
     Solution sol;
 
-    cout << sol.reverseWords("the sky is blue");
+    string hay(10000000, 'a');
+    hay.back() = 'b';
+    string needle("aaaab");
+    
+    INIT_TIME(timer);
+
+    cout << sol.strStr(hay, needle);
 
     PRINT_ELAPSED(timer);
     return 0;
