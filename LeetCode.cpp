9a57dfd72dc775d27f19484141a73fc32053964e
Danil Pinchuk <512kb@proton.me>
2024-02-18 05:27:45 -0500

    209. Minimum Size Subarray Sum

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -10,38 +10,21 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-
-        int a = 0;
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int a = 0, b = 0;
         int sum = 0;
-        vector<vector<int>> result;
+        int minLen = nums.size() + 1;
 
-        while (a < nums.size() - 2) {
-            int b = a + 1;
-            int c = nums.size() - 1;
+        while (b < nums.size()) {
+            sum += nums[b++];
 
-            while (b < c) {
-                sum = nums[a] + nums[b] + nums[c];
-
-                if (sum < 0) {
-                    ++b;
-                } else if (sum > 0) {
-                    --c;
-                } else {
-                    result.push_back({ nums[a], nums[b], nums[c] });
-                    ++b; --c;
-                    while (b < c && nums[b] == nums[b - 1]) ++b;
-                    while (b < c && nums[c] == nums[c + 1]) --c;
-                }
+            while (sum >= target) {
+                minLen = min(minLen, b - a);
+                sum -= nums[a++];
             }
-
-            ++a;
-            while (a < b && nums[a] == nums[a - 1])
-                ++a;
         }
 
-        return result;
+        return minLen == nums.size() + 1 ? 0 : minLen;
     }
 };
 
