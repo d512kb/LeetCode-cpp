ae3c1202334e6ab920503a79b45333a78d5cf143
Danil Pinchuk <512kb@proton.me>
2024-02-22 06:19:30 -0500

    36. Valid Sudoku

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -11,46 +11,30 @@ using namespace std;
 
 class Solution {
 public:
-    string minWindow(string s, string t) {
-        if (s.size() < t.size() || t.empty())
-            return "";
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<int> h, v, z;
 
-        vector<int> charsData(123, 0);
-        vector<int> countChars(123, 0);
+        for (int i = 0; i < 9; ++i) {
+            h.assign(64, 0);
+            v.assign(64, 0);
+            z.assign(64, 0);
 
-        for (const auto& c : t) {
-            ++charsData[c];
-        }
+            for (int j = 0; j < 9; ++j) {
+                char& hc = board[i][j];
+                char& vc = board[j][i];
+                char& zc = board[i / 3 * 3 + j / 3][i % 3 * 3 + j % 3];
 
-        int a = 0, b = 0;
-        int collected = 0;
-        int minLen = s.size()+1;
-        pair<int, int> minPos{ a, b };
-
-        while (b < s.size()) {
-            const char c = s[b++];
-
-            if (++countChars[c] <= charsData[c]) {
-                ++collected;
-            }
-
-            while (a < b && collected == t.size()) {
-                const char c = s[a];
-
-                if (countChars[c]-- <= charsData[c]) {
-                    if (b - a < minLen) {
-                        minPos.first = a;
-                        minPos.second = b;
-                        minLen = b - a;
-                    }
-                    --collected;
+                if (hc != 46 && ++h[hc] > 1) {
+                    return false;
+                } else if (vc != 46 && ++v[vc] > 1) {
+                    return false;
+                } else if (zc != 46 && ++z[zc] > 1) {
+                    return false;
                 }
-
-                ++a;
             }
         }
 
-        return { s.begin() + minPos.first, s.begin() + minPos.second };
+        return true;
     }
 };
 
