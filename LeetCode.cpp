e1b40eb02f5d28807b9134c0f39ff557c619b417
Danil Pinchuk <512kb@proton.me>
2024-02-23 02:38:59 -0500

    383. Ransom Note

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,34 +7,21 @@ using namespace std;
 
 class Solution {
 public:
-    void gameOfLife(vector<vector<int>>& board) {
-        vector<vector<int>> nextBoard(board);
-        int rMax = board.size() - 1;
-        int cMax = board.front().size() - 1;
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> chars(128);
+        int mLen = magazine.size();
+        int i = 0;
 
-        vector<pair<int, int>> dirs{ {-1,-1},{-1,0},{-1, 1}, {0,-1}, {0,1}, {1,-1},{1,0},{1,1} };
-
-        for (int r = 0; r <= rMax; ++r) {
-            for (int c = 0; c <= cMax; ++c) {
-                int n = 0;
-
-                for (const auto& dir : dirs) {
-                    int rc = r + dir.first;
-                    int cc = c + dir.second;
-
-                    if (rc >= 0 && rc <= rMax && cc >= 0 && cc <= cMax)
-                        n += board[rc][cc];
-                }
-
-                if (board[r][c] == 0) {
-                    nextBoard[r][c] = n == 3;
-                } else {
-                    nextBoard[r][c] = n > 1 && n < 4;
-                }
+        for (const char& c : ransomNote) {
+            while (chars[c] == 0 && i < mLen) {
+                ++chars[magazine[i++]];
             }
+
+            if (--chars[c] < 0)
+                return false;
         }
 
-        board = nextBoard;
+        return true;
     }
 };
 
