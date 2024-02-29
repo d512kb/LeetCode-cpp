0a293ba8f0365c158676cd77b010e2856c0ded1b
Danil Pinchuk <512kb@proton.me>
2024-02-29 00:45:15 -0500

    155. Min Stack

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -5,40 +5,51 @@
 
 using namespace std;
 
-class Solution {
+class MinStack {
 public:
-    string simplifyPath(string path) {
-        vector<string> v;
-        int a = 0;
+    MinStack() {
 
-        while (a < path.size()) {
-            int b = path.find('/', a);
-            b = b == string::npos ? path.size() : b;
-            string s = path.substr(a, b - a);
-
-            if (s == ".." && !v.empty()) {
-                v.pop_back();
-            } else if (s != ".." && s != "/" && s != "." && !s.empty()) {
-                v.push_back(s);
-            }
-
-            a = b + 1;
-        }
-
-        string result;
-        for (string& s : v) {
-            result.append("/").append(s);
-        }
-
-        return result.empty() ? "/" : result;
     }
+
+    void push(int val) {
+        m_stack.push(val);
+
+        if (m_mins.empty() || val <= m_mins.top()) {
+            m_mins.push(val);
+        }
+    }
+
+    void pop() {
+        if (m_stack.top() <= m_mins.top())
+            m_mins.pop();
+
+        m_stack.pop();
+    }
+
+    int top() {
+        return m_stack.top();
+    }
+
+    int getMin() {
+        return m_mins.top();
+    }
+
+private:
+    stack<int> m_stack;
+    stack<int> m_mins;
 };
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
 
 int main() {
     INIT_TIME(timer);
-
-    Solution sol;
-    sol.simplifyPath("/../");
 
     PRINT_ELAPSED(timer);
     return 0;
