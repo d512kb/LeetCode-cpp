30cf40bf11e8973a2544543ae88d651ff316cc79
Danil Pinchuk <512kb@proton.me>
2024-03-01 03:04:07 -0500

    141. Linked List Cycle

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -5,48 +5,32 @@
 
 using namespace std;
 
-class Solution {
-public:
-    int calculate(string s) {
-        int num = 0;
-        int sum = 0;
-        int sign = 1;
-        vector<pair<int, int>> sums;
+// Definition for singly-linked list.
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+ 
+ class Solution {
+ public:
+     bool hasCycle(ListNode* head) {
+         ListNode* node = head;
 
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == ' ') {
-                continue;
-            } else if (s[i] >= '0' && s[i] <= '9') {
-                num = num * 10 + (s[i] - '0');
-            } else if (s[i] == '(') {
-                sums.emplace_back(sum, sign);
-                sign = 1;
-                sum = 0;
-            } else if (s[i] == ')') {
-                sum = sum + sign * num;
-                num = 0;
+         while (head && node && node->next) {
+             head = head->next;
+             node = node->next->next;
 
-                sum = sums.back().first + sums.back().second * sum;
-                sums.pop_back();
-            } else if (s[i] == '-' || s[i] == '+') {
-                sum += sign * num;
-                num = 0;
+             if (head == node)
+                 return true;
+         }
 
-                sign = ',' - s[i];
-            }
-        }
-
-        sum += sign * num;
-
-        return sum;
-    }
-};
+         return false;
+     }
+ };
 
 int main() {
     INIT_TIME(timer);
-
-    Solution sol;
-    sol.calculate("1 - ((2 - 3) - (4 - 5))");
         
     PRINT_ELAPSED(timer);
     return 0;
