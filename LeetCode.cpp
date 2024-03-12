9825e5d01c8c21817772a74171796a0df02e3c19
Danil Pinchuk <512kb@proton.me>
2024-03-12 07:23:10 -0400

    114. Flatten Binary Tree to Linked List (slow+fast)

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -5,54 +5,89 @@
 
 using namespace std;
 
-class Node {
-public:
+//Definition for a binary tree node.
+struct TreeNode {
     int val;
-    Node* left;
-    Node* right;
-    Node* next;
-
-    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val, Node* _left, Node* _right, Node* _next)
-        : val(_val), left(_left), right(_right), next(_next) {}
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
 };
  
 class Solution {
 public:
-    Node* connect(Node* root) {
-        Node* preHead = new Node();
-        Node* prev = preHead;
-        Node* node = root;
+    void flatten(TreeNode* root) {
+        TreeNode* preRoot = new TreeNode(0);
+        TreeNode* preRootPtr = preRoot;
 
-        while (node) {
-            if (node->left) {
-                prev->next = node->left;
-                prev = node->left;
-            }
-            if (node->right) {
-                prev->next = node->right;
-                prev = node->right;
-            }
-            node = node->next;
+        flattenHelper(root, preRoot);
+        setRight(root);
 
-            if (!node) {
-                node = preHead->next;
-                preHead->next = nullptr;
-                prev = preHead;
+        delete preRootPtr;
+    }
+    
+    void flattenIter(TreeNode* root) {
+        TreeNode* cur = root;
+        while (cur) {
+            if (cur->left) {
+                TreeNode* tmp = cur->left;
+                while (tmp->right) {
+                    tmp = tmp->right;
+                }
+                tmp->right = cur->right;
+                cur->right = cur->left;
+                cur->left = nullptr;
             }
+            cur = cur->right;
         }
+    }
 
-        delete preHead;
+    TreeNode* prev = nullptr;
+    void flattenRecurFast(TreeNode* root) {
+        if (!root) return;
 
-        return root;
+        flatten(root->right);
+        flatten(root->left);
+        root->right = prev;
+        root->left = nullptr;
+        prev = root;
+    }
+private:
+    void flattenHelper(TreeNode* root, TreeNode*& prev) {
+        if (!root)
+            return;
+
+        prev->left = root;
+        prev = root;
+
+        flattenHelper(root->left, prev);
+        flattenHelper(root->right, prev);
+    }
+
+    void setRight(TreeNode* root) {
+        if (!root)
+            return;
+
+        setRight(root->left);
+
+        root->right = root->left;
+        root->left = nullptr;
     }
 };
 
 int main() {
     INIT_TIME(timer);
+
+    TreeNode root[]{0,1,2,3,4,5,6};
+    root[1].left = &root[2];
+    root[1].right = &root[5];
+    root[2].left = &root[3];
+    root[2].right = &root[4];
+    root[5].right = &root[6];
+    
+    Solution sol;
+    sol.flatten(&root[1]);
 
     PRINT_ELAPSED(timer);
     return 0;
