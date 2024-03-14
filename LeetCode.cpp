f84ef499260ca0fff0226e17f232380bba1b926b
Danil Pinchuk <512kb@proton.me>
2024-03-14 16:59:33 -0400

    236. Lowest Common Ancestor of a Binary Tree

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -15,37 +15,35 @@ struct TreeNode {
     TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
 };
  
-class BSTIterator {
+class Solution {
 public:
-    BSTIterator(TreeNode* root) {
-        if (root)
-            saveTree(root);
-    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || root == p || root == q)
+            return root;
 
-    int next() {
-        TreeNode* node = m_backTrack.back();
-        m_backTrack.pop_back();
-        if (node->right)
-            saveTree(node->right);
-        return node->val;
-    }
+        TreeNode* a = lowestCommonAncestor(root->left, p, q);
+        TreeNode* b = lowestCommonAncestor(root->right, p, q);
 
-    bool hasNext() {
-        return !m_backTrack.empty();
-    }
-private:
-    void saveTree(TreeNode* node) {
-        while (node) {
-            m_backTrack.push_back(node);
-            node = node->left;
-        }
-    }
+        if (a && b)
+            return root;
 
-    vector<TreeNode*> m_backTrack;
+        return !a ? b : !b ? a : nullptr;
+    }
 };
 
 int main() {
     INIT_TIME(timer);
+
+    TreeNode tn[]{ 0,1,2,3,4,5,6,7,8,9 };
+    tn[3].left = &tn[5];
+    tn[3].right = &tn[1];
+    tn[5].left = &tn[6];
+    tn[5].right = &tn[2];
+    tn[2].left = &tn[7];
+    tn[2].right = &tn[4];
+
+    Solution sol;
+    auto p = sol.lowestCommonAncestor(&tn[3], &tn[6], &tn[4]);
 
     PRINT_ELAPSED(timer);
     return 0;
