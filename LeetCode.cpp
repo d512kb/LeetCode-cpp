11be32fb9de0ede82614214b59dbea6b23c838ca
Danil Pinchuk <512kb@proton.me>
2024-03-14 22:29:05 -0400

    103. Binary Tree Zigzag Level Order Traversal

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -17,23 +17,37 @@ struct TreeNode {
  
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> result;
-        levelOrderHelper(root, 1, result);
+        deque<TreeNode*> q;
+        vector<int> row;
+
+        if (root)
+            q.push_back(root);
+
+        bool odd = false;
+        while (!q.empty()) {
+            int s = q.size();
+            odd = !odd;
+            row.clear();
+
+            for (; s > 0; --s) {
+                TreeNode* node = q.front();
+                q.pop_front();
+                row.push_back(node->val);
+                if (node->left) q.push_back(node->left);
+                if (node->right) q.push_back(node->right);
+            }
+
+            if (odd) {
+                result.push_back(move(row));
+            } else {
+                result.emplace_back();
+                result.back().insert(result.back().end(), row.rbegin(), row.rend());
+            }
+        }
+
         return result;
-    }
-private:
-    void levelOrderHelper(TreeNode* node, int h, vector<vector<int>>& nodes) {
-        if (!node)
-            return;
-
-        if (h > nodes.size())
-            nodes.emplace_back();
-
-        nodes[h - 1].push_back(node->val);
-
-        levelOrderHelper(node->left, h + 1, nodes);
-        levelOrderHelper(node->right, h + 1, nodes);
     }
 };
 
