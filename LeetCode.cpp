997cfa10285fde0fc12a4219e251a3efdae48f1e
Danil Pinchuk <512kb@proton.me>
2024-03-14 22:24:13 -0400

    102. Binary Tree Level Order Traversal

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -17,27 +17,23 @@ struct TreeNode {
  
 class Solution {
 public:
-    vector<double> averageOfLevels(TreeNode* root) {
-        vector<double> result;
-        deque<TreeNode*> q;
-        q.push_back(root);
-
-        while (!q.empty()) {
-            int s = q.size();
-            double sum = 0;
-
-            for (int i = 0; i < s; ++i) {
-                TreeNode* node = q.front();
-                sum += node->val;
-                q.pop_front();
-                if (node->left) q.push_back(node->left);
-                if (node->right) q.push_back(node->right);
-            }
-
-            result.push_back(sum / s);
-        }
-
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        levelOrderHelper(root, 1, result);
         return result;
+    }
+private:
+    void levelOrderHelper(TreeNode* node, int h, vector<vector<int>>& nodes) {
+        if (!node)
+            return;
+
+        if (h > nodes.size())
+            nodes.emplace_back();
+
+        nodes[h - 1].push_back(node->val);
+
+        levelOrderHelper(node->left, h + 1, nodes);
+        levelOrderHelper(node->right, h + 1, nodes);
     }
 };
 
