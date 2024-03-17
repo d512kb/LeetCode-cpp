30933322591589936aaec62351679e0c272f962c
Danil Pinchuk <512kb@proton.me>
2024-03-16 20:49:03 -0400

    133. Clone Graph

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -5,59 +5,73 @@
 
 using namespace std;
 
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
 class Solution {
 public:
-    void solve(vector<vector<char>>& board) {
-        for (int i = 0; i < board.size(); ++i) {
-            for (int j = 0; j < board.front().size(); ++j) {
-                if (board[i][j] == 'O') {
-                    vector<pair<int, int>> cells;
-                    if (checkRegion(board, cells, i, j)) {
-                        for (const auto& p : cells) {
-                            board[p.first][p.second] = 'X';
-                        }
+    Node* cloneGraph(Node* node) {
+        if (!node)
+            return nullptr;
+
+        unordered_map<Node*, Node*> mapping;
+        Node* result = new Node(node->val);
+        mapping.emplace(node, result);
+
+        deque<Node*> q;
+        q.push_back(node);
+
+        while (!q.empty()) {
+            int s = q.size();
+
+            for (; s > 0; --s) {
+                node = q.front();
+                q.pop_front();
+
+                for (Node* n : node->neighbors) {
+                    auto iter = mapping.find(n);
+
+                    if (iter == mapping.end()) {
+                        iter = mapping.emplace(n, new Node(n->val)).first;
+                        q.push_back(n);
                     }
+
+                    mapping[node]->neighbors.push_back(iter->second);
                 }
             }
         }
 
-        for (auto& v : board) {
-            for (auto& c : v) {
-                if (c == 'T')
-                    c = 'O';
-            }
-        }
-    }
-private:
-    bool checkRegion(vector<vector<char>>& board, vector<pair<int, int>>& cells, int i, int j) {
-        if (board[i][j] == 'X' || board[i][j] == 'T')
-            return true;
-
-        bool res = true;
-        board[i][j] = 'T';
-        cells.emplace_back(i, j);
-        int crd[]{ 0,1,0,-1,0 };
-
-        for (int c = 0; c < 4; ++c) {
-            int ic = i + crd[c];
-            int jc = j + crd[c + 1];
-
-            if (ic < 0 || ic >= board.size() || jc < 0 || jc >= board.front().size()) {
-                res = false;
-                continue;
-            }
-
-            res = checkRegion(board, cells, ic, jc) && res;
-        }
-
-        return res;
+        return result;
     }
 };
 
 int main() {
     INIT_TIME(timer);
 
-    vector<vector<char>> board{{'X','X','O','X'},{'O','O','O','X'},{'X','X','O','X'},{'X','O','X','O'}};
+    Node nodes[]{ 0,1,2,3,4 };
+    nodes[1].neighbors.assign({ &nodes[2], &nodes[4] });
+    nodes[2].neighbors.assign({ &nodes[1], &nodes[3] });
+    nodes[3].neighbors.assign({ &nodes[2], &nodes[4] });
+    nodes[4].neighbors.assign({ &nodes[1], &nodes[3] });
+
+    Solution sol;
+    auto cl = sol.cloneGraph(&nodes[1]);
 
     PRINT_ELAPSED(timer);
     return 0;
