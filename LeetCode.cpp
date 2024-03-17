bbb1d1a203864c2930e921f85eb7cefe75f0180c
Danil Pinchuk <512kb@proton.me>
2024-03-16 20:48:17 -0400

    130. Surrounded Regions

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,54 +7,57 @@ using namespace std;
 
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
-        int rows = grid.size();
-        int cols = grid.front().size();
-        int islands = 0;
-
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j) {
-                if (grid[i][j] == '1') {
-                    deque<pair<int, int>> cells;
-                    cells.emplace_back(i, j);
-                    grid[i][j] = '0';
-                    ++islands;
-
-                    while (!cells.empty()) {
-                        int c = cells.size();
-
-                        for (; c > 0; --c) {
-                            auto [i, j] = cells.front();
-                            cells.pop_front();
-
-                            if (i - 1 >= 0 && grid[i - 1][j] == '1') {
-                                cells.emplace_back(i - 1, j);
-                                grid[i - 1][j] = '0';
-                            }
-                            if (i + 1 < rows && grid[i + 1][j] == '1') {
-                                cells.emplace_back(i + 1, j);
-                                grid[i + 1][j] = '0';
-                            }
-                            if (j - 1 >= 0 && grid[i][j - 1] == '1') {
-                                cells.emplace_back(i, j - 1);
-                                grid[i][j - 1] = '0';
-                            }
-                            if (j + 1 < cols && grid[i][j + 1] == '1') {
-                                cells.emplace_back(i, j + 1);
-                                grid[i][j + 1] = '0';
-                            }
+    void solve(vector<vector<char>>& board) {
+        for (int i = 0; i < board.size(); ++i) {
+            for (int j = 0; j < board.front().size(); ++j) {
+                if (board[i][j] == 'O') {
+                    vector<pair<int, int>> cells;
+                    if (checkRegion(board, cells, i, j)) {
+                        for (const auto& p : cells) {
+                            board[p.first][p.second] = 'X';
                         }
                     }
                 }
             }
         }
 
-        return islands;
+        for (auto& v : board) {
+            for (auto& c : v) {
+                if (c == 'T')
+                    c = 'O';
+            }
+        }
+    }
+private:
+    bool checkRegion(vector<vector<char>>& board, vector<pair<int, int>>& cells, int i, int j) {
+        if (board[i][j] == 'X' || board[i][j] == 'T')
+            return true;
+
+        bool res = true;
+        board[i][j] = 'T';
+        cells.emplace_back(i, j);
+        int crd[]{ 0,1,0,-1,0 };
+
+        for (int c = 0; c < 4; ++c) {
+            int ic = i + crd[c];
+            int jc = j + crd[c + 1];
+
+            if (ic < 0 || ic >= board.size() || jc < 0 || jc >= board.front().size()) {
+                res = false;
+                continue;
+            }
+
+            res = checkRegion(board, cells, ic, jc) && res;
+        }
+
+        return res;
     }
 };
 
 int main() {
     INIT_TIME(timer);
+
+    vector<vector<char>> board{{'X','X','O','X'},{'O','O','O','X'},{'X','X','O','X'},{'X','O','X','O'}};
 
     PRINT_ELAPSED(timer);
     return 0;
