d8b1562fc78af33cb1c1397129341b3dd1325558
Danil Pinchuk <512kb@proton.me>
2024-03-19 22:43:48 -0400

    207. Course Schedule

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,63 +6,44 @@
 using namespace std;
 
 class Solution {
-    using Graph = unordered_map<string, unordered_map<string, double>>;
 public:
-    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        Graph graph;
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, unordered_set<int>> dependents;
+        vector<int> depsCount(numCourses, 0);
 
-        for (int i = 0; i < equations.size(); ++i) {
-            graph[equations[i][0]].emplace(equations[i][0], 1);
-            graph[equations[i][1]].emplace(equations[i][1], 1);
-            graph[equations[i][0]].emplace(equations[i][1], values[i]);
-            graph[equations[i][1]].emplace(equations[i][0], 1 / values[i]);
+        for (const auto& pre : prerequisites) {
+            dependents[pre[1]].insert(pre[0]);
+            ++depsCount[pre[0]];
         }
 
-        vector<double> result;
+        deque<int> q;
 
-        for (const auto& query : queries) {
-            unordered_set<string> visited;
-            double path = findPath(graph, visited, query[0], query[1]);
-
-            result.push_back(path > 0 ? path : -1);
-        }
-
-        return result;
-    }
-private:
-    double findPath(const Graph& graph, unordered_set<string>& visited, string a, string b) {
-        auto iter = graph.find(a);
-
-        if (iter == graph.end())
-            return 0;
-
-        visited.insert(a);
-
-        if (a == b)
-            return 1;
-
-        double val = 0;
-
-        for (const auto& neighbor : iter->second) {
-            if (!visited.contains(neighbor.first)) {
-                val += neighbor.second * findPath(graph, visited, neighbor.first, b);
+        for (int i = 0; i < depsCount.size(); ++i) {
+            if (depsCount[i] == 0) {
+                q.push_back(i);
             }
         }
 
-        return val;
+        while (!q.empty()) {
+            int i = q.front();
+
+            for (int dep : dependents[i]) {
+                if (--depsCount[dep] == 0) {
+                    q.push_back(dep);
+                }
+            }
+
+            dependents.erase(i);
+            q.pop_front();
+        }
+
+        return dependents.empty();
     }
 };
 
 int main() {
     INIT_TIME(timer);
-
-    vector<vector<string>> equations{ {"a", "b"}, {"b", "c"}, {"a", "c"}};
-    vector<double> values{ 2, 3, 6 };
-    vector<vector<string>> queries{ {"a", "c"}, { "b", "a" }, {"a", "e"}, {"a", "a"}, {"x","x"} };
-
-    Solution sol;
-    auto result = sol.calcEquation(equations, values, queries);
-
+        
     PRINT_ELAPSED(timer);
     return 0;
 }
