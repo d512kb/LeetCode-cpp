e52a9227845b89188daaad0c851829a72eed99bb
Danil Pinchuk <512kb@proton.me>
2024-03-27 02:53:48 -0400

    909. Snakes and Ladders

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,46 +7,68 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, vector<int>> dependents;
-        vector<int> depsCount(numCourses, 0);
+    int snakesAndLadders(vector<vector<int>>& board) {
+        int n = board.size();
+        int n2 = n * n;
 
-        for (const auto& p : prerequisites) {
-            dependents[p[1]].push_back(p[0]);
-            ++depsCount[p[0]];
+        vector<int> linear(n2 + 1);
+
+        int i = 1;
+        for (int line = n - 1; line > 0; line -= 2) {
+            for (int item : board[line]) {
+                linear[i++] = item;
+            }
+
+            auto a = board[line - 1].rbegin();
+            auto b = board[line - 1].rend();
+            for (a; a < b; ++a) {
+                linear[i++] = *a;
+            }
+        }
+
+        if (n % 2) {
+            for (int item : board[0]) {
+                linear[i++] = item;
+            }
         }
 
         queue<int> q;
-        vector<int> result;
-
-        for (int i = 0; i < depsCount.size(); ++i) {
-            if (depsCount[i] == 0)
-                q.push(i);
-        }
+        q.push(1);
+        linear[1] = 0;
+        int stepCount = 0;
 
         while (!q.empty()) {
-            int i = q.front();
-            result.push_back(i);
+            int s = q.size();
+            ++stepCount;
 
-            for (const auto& dep : dependents[i]) {
-                if (--depsCount[dep] == 0) {
-                    q.push(dep);
+            for (; s > 0; --s) {
+                int a = q.front();
+                q.pop();
+
+                for (int next = 1; next <= 6; ++next) {
+                    if (a + next == n2 || linear[a + next] == n2)
+                        return stepCount;
+
+                    if (linear[a + next])
+                        q.push(linear[a + next] == -1 ? a + next : linear[a + next]);
+
+                    linear[a + next] = 0;
                 }
             }
-
-            q.pop();
         }
 
-        if (result.size() != numCourses)
-            result.clear();
-
-        return result;
+        return -1;
     }
 };
 
 int main() {
     INIT_TIME(timer);
-        
+
+    vector<vector<int>> v {{-1, -1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1}, {-1, 35, -1, -1, 13, -1}, {-1, -1, -1, -1, -1, -1}, {-1, 15, -1, -1, -1, -1}};
+
+    Solution sol;
+    int i = sol.snakesAndLadders(v);
+
     PRINT_ELAPSED(timer);
     return 0;
 }
