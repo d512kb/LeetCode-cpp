c906f0fccb6ba32f7e39ded2544abea70a185dc4
Danil Pinchuk <512kb@proton.me>
2024-04-03 03:58:13 -0400

    17. Letter Combinations of a Phone Number

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -5,74 +5,37 @@
 
 using namespace std;
 
-class WordDictionary {
-    struct Node {
-        Node(char c = 0) : c(c), word(false), nodes{ nullptr } {};
-
-        char c;
-        bool word;
-        Node* nodes[26];
-    };
+class Solution {
 public:
-    WordDictionary() {
+    vector<string> letterCombinations(string digits) {
+        vector<string> result;
 
-    }
+        if (digits.empty())
+            return result;
 
-    void addWord(string word) {
-        Node* node = &root;
-        int sz = word.size();
-
-        for (int i = 0; i < sz; ++i) {
-            int idx = word[i] - 'a';
-            if (!node->nodes[idx]) {
-                node->nodes[idx] = new Node(word[i]);
-            }
-            node = node->nodes[idx];
-        }
-
-        node->word = true;
-    }
-
-    bool search(string word) {
-        return search(word, &root, 0);
-    }
-private:
-    bool search(const string& word, Node* node, int index) {
-        if (index == word.size()) {
-            return node->word;
-        }
-
-        bool result = false;
-
-        if (word[index] == '.') {
-            for (auto n : node->nodes) {
-                result = result || (n && search(word, n, index + 1));
-            }
-        } else {
-            int idx = word[index] - 'a';
-            result = node->nodes[idx] && search(word, node->nodes[idx], index + 1);
-        }
+        iterate(digits, 0, "", result);
 
         return result;
     }
-
 private:
-    Node root;
+    void iterate(const string& rest, int index, string collected, vector<string>& result) {
+        if (index == rest.size()) {
+            result.push_back(move(collected));
+            return;
+        }
+
+        int digit = rest[index] - '0';
+
+        for (char c : keys[digit]) {
+            iterate(rest, index + 1, collected + c, result);
+        }
+    }
+private:
+    vector<string> keys{ "", "", "abc","def","ghi","jkl","mno","pqrs","tuv","wxyz" };
 };
 
 int main() {
     INIT_TIME(timer);
-
-    WordDictionary dict;
-    dict.addWord("bad");
-    dict.addWord("dad");
-    dict.addWord("mad");
-    dict.search("pad");
-    dict.search("bad");
-    dict.search(".ad");
-    dict.search("..");
-    dict.addWord("da");
-    dict.search("..");
 
     PRINT_ELAPSED(timer);
     return 0;
