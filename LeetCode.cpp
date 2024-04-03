9be70226b051011535c27fea3b11d41b9728c558
Danil Pinchuk <512kb@proton.me>
2024-04-03 02:55:37 -0400

    211. Design Add and Search Words Data Structure

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -5,25 +5,24 @@
 
 using namespace std;
 
-class Trie {
+class WordDictionary {
     struct Node {
-        Node() : c(0), word(false), nodes{ nullptr } {}
-        Node(char c) : c(c), word(false), nodes{ nullptr } {}
+        Node(char c = 0) : c(c), word(false), nodes{ nullptr } {};
 
         char c;
         bool word;
         Node* nodes[26];
     };
 public:
-    Trie() {
+    WordDictionary() {
 
     }
 
-    void insert(string word) {
-        int wordSize = word.size();
+    void addWord(string word) {
         Node* node = &root;
+        int sz = word.size();
 
-        for (int i = 0; i < wordSize; ++i) {
+        for (int i = 0; i < sz; ++i) {
             int idx = word[i] - 'a';
             if (!node->nodes[idx]) {
                 node->nodes[idx] = new Node(word[i]);
@@ -35,35 +34,45 @@ public:
     }
 
     bool search(string word) {
-        return search(word, false);
-    }
-
-    bool startsWith(string prefix) {
-        return search(prefix, true);
+        return search(word, &root, 0);
     }
 private:
-    bool search(const string& word, bool prefixOnly) {
-        int wordSize = word.size();
-        Node* node = &root;
-
-        for (int i = 0; i < wordSize; ++i) {
-            int idx = word[i] - 'a';
-            if (!node->nodes[idx]) {
-                return false;
-            }
-            node = node->nodes[idx];
+    bool search(const string& word, Node* node, int index) {
+        if (index == word.size()) {
+            return node->word;
         }
 
-        return prefixOnly || node->word;
+        bool result = false;
+
+        if (word[index] == '.') {
+            for (auto n : node->nodes) {
+                result = result || (n && search(word, n, index + 1));
+            }
+        } else {
+            int idx = word[index] - 'a';
+            result = node->nodes[idx] && search(word, node->nodes[idx], index + 1);
+        }
+
+        return result;
     }
 
 private:
     Node root;
 };
 
-
 int main() {
     INIT_TIME(timer);
+
+    WordDictionary dict;
+    dict.addWord("bad");
+    dict.addWord("dad");
+    dict.addWord("mad");
+    dict.search("pad");
+    dict.search("bad");
+    dict.search(".ad");
+    dict.search("..");
+    dict.addWord("da");
+    dict.search("..");
 
     PRINT_ELAPSED(timer);
     return 0;
