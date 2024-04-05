5a57d75789f1e86070b0f6d600249a1f02556037
Danil Pinchuk <512kb@proton.me>
2024-04-04 22:56:51 -0400

    212. Word Search II

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,36 +6,83 @@
 using namespace std;
 
 class Solution {
+    struct Node {
+        Node() : word(false), nodes{ nullptr } {};
+        bool word;
+        Node* nodes[26];
+    };
+
+    void addWord(const string& word) {
+        Node* node = &root;
+
+        for (char c : word) {
+            int idx = c - 'a';
+            if (!node->nodes[idx])
+                node->nodes[idx] = new Node();
+
+            node = node->nodes[idx];
+        }
+
+        node->word = true;
+    }
+
+    Node* getNode(Node* node, char c) {
+        return node->nodes[c - 'a'];
+    }
+
+    Node root;
 public:
-    vector<string> letterCombinations(string digits) {
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        int rows = board.size();
+        int cols = board.front().size();
+        string word;
         vector<string> result;
 
-        if (digits.empty())
-            return result;
+        for (const string& word : words) {
+            addWord(word);
+        }
 
-        iterate(digits, 0, "", result);
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                processCell(board, i, j, rows, cols, word, &root, result);
+            }
+        }
 
         return result;
     }
 private:
-    void iterate(const string& rest, int index, string collected, vector<string>& result) {
-        if (index == rest.size()) {
-            result.push_back(move(collected));
+    void processCell(vector<vector<char>>& board, int i, int j, int rows, int cols, string& word, Node* node, vector<string>& result) {
+        if (i < 0 || i >= rows || j < 0 || j >= cols || !board[i][j] || !getNode(node, board[i][j]))
             return;
+
+        char ch = board[i][j];
+        board[i][j] = 0;
+        word.push_back(ch);
+        node = getNode(node, ch);
+
+        if (node->word) {
+            result.push_back(word);
+            node->word = false;
         }
 
-        int digit = rest[index] - '0';
+        processCell(board, i, j + 1, rows, cols, word, node, result);
+        processCell(board, i + 1, j, rows, cols, word, node, result);
+        processCell(board, i, j - 1, rows, cols, word, node, result);
+        processCell(board, i - 1, j, rows, cols, word, node, result);
 
-        for (char c : keys[digit]) {
-            iterate(rest, index + 1, collected + c, result);
-        }
+        board[i][j] = ch;
+        word.pop_back();
     }
-private:
-    vector<string> keys{ "", "", "abc","def","ghi","jkl","mno","pqrs","tuv","wxyz" };
 };
 
 int main() {
     INIT_TIME(timer);
+
+    vector<vector<char>> board {{'o', 'a', 'a', 'n'}, {'e', 't', 'a', 'e'}, {'i', 'h', 'k', 'r'}, {'i', 'f', 'l', 'v'}};
+    vector<string> words{"oath","pea","eat","rain" };
+
+    Solution sol;
+    auto w = sol.findWords(board, words);
 
     PRINT_ELAPSED(timer);
     return 0;
