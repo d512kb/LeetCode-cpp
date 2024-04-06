108e4cb03f2e2fdcb1b8068a677c165bc5f1121c
Danil Pinchuk <512kb@proton.me>
2024-04-06 04:25:24 -0400

    39. Combination Sum

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,31 +7,30 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int>& nums) {
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> result;
-        vector<int> used(nums.size(), 0);
-        vector<int> pm;
+        vector<int> v;
 
-        perm(nums, used, pm, result);
+        calc(candidates, 0, target, v, result);
 
         return result;
-
     }
 private:
-    void perm(vector<int>& nums, vector<int>& used, vector<int>& pm, vector<vector<int>>& result) {
-        if (pm.size() == nums.size()) {
-            result.push_back(pm);
+    void calc(vector<int>& candidates, int index, int target, vector<int>& v, vector<vector<int>>& result) {
+        if (target == 0) {
+            result.push_back(v);
             return;
         }
 
-        for (int i = 0; i < nums.size(); ++i) {
-            if (!used[i]) {
-                used[i] = 1;
-                pm.push_back(nums[i]);
-                perm(nums, used, pm, result);
-                pm.pop_back();
-                used[i] = 0;
-            }
+        if (target < 0)
+            return;
+
+        for (; index < candidates.size(); ++index) {
+            target -= candidates[index];
+            v.push_back(candidates[index]);
+            calc(candidates, index, target, v, result);
+            v.pop_back();
+            target += candidates[index];
         }
     }
 };
