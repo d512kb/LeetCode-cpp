aa7f22e854efb76a4577ce61e2c6bf93ebd9007d
Danil Pinchuk <512kb@proton.me>
2024-04-15 07:36:13 -0400

    918. Maximum Sum Circular Subarray

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,16 +7,23 @@ using namespace std;
 
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int currMax = 0;
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int sumPlus = 0;
+        int sumMinus = 0;
         int maxSum = nums[0];
+        int minSum = nums[0];
+        int totalSum = 0;
 
         for (int num : nums) {
-            currMax = max(currMax, 0) + num;
-            maxSum = max(maxSum, currMax);
+            totalSum += num;
+            sumPlus = max(sumPlus, 0) + num;
+            sumMinus = min(sumMinus, 0) + num;
+
+            maxSum = max(maxSum, sumPlus);
+            minSum = min(minSum, sumMinus);
         }
 
-        return maxSum;
+        return maxSum < 0 ? maxSum : max(maxSum, totalSum - minSum);
     }
 };
 
