e10430bcfa5ef89d294ace40cc7b12461cc42544
Danil Pinchuk <512kb@proton.me>
2024-04-16 22:46:57 -0400

    34. Find First and Last Position of Element in Sorted Array

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,32 +7,51 @@ using namespace std;
 
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
+    vector<int> searchRange(vector<int>& nums, int target) {
         int a = 0;
         int b = nums.size() - 1;
 
         while (a <= b) {
             int mid = (a + b) / 2;
 
-            if (nums[mid] == target)
-                return mid;
-
-            if (nums[a] <= nums[mid]) {
-                if (nums[a] <= target && target <= nums[mid]) {
-                    b = mid - 1;
-                } else {
-                    a = mid + 1;
-                }
+            if (nums[mid] < target) {
+                a = mid + 1;
+            } else if (nums[mid] > target) {
+                b = mid - 1;
             } else {
-                if (nums[mid] <= target && target <= nums[b]) {
-                    a = mid + 1;
-                } else {
-                    b = mid - 1;
+                int ra = a;
+                int rb = mid;
+                int rmid = 0;
+
+                while (ra <= rb) {
+                    rmid = (ra + rb) / 2;
+
+                    if (nums[rmid] == target) {
+                        rb = rmid - 1;
+                    } else {
+                        ra = rmid + 1;
+                    }
                 }
+
+                a = ra;
+                ra = mid;
+                rb = b;
+
+                while (ra <= rb) {
+                    rmid = (ra + rb) / 2;
+
+                    if (nums[rmid] == target) {
+                        ra = rmid + 1;
+                    } else {
+                        rb = rmid - 1;
+                    }
+                }
+
+                return { a, rb };
             }
         }
 
-        return -1;
+        return { -1, -1 };
     }
 };
 
