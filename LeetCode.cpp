8dc541485bc322eb67bc8b5d9cb201c19fc9b1d8
Danil Pinchuk <512kb@proton.me>
2024-04-27 22:44:55 -0400

    502. IPO

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,46 +7,32 @@ using namespace std;
 
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if (nums1.size() > nums2.size()) {
-            return findMedianSortedArrays(nums2, nums1);
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        vector<pair<int, int>> gainData;
+
+        int sz = profits.size();
+        for (int i = 0; i < sz; ++i) {
+            gainData.emplace_back(capital[i], profits[i]);
         }
 
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        int nHalf = (n1 + n2) / 2;
+        sort(gainData.begin(), gainData.end());
 
-        int low = 0;
-        int high = n1;
+        priority_queue<int> gains;
+        int i = 0;
 
-        while (low <= high) {
-            int mid1 = (low + high) / 2;
-            int mid2 = nHalf - mid1;
-
-            int left1 = numeric_limits<int>::min();
-            int right1 = numeric_limits<int>::max();
-            int left2 = left1;
-            int right2 = right1;
-
-            if (mid1 > 0) left1 = nums1[mid1 - 1];
-            if (mid1 < n1) right1 = nums1[mid1];
-            if (mid2 > 0) left2 = nums2[mid2 - 1];
-            if (mid2 < n2) right2 = nums2[mid2];
-
-            if (left1 <= right2 && left2 <= right1) {
-                if ((n1 + n2) % 2) {
-                    return min(right1, right2);
-                } else {
-                    return (max(left1, left2) + min(right1, right2)) / 2.0;
-                }
-            } else if (left1 > right2) {
-                high = mid1 - 1;
-            } else {
-                low = mid1 + 1;
+        while (k--) {
+            for (; i < sz && gainData[i].first <= w; ++i) {
+                gains.push(gainData[i].second);
             }
+
+            if (gains.empty())
+                return w;
+
+            w += gains.top();
+            gains.pop();
         }
 
-        return 0;
+        return w;
     }
 };
 
