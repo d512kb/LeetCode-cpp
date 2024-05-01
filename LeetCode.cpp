bdb0a941a08cd68b3eba500c715a5a65ec4368d0
Danil Pinchuk <512kb@proton.me>
2024-05-01 18:00:01 -0400

    191. Number of 1 Bits

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,12 +7,11 @@ using namespace std;
 
 class Solution {
 public:
-    uint32_t reverseBits(uint32_t n) {
-        uint32_t result{ 0 };
+    int hammingWeight(int n) {
+        int result = 0;
 
-        for (int i = 0; i < 32; ++i) {
-            result <<= 1;
-            result |= n & 1;
+        while (n) {
+            result += (n % 2);
             n >>= 1;
         }
 
