c311775087ce94ab53e0ff8689bdd5d1641791b3
Danil Pinchuk <512kb@proton.me>
2024-05-02 00:12:52 -0400

    137. Single Number II

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -8,10 +8,24 @@ using namespace std;
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int result = 0;
+        vector<int16_t> bits(32, 0);
 
         for (int n : nums) {
-            result ^= n;
+            for (int i = 31; i >= 0; --i) {
+                bits[i] += n % 2;
+                n >>= 1;
+            }
+        }
+
+        uint32_t powerOfTwo = 1;
+        int result = bits[31] % 3 > 0;
+
+        for (int i = 30; i >= 0; --i) {
+            powerOfTwo *= 2;
+
+            if (bits[i] % 3) {
+                result += powerOfTwo;
+            }
         }
 
         return result;
