0be8c781590f74d996e1f8af1376fa7e196f1ee4
Danil Pinchuk <512kb@proton.me>
2024-06-04 18:35:56 -0400

    139. Word Break

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,23 +6,83 @@
 using namespace std;
 
 class Solution {
-public:
-    int rob(vector<int>& nums) {
-        int preLoot = 0;
-        int loot = nums[0];
+    struct Node {
+        Node() : w(false), nodes{ nullptr } {}
+        bool w;
+        Node* nodes[26];
+    };
 
-        for (int i = 1; i < nums.size(); ++i) {
-            int tmp = loot;
-            loot = max(nums[i] + preLoot, loot);
-            preLoot = tmp;
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        for (string& s : wordDict) {
+            addWord(s);
         }
 
-        return loot;
+        m_indexes.assign(s.size(), 0);
+
+        return checkString(s);
+    }
+
+private:
+    Node root;
+    vector<char> m_indexes;
+
+    void addWord(const string& word) {
+        Node* node = &root;
+
+        for (char c : word) {
+            int idx = c - 'a';
+            if (!node->nodes[idx]) {
+                node->nodes[idx] = new Node();
+            }
+            node = node->nodes[idx];
+        }
+
+        node->w = true;
+    }
+
+    bool checkString(const string& s) {
+        int sz = s.size();
+        queue<int> q;
+        q.push(0);
+
+        while (!q.empty()) {
+            int qz = q.size();
+
+            while (qz--) {
+                int index = q.front();
+                q.pop();
+
+                if (index == sz)
+                    return true;
+
+                if (m_indexes[index]) { continue; }
+                m_indexes[index] = 1;
+                Node* node = &root;
+
+                for (; index < sz; ++index) {
+                    node = node->nodes[s[index] - 'a'];
+
+                    if (!node)
+                        break;
+
+                    if (node->w) {
+                        q.push(index + 1);
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 };
 
 int main() {
     INIT_TIME(timer);
+
+    Solution sol;
+    vector<string> dict{ "cats","dog","sand","and","cat","an" };
+    bool r = sol.wordBreak("catsandogcat", dict);
 
     PRINT_ELAPSED(timer);
     return 0;
