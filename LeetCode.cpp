28fbe0baf6ea8bf6582582a59ea8d5b6a7d26e9c
Danil Pinchuk <512kb@proton.me>
2024-07-01 03:51:48 -0400

    287. Find the Duplicate Number

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,12 +7,18 @@ using namespace std;
 
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-        for (int i = 0, z = 0, sz = nums.size(); i < sz; ++i) {
-            if (nums[i]) {
-                swap(nums[z++], nums[i]);
-            }
+    int findDuplicate(vector<int>& nums) {
+        for (int i = 0, sz = nums.size(); i < sz; ++i) {
+            int& v = nums[i];
+            int index = v > 0 ? v : -v;
+
+            if (nums[index] < 0)
+                return index;
+
+            nums[index] = -nums[index];
         }
+
+        return 0;
     }
 };
 
