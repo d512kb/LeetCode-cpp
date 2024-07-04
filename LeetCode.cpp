0717dad83c8893381c073f8e2c417a8b4f687c39
Danil Pinchuk <512kb@proton.me>
2024-07-03 23:53:43 -0400

    931. Minimum Falling Path Sum

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,16 +7,28 @@ using namespace std;
 
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        vector<int> dp(n, 1);
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int rowSz = matrix.front().size();
+        vector<vector<int>> dp(2, vector<int>(rowSz, 0));
+        auto readRowIter = dp.begin();
+        auto writeRowIter = next(dp.begin());
 
-        while (--m) {
-            for (int i = 1; i < n; ++i) {
-                dp[i] += dp[i - 1];
+        for (auto& row : matrix) {
+            for (int i = 0; i < rowSz; ++i) {
+                auto& readRow = *readRowIter;
+                auto& writeRow = *writeRowIter;
+
+                int a = readRow[max(0, i - 1)];
+                int b = readRow[i];
+                int c = readRow[min(rowSz - 1, i + 1)];
+
+                writeRow[i] = row[i] + min({ a, b, c });
             }
+
+            swap(readRowIter, writeRowIter);
         }
 
-        return dp.back();
+        return *min_element(readRowIter->begin(), readRowIter->end());
     }
 };
 
