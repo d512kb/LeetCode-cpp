27bab8cd8233381b1746f5aea5b7d77ce77da38f
Danil Pinchuk <512kb@proton.me>
2024-07-05 10:42:47 -0400

    1550. Three Consecutive Odds

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,33 +7,24 @@ using namespace std;
 
 class Solution {
 public:
-    long long mostPoints(vector<vector<int>>& questions) {
-        vector<int64_t> dp(questions.size());
-        auto dpIter = dp.rbegin();
-        int64_t result = 0;
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int oddsCount = 0;
 
-        for (auto iter = questions.rbegin(), iterEnd = questions.rend(); iter < iterEnd; ++iter, ++dpIter) {
-            int64_t val = (*iter)[0];
-            int steps = (*iter)[1];
-
-            if (distance(dp.rbegin(), dpIter) > steps) {
-                val += *prev(dpIter, steps + 1);
+        for (int n : arr) {
+            if (n % 2) {
+                if (++oddsCount == 3)
+                    return true;
+            } else {
+                oddsCount = 0;
             }
-
-            result = max(result, val);
-            *dpIter = result;
         }
 
-        return result;
+        return false;
     }
 };
 
 int main() {
     INIT_TIME(timer);
-
-    Solution sol;
-    vector<vector<int>> v {{3,2 }, { 4,3 }, { 4,4 }, { 2,5 }};
-    sol.mostPoints(v);
 
     PRINT_ELAPSED(timer);
     return 0;
