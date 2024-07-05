13925f68816a27aa5ea4b60556123ba403f34676
Danil Pinchuk <512kb@proton.me>
2024-07-05 18:25:08 -0400

    1027. Longest Arithmetic Subsequence

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,14 +7,28 @@ using namespace std;
 
 class Solution {
 public:
-    int longestSubsequence(vector<int>& arr, int difference) {
-        unordered_map<int, int> dp;
+    int longestArithSeqLength(vector<int>& nums) {
+        int lowest = nums.front();
+        int highest = lowest;
+
+        for (int n : nums) {
+            lowest = min(lowest, n);
+            highest = max(highest, n);
+        }
+
+        int maxDiff = highest - lowest;
+        vector<vector<int>> dp(nums.size(), vector<int>(maxDiff * 2 + 1, 1));
         int result = 0;
 
-        for (int n : arr) {
-            int val = 1 + dp[n - difference];
-            dp[n] = val;
-            result = max(result, val);
+        for (int i = 0, sz = nums.size(); i < sz; ++i) {
+            for (int j = 0; j < i; ++j) {
+                int diff = nums[i] - nums[j] + maxDiff;
+                int currDiff = dp[j][diff] + 1;
+
+                dp[i][diff] = currDiff;
+
+                result = max(result, currDiff);
+            }
         }
 
         return result;
