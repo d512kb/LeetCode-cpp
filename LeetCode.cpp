5702afe7702f620cc60601cc11f958b7d4736ef8
Danil Pinchuk <512kb@proton.me>
2024-07-07 05:24:50 -0400

    3210. Find the Encrypted String

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,28 +7,11 @@ using namespace std;
 
 class Solution {
 public:
-    int longestArithSeqLength(vector<int>& nums) {
-        int lowest = nums.front();
-        int highest = lowest;
+    string getEncryptedString(string s, int k) {
+        string result(s);
 
-        for (int n : nums) {
-            lowest = min(lowest, n);
-            highest = max(highest, n);
-        }
-
-        int maxDiff = highest - lowest;
-        vector<vector<int>> dp(nums.size(), vector<int>(maxDiff * 2 + 1, 1));
-        int result = 0;
-
-        for (int i = 0, sz = nums.size(); i < sz; ++i) {
-            for (int j = 0; j < i; ++j) {
-                int diff = nums[i] - nums[j] + maxDiff;
-                int currDiff = dp[j][diff] + 1;
-
-                dp[i][diff] = currDiff;
-
-                result = max(result, currDiff);
-            }
+        for (int i = 0, sz = s.size(); i < sz; ++i) {
+            result[i] = s[(i + k) % sz];
         }
 
         return result;
