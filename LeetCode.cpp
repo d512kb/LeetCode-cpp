7fcdb7b596ea764d0d318cde5f5269677b19d41f
Danil Pinchuk <512kb@proton.me>
2024-07-07 05:30:10 -0400

    3211. Generate Binary Strings Without Adjacent Zeros

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,14 +7,35 @@ using namespace std;
 
 class Solution {
 public:
-    string getEncryptedString(string s, int k) {
-        string result(s);
+    vector<string> validStrings(int n) {
+        vector<string> result;
+        string s1("0");
+        string s2("1");
 
-        for (int i = 0, sz = s.size(); i < sz; ++i) {
-            result[i] = s[(i + k) % sz];
-        }
+        buildString(n, s1, result);
+        buildString(n, s2, result);
 
         return result;
+    }
+private:
+    void buildString(int n, string& s, vector<string>& result) {
+        if (s.size() == n) {
+            result.push_back(s);
+            return;
+        }
+
+        if (s.back() == '0') {
+            s.push_back('1');
+            buildString(n, s, result);
+            s.pop_back();
+        } else {
+            s.push_back('0');
+            buildString(n, s, result);
+            s.pop_back();
+            s.push_back('1');
+            buildString(n, s, result);
+            s.pop_back();
+        }
     }
 };
 
