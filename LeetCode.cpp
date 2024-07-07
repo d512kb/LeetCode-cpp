66d6b9dfcec65c47e2699b0d7ed6c8cd535e96fc
Danil Pinchuk <512kb@proton.me>
2024-07-07 19:16:05 -0400

    1143. Longest Common Subsequence

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,32 +7,26 @@ using namespace std;
 
 class Solution {
 public:
-    int numberOfSubmatrices(vector<vector<char>>& grid) {
-        int rows = grid.size();
-        int cols = grid[0].size();
-        int result = 0;
+    int longestCommonSubsequence(string text1, string text2) {
+        int sz1 = text1.size();
+        int sz2 = text2.size();
 
-        vector<pair<int, int>> dp(cols + 1);
+        vector<int> dpCurr(sz2 + 1);
+        vector<int> dpPrev(sz2 + 1);
 
-        for (auto& row : grid) {
-            auto prevRow = dp;
+        for (int i = 0; i < sz1; ++i) {
+            swap(dpCurr, dpPrev);
 
-            for (int j = 0; j < cols; ++j) {
-                char c = row[j];
-                auto& currCell = dp[j + 1];
-                auto& leftCell = dp[j];
-                auto& diagCell = prevRow[j];
-
-                currCell.first += leftCell.first - diagCell.first + (c == 'X');
-                currCell.second += leftCell.second - diagCell.second + (c == 'Y');
-
-                if (currCell.first > 0 && currCell.first == currCell.second) {
-                    ++result;
+            for (int j = 0; j < sz2; ++j) {
+                if (text1[i] == text2[j]) {
+                    dpCurr[j + 1] = 1 + dpPrev[j];
+                } else {
+                    dpCurr[j + 1] = max(dpCurr[j], dpPrev[j + 1]);
                 }
             }
         }
 
-        return result;
+        return dpCurr.back();
     }
 };
 
