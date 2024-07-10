ca4189adaa0e047b1ed1fb6b10c98bd32852f86a
Danil Pinchuk <512kb@proton.me>
2024-07-10 00:11:19 -0400

    96. Unique Binary Search Trees

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,36 +7,21 @@ using namespace std;
 
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        m_cache.assign(prices.size(), vector<int>(2, -1));
+    int numTrees(int n) {
+        vector<int> dp(n + 1, 1);
 
-        return calcPrices(prices, 0, true);
+        for (int i = 2; i <= n; ++i) {
+            int trees = 0;
+
+            for (int j = 1; j <= i; ++j) {
+                trees += dp[j - 1] * dp[i - j];
+            }
+
+            dp[i] = trees;
+        }
+
+        return dp.back();
     }
-private:
-    int calcPrices(vector<int>& prices, int index, bool canBuy) {
-        if (index >= prices.size()) {
-            return 0;
-        }
-
-        int& result = m_cache[index][canBuy];
-
-        if (result >= 0) {
-            return result;
-        }
-
-        if (canBuy) {
-            result = max(-prices[index] + calcPrices(prices, index + 1, false),
-                         calcPrices(prices, index + 1, true));
-        } else {
-            result = max(prices[index] + calcPrices(prices, index + 2, true),
-                         calcPrices(prices, index + 1, false));
-        }
-
-        return result;
-    }
-
-    vector<vector<int>> m_cache;
-    int m_maxProfit;
 };
 
 int main() {
