454c610e7ccae7b8e4ed8d561daee0984c9061b5
Danil Pinchuk <512kb@proton.me>
2024-07-10 05:33:17 -0400

    543. Diameter of Binary Tree

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -14,62 +14,31 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
- 
+
 class Solution {
 public:
-    vector<TreeNode*> generateTrees(int n) {
-        return genTrees(1, n);
-    }
-private:
-    vector<TreeNode*> genTrees(int from, int to) {
-        if (from > to)
-            return { nullptr };
+    int diameterOfBinaryTree(TreeNode* root) {
+        int result = 0;
 
-        if (from == to) {
-            return { new TreeNode(from) };
-        }
-
-        vector<TreeNode*> result;
-
-        for (int i = from; i <= to; ++i) {
-            auto leftTrees = genTrees(from, i - 1);
-            auto rightTrees = genTrees(i + 1, to);
-
-            int ltsz = leftTrees.size();
-            int rtsz = rightTrees.size();
-
-            for (int lt = 0; lt < ltsz; ++lt) {
-                for (int rt = 0; rt < rtsz; ++rt) {
-                    result.push_back(new TreeNode(i, copyTree(leftTrees[lt]), copyTree(rightTrees[rt])));
-                }
-            }
-
-            for_each(leftTrees.begin(), leftTrees.end(), deleteTree);
-            for_each(rightTrees.begin(), rightTrees.end(), deleteTree);
-        }
+        calcBranchLen(root, result);
 
         return result;
     }
-
-    static TreeNode* copyTree(TreeNode* root) {
+private:
+    int calcBranchLen(TreeNode* root, int& diameter) {
         if (!root) {
-            return nullptr;
+            return -1;
         }
 
-        return new TreeNode(root->val, copyTree(root->left), copyTree(root->right));
-    }
+        int leftBranchLen = 1 + calcBranchLen(root->left, diameter);
+        int rightBranchLen = 1 + calcBranchLen(root->right, diameter);
 
-    static void deleteTree(TreeNode* root) {
-        if (!root) {
-            return;
-        }
+        diameter = max(diameter, leftBranchLen + rightBranchLen);
 
-        deleteTree(root->left);
-        deleteTree(root->right);
-
-        delete root;
+        return max(leftBranchLen, rightBranchLen);
     }
 };
+
 
 int main() {
     INIT_TIME(timer);
