14b7dd5730d00112520a3027e7cd3ec735d8eb98
Danil Pinchuk <512kb@proton.me>
2024-07-10 02:27:25 -0400

    95. Unique Binary Search Trees II

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -5,22 +5,69 @@
 
 using namespace std;
 
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+ 
 class Solution {
 public:
-    int numTrees(int n) {
-        vector<int> dp(n + 1, 1);
+    vector<TreeNode*> generateTrees(int n) {
+        return genTrees(1, n);
+    }
+private:
+    vector<TreeNode*> genTrees(int from, int to) {
+        if (from > to)
+            return { nullptr };
 
-        for (int i = 2; i <= n; ++i) {
-            int trees = 0;
-
-            for (int j = 1; j <= i; ++j) {
-                trees += dp[j - 1] * dp[i - j];
-            }
-
-            dp[i] = trees;
+        if (from == to) {
+            return { new TreeNode(from) };
         }
 
-        return dp.back();
+        vector<TreeNode*> result;
+
+        for (int i = from; i <= to; ++i) {
+            auto leftTrees = genTrees(from, i - 1);
+            auto rightTrees = genTrees(i + 1, to);
+
+            int ltsz = leftTrees.size();
+            int rtsz = rightTrees.size();
+
+            for (int lt = 0; lt < ltsz; ++lt) {
+                for (int rt = 0; rt < rtsz; ++rt) {
+                    result.push_back(new TreeNode(i, copyTree(leftTrees[lt]), copyTree(rightTrees[rt])));
+                }
+            }
+
+            for_each(leftTrees.begin(), leftTrees.end(), deleteTree);
+            for_each(rightTrees.begin(), rightTrees.end(), deleteTree);
+        }
+
+        return result;
+    }
+
+    static TreeNode* copyTree(TreeNode* root) {
+        if (!root) {
+            return nullptr;
+        }
+
+        return new TreeNode(root->val, copyTree(root->left), copyTree(root->right));
+    }
+
+    static void deleteTree(TreeNode* root) {
+        if (!root) {
+            return;
+        }
+
+        deleteTree(root->left);
+        deleteTree(root->right);
+
+        delete root;
     }
 };
 
