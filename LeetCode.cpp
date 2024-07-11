fbc53930cf6c0488b0e8d0611925bfa2811f7ab6
Danil Pinchuk <512kb@proton.me>
2024-07-11 02:11:49 -0400

    2466. Count Ways To Build Good Strings

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -5,40 +5,29 @@
 
 using namespace std;
 
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
 class Solution {
 public:
-    int diameterOfBinaryTree(TreeNode* root) {
-        int result = 0;
+    int countGoodStrings(int low, int high, int zero, int one) {
+        const int upperCut = 1000000007;
+        vector<int> dp(high + 1);
+        dp[0] = 1;
+        uint64_t result = 0;
 
-        calcBranchLen(root, result);
-
-        return result;
-    }
-private:
-    int calcBranchLen(TreeNode* root, int& diameter) {
-        if (!root) {
-            return -1;
+        for (int i = 1; i < low; ++i) {
+            int tr = (i >= zero) ? dp[i - zero] : 0;
+            tr += (i >= one) ? dp[i - one] : 0;
+            dp[i] = tr % upperCut;
         }
 
-        int leftBranchLen = 1 + calcBranchLen(root->left, diameter);
-        int rightBranchLen = 1 + calcBranchLen(root->right, diameter);
+        for (int i = low; i <= high; ++i) {
+            int val = (dp[i - zero] + dp[i - one]) % upperCut;
+            dp[i] = val;
+            result += val;
+        }
 
-        diameter = max(diameter, leftBranchLen + rightBranchLen);
-
-        return max(leftBranchLen, rightBranchLen);
+        return result % upperCut;
     }
 };
-
 
 int main() {
     INIT_TIME(timer);
