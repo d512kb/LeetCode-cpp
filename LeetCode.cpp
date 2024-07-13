fd68e511aa8fba3ef9fcfd2e7e60bc60c4bc4f06
Danil Pinchuk <512kb@proton.me>
2024-07-13 05:36:34 -0400

    354. Russian Doll Envelopes

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,14 +7,24 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> countBits(int n) {
-        vector<int> result(n + 1);
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(envelopes.begin(), envelopes.end(), [](const auto& e1, const auto& e2) {
+            return e1[0] == e2[0] ? e1[1] > e2[1] : e1[0] < e2[0];
+        });
+        vector<int> lis;
 
-        for (int i = 1; i <= n; ++i) {
-            result[i] = i % 2 + result[i / 2];
+        for (auto& e : envelopes) {
+            int height = e[1];
+            auto iter = lower_bound(lis.begin(), lis.end(), height);
+
+            if (iter == lis.end()) {
+                lis.push_back(height);
+            } else {
+                *iter = height;
+            }
         }
 
-        return result;
+        return lis.size();
     }
 };
 
