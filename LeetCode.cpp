000735602dfb4cb83ed8ba46fd354b32032b9690
Danil Pinchuk <512kb@proton.me>
2024-07-13 01:38:06 -0400

    338. Counting Bits

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -5,25 +5,16 @@
 
 using namespace std;
 
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
- 
 class Solution {
 public:
-    TreeNode* searchBST(TreeNode* root, int val) {
-        if (!root) { return nullptr; }
+    vector<int> countBits(int n) {
+        vector<int> result(n + 1);
 
-        if (val == root->val) { return root; }
-        if (val < root->val) { return searchBST(root->left, val); }
+        for (int i = 1; i <= n; ++i) {
+            result[i] = i % 2 + result[i / 2];
+        }
 
-        return searchBST(root->right, val);
+        return result;
     }
 };
 
