b2a0b44b5069aab2f47b1e197357198cc9d5bd58
Danil Pinchuk <512kb@proton.me>
2024-07-17 11:49:47 -0400

    455. Assign Cookies

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -5,49 +5,27 @@
 
 using namespace std;
 
-// Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
 class Solution {
 public:
-    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
-        vector<char> toDelete(1000 + 1);
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
 
-        for (int val : to_delete) {
-            toDelete[val] = 1;
-        }
+        int gi = 0;
+        int result = 0;
 
-        vector<TreeNode*> result;
-        if (delNodes(root, toDelete, result)) {
-            result.push_back(root);
+        for (int cookie : s) {
+            if (gi >= g.size()) {
+                return result;
+            }
+
+            if (g[gi] <= cookie) {
+                ++result;
+                ++gi;
+            }
         }
 
         return result;
-    }
-private:
-    TreeNode* delNodes(TreeNode* node, vector<char>& toDelete, vector<TreeNode*>& result) {
-        if (!node)
-            return nullptr;
-
-        node->left = delNodes(node->left, toDelete, result);
-        node->right = delNodes(node->right, toDelete, result);
-
-        if (toDelete[node->val]) {
-            if (node->left) result.push_back(node->left);
-            if (node->right) result.push_back(node->right);
-
-            delete node;
-            return nullptr;
-        }
-
-        return node;
     }
 };
 
