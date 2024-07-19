f5c5d8f085ae1fd447822d9ebe3ee78072962d05
Danil Pinchuk <512kb@proton.me>
2024-07-19 07:01:13 -0400

    1530. Number of Good Leaf Nodes Pairs

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -17,26 +17,59 @@ struct TreeNode {
 
 class Solution {
 public:
-    int rob(TreeNode* root) {
-        return robHelper(root).first;
+    int countPairs(TreeNode* root, int distance) {
+        vector<TreeNode*> currentPath;
+        vector<vector<TreeNode*>> paths;
+
+        calcPaths(root, currentPath, paths);
+        int sz = paths.size();
+        int result = 0;
+
+        for (int i = 0; i < sz; ++i) {
+            for (int j = i + 1; j < sz; ++j) {
+                auto& path1 = paths[i];
+                auto& path2 = paths[j];
+
+                auto mism = mismatch(path1.begin(), path1.end(), path2.begin());
+
+                if (std::distance(mism.first, path1.end()) + std::distance(mism.second, path2.end()) <= distance) {
+                    ++result;
+                }
+            }
+        }
+
+        return result;
     }
+
 private:
-    pair<int, int> robHelper(TreeNode* node) {
+    void calcPaths(TreeNode* node, vector<TreeNode*>& currentPath, vector<vector<TreeNode*>>& paths) {
         if (!node)
-            return { 0, 0 };
+            return;
 
-        auto leftTree = robHelper(node->left);
-        auto rightTree = robHelper(node->right);
+        currentPath.push_back(node);
 
-        int robThis = node->val + leftTree.second + rightTree.second;
-        int robThat = leftTree.first + rightTree.first;
+        if (!node->left && !node->right) {
+            paths.emplace_back(currentPath.begin(), currentPath.end());
+        } else {
+            calcPaths(node->left, currentPath, paths);
+            calcPaths(node->right, currentPath, paths);
+        }
 
-        return { max(robThis, robThat), robThat };
+        currentPath.pop_back();
     }
 };
 
 int main() {
     INIT_TIME(timer);
+
+    TreeNode nodes[]{ TreeNode(0), TreeNode(1), TreeNode(2), TreeNode(3), TreeNode(4) };
+
+    nodes[1].left = &nodes[2];
+    nodes[1].right = &nodes[3];
+    nodes[2].right = &nodes[4];
+
+    Solution sol;
+    sol.countPairs(&nodes[1], 3);
 
     PRINT_ELAPSED(timer);
     return 0;
