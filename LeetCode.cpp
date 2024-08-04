fb747d54d37fa534b40b1e873ee7742f6abad0f2
Danil Pinchuk <512kb@proton.me>
2024-08-04 05:19:59 -0400

    3242. Design Neighbor Sum Service

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -5,28 +5,69 @@
 
 using namespace std;
 
-class Solution {
+class neighborSum {
 public:
-    vector<int> frequencySort(vector<int>& nums) {
-        vector<int> freq(201);
+    neighborSum(vector<vector<int>>& grid) {
+        int size = grid.size();
+        m_adjacentSums.assign(size * size, 0);
+        m_diagonalSums.assign(size * size, 0);
 
-        for (int n : nums) {
-            ++freq[n + 100];
-        }
+        for (int row = 0; row < size; ++row) {
+            for (int col = 0; col < size; ++col) {
+                int adjSum = 0;
+                int diagSum = 0;
+                bool leftColValid = col - 1 >= 0;
+                bool rightColValid = col + 1 < size;
 
-        sort(nums.begin(), nums.end(), [&freq](int a, int b) {
-            int freqA = freq[a + 100];
-            int freqB = freq[b + 100];
+                if (row - 1 >= 0) {
+                    adjSum += grid[row - 1][col];
 
-            if (freqA == freqB) {
-                return b < a;
-            } else {
-                return freqA < freqB;
+                    if (leftColValid) {
+                        diagSum += grid[row - 1][col - 1];
+                    }
+
+                    if (rightColValid) {
+                        diagSum += grid[row - 1][col + 1];
+                    }
+                }
+
+                if (leftColValid) {
+                    adjSum += grid[row][col - 1];
+                }
+
+                if (rightColValid) {
+                    adjSum += grid[row][col + 1];
+                }
+
+                if (row + 1 < size) {
+                    adjSum += grid[row + 1][col];
+
+                    if (leftColValid) {
+                        diagSum += grid[row + 1][col - 1];
+                    }
+
+                    if (rightColValid) {
+                        diagSum += grid[row + 1][col + 1];
+                    }
+                }
+
+                int val = grid[row][col];
+                m_adjacentSums[val] = adjSum;
+                m_diagonalSums[val] = diagSum;
             }
-        });
-
-        return nums;
+        }
     }
+
+    int adjacentSum(int value) {
+        return m_adjacentSums[value];
+    }
+
+    int diagonalSum(int value) {
+        return m_diagonalSums[value];
+    }
+private:
+    vector<int> m_adjacentSums;
+    vector<int> m_diagonalSums;
 };
 
 int main() {
