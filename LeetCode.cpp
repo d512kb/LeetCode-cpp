5d1a7ca4084c402426f2ff4a9793e3c1dbfe27c5
Danil Pinchuk <512kb@proton.me>
2024-08-08 05:48:32 -0400

    518. Coin Change II

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -5,69 +5,20 @@
 
 using namespace std;
 
-class neighborSum {
+class Solution {
 public:
-    neighborSum(vector<vector<int>>& grid) {
-        int size = grid.size();
-        m_adjacentSums.assign(size * size, 0);
-        m_diagonalSums.assign(size * size, 0);
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1);
+        dp[0] = 1;
 
-        for (int row = 0; row < size; ++row) {
-            for (int col = 0; col < size; ++col) {
-                int adjSum = 0;
-                int diagSum = 0;
-                bool leftColValid = col - 1 >= 0;
-                bool rightColValid = col + 1 < size;
-
-                if (row - 1 >= 0) {
-                    adjSum += grid[row - 1][col];
-
-                    if (leftColValid) {
-                        diagSum += grid[row - 1][col - 1];
-                    }
-
-                    if (rightColValid) {
-                        diagSum += grid[row - 1][col + 1];
-                    }
-                }
-
-                if (leftColValid) {
-                    adjSum += grid[row][col - 1];
-                }
-
-                if (rightColValid) {
-                    adjSum += grid[row][col + 1];
-                }
-
-                if (row + 1 < size) {
-                    adjSum += grid[row + 1][col];
-
-                    if (leftColValid) {
-                        diagSum += grid[row + 1][col - 1];
-                    }
-
-                    if (rightColValid) {
-                        diagSum += grid[row + 1][col + 1];
-                    }
-                }
-
-                int val = grid[row][col];
-                m_adjacentSums[val] = adjSum;
-                m_diagonalSums[val] = diagSum;
+        for (int c : coins) {
+            for (int i = c; i <= amount; ++i) {
+                dp[i] += dp[i - c];
             }
         }
-    }
 
-    int adjacentSum(int value) {
-        return m_adjacentSums[value];
+        return dp.back();
     }
-
-    int diagonalSum(int value) {
-        return m_diagonalSums[value];
-    }
-private:
-    vector<int> m_adjacentSums;
-    vector<int> m_diagonalSums;
 };
 
 int main() {
