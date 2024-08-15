944a46b81e1f0441adbafa16648a5b3529f3360f
Danil Pinchuk <512kb@proton.me>
2024-08-15 01:09:09 -0400

    2390. Removing Stars From a String

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,40 +7,19 @@ using namespace std;
 
 class Solution {
 public:
-    bool closeStrings(string word1, string word2) {
-        int word1chars[26];
-        int word2chars[26];
-        memset(word1chars, 0, sizeof(word1chars));
-        memset(word2chars, 0, sizeof(word2chars));
+    string removeStars(string s) {
+        vector<char> stack;
+        stack.reserve(s.size());
 
-        if (word1.size() != word2.size()) {
-            return false;
-        }
-
-        for (int i = 0, sz = word1.size(); i < sz; ++i) {
-            ++word1chars[word1[i] - 'a'];
-            ++word2chars[word2[i] - 'a'];
-        }
-
-        for (int i = 0; i < 26; ++i) {
-            int fromCount = word1chars[i];
-            int toCount = word2chars[i];
-
-            if (toCount != fromCount) {
-                if (toCount == 0 || fromCount == 0) {
-                    return false;
-                }
-
-                auto iter = find(word1chars + i + 1, end(word1chars), toCount);
-                if (iter == end(word1chars)) {
-                    return false;
-                }
-
-                *iter = fromCount;
+        for (char c : s) {
+            if (c == '*') {
+                stack.pop_back();
+            } else {
+                stack.push_back(c);
             }
         }
 
-        return true;
+        return string(stack.begin(), stack.end());
     }
 };
 
