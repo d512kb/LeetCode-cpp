9b70dc064e9827bdc9b60c56c4cc13e41dc52673
Danil Pinchuk <512kb@proton.me>
2024-08-15 01:01:53 -0400

    1657. Determine if Two Strings Are Close

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,30 +7,40 @@ using namespace std;
 
 class Solution {
 public:
-    int equalPairs(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int result = 0;
+    bool closeStrings(string word1, string word2) {
+        int word1chars[26];
+        int word2chars[26];
+        memset(word1chars, 0, sizeof(word1chars));
+        memset(word2chars, 0, sizeof(word2chars));
 
-        map<vector<int>, int> mp;
-
-        for (auto& row : grid) {
-            ++mp[row];
+        if (word1.size() != word2.size()) {
+            return false;
         }
 
-        for (int i = 0; i < n; ++i) {
-            vector<int> col(n);
+        for (int i = 0, sz = word1.size(); i < sz; ++i) {
+            ++word1chars[word1[i] - 'a'];
+            ++word2chars[word2[i] - 'a'];
+        }
 
-            for (int j = 0; j < n; ++j) {
-                col[j] = grid[j][i];
-            }
+        for (int i = 0; i < 26; ++i) {
+            int fromCount = word1chars[i];
+            int toCount = word2chars[i];
 
-            auto iter = mp.find(col);
-            if (iter != mp.end()) {
-                result += iter->second;
+            if (toCount != fromCount) {
+                if (toCount == 0 || fromCount == 0) {
+                    return false;
+                }
+
+                auto iter = find(word1chars + i + 1, end(word1chars), toCount);
+                if (iter == end(word1chars)) {
+                    return false;
+                }
+
+                *iter = fromCount;
             }
         }
 
-        return result;
+        return true;
     }
 };
 
