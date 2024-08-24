076bdad1706d2790d9e28b89c116bfc33691a971
Danil Pinchuk <512kb@proton.me>
2024-08-24 14:24:34 -0400

    2336. Smallest Number in Infinite Set

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -5,58 +5,29 @@
 
 using namespace std;
 
-class Solution {
+class SmallestInfiniteSet {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-
-        queue<pair<int, int>> q;
-        int fresh = 0;
-
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                int g = grid[i][j];
-
-                if (g == 2) {
-                    q.emplace(i, j);
-                } else if (g == 1) {
-                    ++fresh;
-                }
-            }
-        }
-
-        char dirs[]{ -1, 0, 1, 0, -1 };
-        int result = 0;
-
-        while (!q.empty()) {
-            int sz = q.size();
-
-            while (sz--) {
-                auto [row, col] = q.front();
-                q.pop();
-
-                for (int i = 0; i < 4; ++i) {
-                    int newRow = row + dirs[i];
-                    int newCol = col + dirs[i + 1];
-
-                    if (newRow < 0 || newRow >= m || newCol < 0 || newCol >= n) {
-                        continue;
-                    }
-
-                    if (grid[newRow][newCol] == 1) {
-                        grid[newRow][newCol] = 2;
-                        q.emplace(newRow, newCol);
-                        --fresh;
-                    }
-                }
-            }
-
-            result += !q.empty();
-        }
-
-        return fresh > 0 ? -1 : result;
+    SmallestInfiniteSet() : m_first(1) {
     }
+
+    int popSmallest() {
+        if (m_usedNums.empty()) {
+            return m_first++;
+        }
+
+        int s = *m_usedNums.begin();
+        m_usedNums.erase(m_usedNums.begin());
+        return s;
+    }
+
+    void addBack(int num) {
+        if (num < m_first) {
+            m_usedNums.insert(num);
+        }
+    }
+private:
+    set<int> m_usedNums;
+    int m_first;
 };
 
 int main() {
