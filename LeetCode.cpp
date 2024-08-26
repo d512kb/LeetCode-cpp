6f278a10f03860ebd9543f33bc4202d986b6a5ec
Danil Pinchuk <512kb@proton.me>
2024-08-25 21:30:49 -0400

    1318. Minimum Flips to Make a OR b Equal to c

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,29 +7,24 @@ using namespace std;
 
 class Solution {
 public:
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int minK = max(1l, accumulate(piles.begin(), piles.end(), 0l) / h);
-        int maxK = *max_element(piles.begin(), piles.end());
+    int minFlips(int a, int b, int c) {
+        int result = 0;
 
-        while (minK <= maxK) {
-            int k = (minK + maxK) / 2;
-            int hours = 0;
+        while (a || b || c) {
+            char v = (a & 1) + (b & 1);
 
-            for (int p : piles) {
-                hours += (p - 1) / k + 1;
-
-                if (hours > h) {
-                    minK = k + 1;
-                    break;
-                }
+            if (c & 1) {
+                result += !v;
+            } else {
+                result += v;
             }
 
-            if (hours <= h) {
-                maxK = k - 1;
-            }
+            a >>= 1;
+            b >>= 1;
+            c >>= 1;
         }
 
-        return minK;
+        return result;
     }
 };
 
