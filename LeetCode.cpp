a865fc52c9fbd11120b47f6a04b30b362b51982a
Danil Pinchuk <512kb@proton.me>
2024-09-01 00:53:28 -0400

    3274. Check if Two Chessboard Squares Have the Same Color

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,37 +7,8 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> resultsArray(vector<vector<int>>& queries, int k) {
-        if (queries.size() < k) {
-            return vector<int>(queries.size(), -1);
-        }
-
-        priority_queue<int> pq;
-        auto iter = next(queries.begin(), k);
-
-        for_each(queries.begin(), iter, [this, &pq](const auto& vec) { pq.push(calcDistance(vec)); });
-
-        int distance = pq.top();
-        vector<int> result(k - 1, -1);
-        result.push_back(distance);
-
-        for (; iter < queries.end(); ++iter) {
-            int newDistance = calcDistance(*iter);
-
-            if (newDistance < distance) {
-                pq.pop();
-                pq.push(newDistance);
-                distance = pq.top();
-            }
-
-            result.push_back(distance);
-        }
-
-        return result;
-    }
-private:
-    inline int calcDistance(const vector<int>& point) {
-        return abs(point[0]) + abs(point[1]);
+    bool checkTwoChessboards(string coordinate1, string coordinate2) {
+        return 0 == (coordinate1[0] + coordinate1[1] + coordinate2[0] + coordinate2[1]) % 2;
     }
 };
 
