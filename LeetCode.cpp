b469836f59d7fd3fcd46cc0e5f80a7f082cab16f
Danil Pinchuk <512kb@proton.me>
2024-09-06 11:58:04 -0400

    704. Binary Search

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,34 +7,23 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> window;
-        int sz = nums.size();
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size();
 
-        for (int i = 0; i < k; ++i) {
-            while (!window.empty() && nums[window.back()] < nums[i]) {
-                window.pop_back();
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid;
+            } else {
+                return mid;
             }
-
-            window.push_back(i);
         }
 
-        vector<int> result{ nums[window.front()] };
-
-        for (int a = 0, b = k; b < sz; ++a, ++b) {
-            if (window.front() == a) {
-                window.pop_front();
-            }
-
-            while (!window.empty() && nums[window.back()] < nums[b]) {
-                window.pop_back();
-            }
-
-            window.push_back(b);
-            result.push_back(nums[window.front()]);
-        }
-
-        return result;
+        return -1;
     }
 };
 
