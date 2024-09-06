fb3ea7bd75aa392f8fd483e645fa295cd1d65a6b
Danil Pinchuk <512kb@proton.me>
2024-09-06 14:46:05 -0400

    441. Arranging Coins

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,24 +7,24 @@ using namespace std;
 
 class Solution {
 public:
-    bool isPerfectSquare(int num) {
-        int min = 1;
-        int max = 46340;
+    int arrangeCoins(int n) {
+        int left = 1;
+        int right = n;
 
-        while (min <= max) {
-            int mid = min + (max - min) / 2;
-            int val = mid * mid;
+        while (left <= right) {
+            uint64_t mid = left + (right - left) / 2;
+            uint64_t sum = mid * (mid + 1) / 2;
 
-            if (val == num) {
-                return true;
-            } else if (val < num) {
-                min = mid + 1;
+            if (sum < n) {
+                left = mid + 1;
+            } else if (sum > n) {
+                right = mid - 1;
             } else {
-                max = mid - 1;
+                return mid;
             }
         }
 
-        return false;
+        return right;
     }
 };
 
