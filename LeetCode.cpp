3a43a34255df5351961753c89363e4077c474774
Danil Pinchuk <512kb@proton.me>
2024-09-06 11:50:43 -0400

    239. Sliding Window Maximum

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,26 +7,31 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> findAnagrams(string s, string p) {
-        if (p.size() > s.size()) { return {}; }
-        int psz = p.size();
-        int ssz = s.size();
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> window;
+        int sz = nums.size();
 
-        int pFreq[26]{ 0 };
-        for (char c : p) { ++pFreq[c - 'a']; }
-
-        int sFreq[26]{ 0 };
-        for (int i = 0; i < psz - 1; ++i) { ++sFreq[s[i] - 'a']; }
-
-        vector<int> result;
-        for (int a = 0, b = psz - 1; b < ssz; ++a, ++b) {
-            ++sFreq[s[b] - 'a'];
-
-            if (equal(sFreq, sFreq + 26, pFreq)) {
-                result.push_back(a);
+        for (int i = 0; i < k; ++i) {
+            while (!window.empty() && nums[window.back()] < nums[i]) {
+                window.pop_back();
             }
 
-            --sFreq[s[a] - 'a'];
+            window.push_back(i);
+        }
+
+        vector<int> result{ nums[window.front()] };
+
+        for (int a = 0, b = k; b < sz; ++a, ++b) {
+            if (window.front() == a) {
+                window.pop_front();
+            }
+
+            while (!window.empty() && nums[window.back()] < nums[b]) {
+                window.pop_back();
+            }
+
+            window.push_back(b);
+            result.push_back(nums[window.front()]);
         }
 
         return result;
