3ba557c99a94e306868278da1a88be1493c29842
Danil Pinchuk <512kb@proton.me>
2024-09-06 12:57:18 -0400

    278. First Bad Version

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -5,24 +5,25 @@
 
 using namespace std;
 
+// The API isBadVersion is defined for you.
+bool isBadVersion(int version);
+
 class Solution {
 public:
-    int countNegatives(vector<vector<int>>& grid) {
-        int colSz = grid[0].size();
-        int col = 0;
-        int row = grid.size() - 1;
-        int ans = 0;
+    int firstBadVersion(int n) {
+        int start = 1;
 
-        while (row >= 0 && col < colSz) {
-            if (grid[row][col] < 0) {
-                ans += colSz - col;
-                --row;
+        while (start <= n) {
+            int mid = start + (n - start) / 2;
+
+            if (isBadVersion(mid)) {
+                n = mid - 1;
             } else {
-                ++col;
+                start = mid + 1;
             }
         }
 
-        return ans;
+        return start;
     }
 };
 
