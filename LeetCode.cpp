8aff1beb0b8651645a2c4cd4ca5cd31243991e52
Danil Pinchuk <512kb@proton.me>
2024-09-11 01:51:30 -0400

    1498. Number of Subsequences That Satisfy the Given Sum Condition

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,23 +7,29 @@ using namespace std;
 
 class Solution {
 public:
-    int triangleNumber(vector<int>& nums) {
+    int numSubseq(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
-        int sz = nums.size();
+
+        int modNumber = 1000000007;
+        auto start = nums.begin();
+        auto stop = lower_bound(start, nums.end(), 1 + target - *start);
+
+        vector<int> seqs(2 + distance(start, stop));
+        seqs[0] = 0;
+        seqs[1] = 1;
+        int prevTwo = 1;
+
+        for (int i = 2; i < seqs.size(); ++i) {
+            seqs[i] = (seqs[i - 1] + prevTwo) % modNumber;
+            prevTwo = (prevTwo * 2) % modNumber;
+        }
+
         int ans = 0;
+        for (; start < stop && *start < 1 + target - *start; ++start) {
+            stop = lower_bound(start, stop, 1 + target - *start);
 
-        for (int c = 2; c < sz; ++c) {
-            int a = 0;
-            int b = c - 1;
-
-            while (a < b) {
-                if (nums[a] + nums[b] > nums[c]) {
-                    ans += b - a;
-                    --b;
-                } else {
-                    ++a;
-                }
-            }
+            ans += seqs[distance(start, stop)];
+            ans %= modNumber;
         }
 
         return ans;
