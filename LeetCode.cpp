f9a4b2524c4fbc89e6095af55a5f86837d3bf82a
Danil Pinchuk <512kb@proton.me>
2024-09-11 23:50:40 -0400

    802. Find Eventual Safe States

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,28 +7,33 @@ using namespace std;
 
 class Solution {
 public:
-    bool repeatedSubstringPattern(string s) {
-        int sz = s.size();
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        vector<int> result;
 
-        for (int div = 2; div <= sz; ++div) {
-            if (sz % div) { continue; }
-
-            int a = 0;
-            int b = sz / div;
-
-            for (; b < sz; ++a, ++b) {
-                if (s[a] != s[b]) {
-                    break;
-                }
-            }
-
-            if (b == sz) {
-                return true;
+        for (int i = 0; i < graph.size(); ++i) {
+            if (isSafeNode(graph, i)) {
+                result.push_back(i);
             }
         }
 
-        return false;
+        return result;
     }
+private:
+    bool isSafeNode(vector<vector<int>>& graph, int node) {
+        if (m_safeNodes[node] || m_visitedNodes[node]) { return m_safeNodes[node]; }
+        if (graph[node].empty()) { return m_safeNodes[node] = 1; }
+
+        m_visitedNodes[node] = 1;
+
+        for (int n : graph[node]) {
+            if (!isSafeNode(graph, n)) { return false; }
+        }
+
+        return m_safeNodes[node] = 1;
+    }
+
+    bitset<10001> m_safeNodes;
+    bitset<10001> m_visitedNodes;
 };
 
 int main() {
