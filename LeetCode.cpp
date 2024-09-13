1e3cb5ffed55f973e9a13559d22d6671416ebc82
Danil Pinchuk <512kb@proton.me>
2024-09-13 03:30:00 -0400

    1192. Critical Connections in a Network

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,26 +7,47 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<vector<int>> result;
-        vector<int> path{ 0 };
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        vector<vector<int>> graph(n);
 
-        traverse(graph, path, 0, graph.size() - 1, result);
+        for (auto& conn : connections) {
+            graph[conn[0]].push_back(conn[1]);
+            graph[conn[1]].push_back(conn[0]);
+        }
+
+        vector<char> visited(n);
+        vector<int> lowest(n);
+        vector<int> nums(n);
+        int num = 0;
+
+        vector<vector<int>> result;
+        findCycle(graph, -1, 0, num, visited, nums, lowest, result);
 
         return result;
     }
 private:
-    void traverse(vector<vector<int>>& graph, vector<int>& path, int node, int lastNode, vector<vector<int>>& result) {
-        if (node == lastNode) {
-            result.push_back(path);
-            return;
+    int findCycle(vector<vector<int>>& graph, int prevNode, int node, int& num, vector<char>& visited, vector<int>& nums, vector<int>& lowest, vector<vector<int>>& result) {
+        visited[node] = 1;
+        ++num;
+        nums[node] = num;
+        lowest[node] = num;
+        int& low = lowest[node];
+
+        for (int near : graph[node]) {
+            if (near == prevNode) { continue; }
+
+            if (visited[near]) {
+                low = min(low, nums[near]);
+            } else {
+                low = min(low, findCycle(graph, node, near, num, visited, nums, lowest, result));
+            }
+
+            if (nums[node] < lowest[near]) {
+                result.push_back({ node, near });
+            }
         }
 
-        for (int sub : graph[node]) {
-            path.push_back(sub);
-            traverse(graph, path, sub, lastNode, result);
-            path.pop_back();
-        }
+        return low;
     }
 };
 
