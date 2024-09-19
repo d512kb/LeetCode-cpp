c29fe875429ee4b91127e84368690b8df36d672b
Danil Pinchuk <512kb@proton.me>
2024-09-19 07:21:22 -0400

    997. Find the Town Judge

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,52 +7,22 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        int rows = heights.size();
-        int cols = heights[0].size();
-        vector<vector<int>> result;
-        vector<vector<char>> dp(rows, vector<char>(cols));
-        char dirs[]{ -1, 0, 1, 0, -1 };
-        queue<tuple<int, int, char>> q;
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> trusts(n + 1);
+        vector<int> trustedBy(n + 1);
 
-        for (int row = 0; row < rows; ++row) {
-            q.emplace(row, -1, 0b01);
-            q.emplace(row, cols, 0b10);
-        }
-        for (int col = 0; col < cols; ++col) {
-            q.emplace(-1, col, 0b01);
-            q.emplace(rows, col, 0b10);
+        for (auto& t : trust) {
+            ++trusts[t[0]];
+            ++trustedBy[t[1]];
         }
 
-        while (!q.empty()) {
-            int sz = q.size();
-
-            while (sz--) {
-                auto [row, col, ocean] = q.front();
-                q.pop();
-                int height = 0;
-
-                if (row >= 0 && row < rows && col >= 0 && col < cols) { height = heights[row][col]; }
-
-                for (int i = 0; i < 4; ++i) {
-                    int newRow = row + dirs[i];
-                    int newCol = col + dirs[i + 1];
-
-                    if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) {
-                        if (dp[newRow][newCol] & ocean) { continue; }
-
-                        if (height <= heights[newRow][newCol]) {
-                            q.emplace(newRow, newCol, ocean);
-                            dp[newRow][newCol] |= ocean;
-
-                            if (dp[newRow][newCol] == 0b11) { result.push_back({ newRow, newCol }); }
-                        }
-                    }
-                }
+        for (int i = 1; i <= n; ++i) {
+            if (trusts[i] == 0 && trustedBy[i] == n - 1) {
+                return i;
             }
         }
 
-        return result;
+        return -1;
     }
 };
 
