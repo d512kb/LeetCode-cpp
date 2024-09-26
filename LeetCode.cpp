3de2e25fb74e3f9495ef3f6f5bb770a12772d7d3
Danil Pinchuk <512kb@proton.me>
2024-09-26 05:09:37 -0400

    567. Permutation in String

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,32 +7,27 @@ using namespace std;
 
 class Solution {
 public:
-    string multiply(string num1, string num2) {
-        if (num1 == "0" || num2 == "0") { return "0"; }
+    bool checkInclusion(string s1, string s2) {
+        if (s1.size() > s2.size()) { return false; }
 
-        if (num2.size() > num1.size()) { swap(num1, num2); }
-        string result(distance(num1.rbegin(), num1.rend()), '0');
-        int resultIndex = 0;
+        vector<int> freqA(26);
+        vector<int> freqB(26);
 
-        for (auto secondIter = num2.rbegin(); secondIter != num2.rend(); ++secondIter, ++resultIndex) {
-            if (result.size() - resultIndex < num1.size()) { result.push_back('0'); }
-            int writeToIndex = resultIndex;
-            char carry = 0;
-
-            for (auto firstIter = num1.rbegin(); firstIter != num1.rend(); ++firstIter, ++writeToIndex) {
-                int a = *firstIter - '0';
-                int b = *secondIter - '0';
-                int c = result[writeToIndex] - '0';
-
-                int mul = a * b + c + carry;
-                result[writeToIndex] = (mul % 10) + '0';
-                carry = mul / 10;
-            }
-
-            if (carry) { result.push_back(carry + '0'); }
+        for (int i = 0; i < s1.size(); ++i) {
+            ++freqA[s1[i] - 'a'];
+            ++freqB[s2[i] - 'a'];
         }
 
-        return string(result.rbegin(), result.rend());
+        if (freqA == freqB) { return true; }
+
+        for (int left = 0, right = s1.size(); right < s2.size(); ++left, ++right) {
+            --freqB[s2[left] - 'a'];
+            ++freqB[s2[right] - 'a'];
+
+            if (freqA == freqB) { return true; }
+        }
+
+        return false;
     }
 };
 
