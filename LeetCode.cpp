3004a9e4ad583ec8f04231eeb9028b36e7a30c55
Danil Pinchuk <512kb@proton.me>
2024-09-26 04:53:50 -0400

    43. Multiply Strings

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,34 +7,38 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(begin(candidates), end(candidates));
+    string multiply(string num1, string num2) {
+        if (num1 == "0" || num2 == "0") { return "0"; }
 
-        vector<vector<int>> result;
-        vector<int> comb;
-        calcSum(candidates, candidates.begin(), target, comb, result);
-        return result;
-    }
-private:
-    void calcSum(vector<int>& candidates, vector<int>::iterator from, int target, vector<int>& comb, vector<vector<int>>& result) {
-        if (target <= 0) { if (target == 0) { result.push_back(comb); } return; }
-        if (from == candidates.end()) { return; }
+        if (num2.size() > num1.size()) { swap(num1, num2); }
+        string result(distance(num1.rbegin(), num1.rend()), '0');
+        int resultIndex = 0;
 
-        comb.push_back(*from);
-        calcSum(candidates, from + 1, target - *from, comb, result);
-        comb.pop_back();
+        for (auto secondIter = num2.rbegin(); secondIter != num2.rend(); ++secondIter, ++resultIndex) {
+            if (result.size() - resultIndex < num1.size()) { result.push_back('0'); }
+            int writeToIndex = resultIndex;
+            char carry = 0;
 
-        auto nextCandidate = upper_bound(from, candidates.end(), *from);
+            for (auto firstIter = num1.rbegin(); firstIter != num1.rend(); ++firstIter, ++writeToIndex) {
+                int a = *firstIter - '0';
+                int b = *secondIter - '0';
+                int c = result[writeToIndex] - '0';
 
-        if (nextCandidate != candidates.end()) {
-            calcSum(candidates, nextCandidate, target, comb, result);
+                int mul = a * b + c + carry;
+                result[writeToIndex] = (mul % 10) + '0';
+                carry = mul / 10;
+            }
+
+            if (carry) { result.push_back(carry + '0'); }
         }
+
+        return string(result.rbegin(), result.rend());
     }
 };
 
 int main() {
     INIT_TIME(timer);
-    
+
     PRINT_ELAPSED(timer);
     return 0;
 }
