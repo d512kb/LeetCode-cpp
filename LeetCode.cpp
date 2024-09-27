bb08fd65de16743d3d2431c03b9f67f77f7afd1c
Danil Pinchuk <512kb@proton.me>
2024-09-27 03:19:36 -0400

    1203. Sort Items by Groups Respecting Dependencies

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,27 +7,78 @@ using namespace std;
 
 class Solution {
 public:
-    bool checkInclusion(string s1, string s2) {
-        if (s1.size() > s2.size()) { return false; }
+    vector<int> sortItems(int n, int m, vector<int>& group, vector<vector<int>>& beforeItems) {
+        vector<vector<int>> groupsDependents(m + 1);
+        vector<int> groupsDepsCount(m + 1);
+        vector<vector<int>> itemsDependents(n);
+        vector<int> itemsDepsCount(n);
 
-        vector<int> freqA(26);
-        vector<int> freqB(26);
+        vector<queue<int>> queues(m + 1);
 
-        for (int i = 0; i < s1.size(); ++i) {
-            ++freqA[s1[i] - 'a'];
-            ++freqB[s2[i] - 'a'];
+        for (int i = 0; i < n; ++i) {
+            if (beforeItems[i].empty()) {
+                queues[group[i] + 1].push(i);
+            } else for (int req : beforeItems[i]) {
+                itemsDependents[req].push_back(i);
+                ++itemsDepsCount[i];
+
+                int currGroup = group[i] + 1;
+                int reqGroup = group[req] + 1;
+
+                if (currGroup > 0 && reqGroup > 0 && currGroup != reqGroup) {
+                    groupsDependents[reqGroup].push_back(currGroup);
+                    ++groupsDepsCount[currGroup];
+                }
+            }
         }
 
-        if (freqA == freqB) { return true; }
+        queue<int> groupsQueue;
+        vector<int> result;
 
-        for (int left = 0, right = s1.size(); right < s2.size(); ++left, ++right) {
-            --freqB[s2[left] - 'a'];
-            ++freqB[s2[right] - 'a'];
-
-            if (freqA == freqB) { return true; }
+        for (int i = 0; i < m + 1; ++i) {
+            if (groupsDepsCount[i] == 0) {
+                groupsQueue.push(i);
+            }
         }
 
-        return false;
+        while (!groupsQueue.empty()) {
+            int sz = groupsQueue.size();
+
+            while (sz--) {
+                int g = groupsQueue.front();
+                groupsQueue.pop();
+
+                for (int gDep : groupsDependents[g]) {
+                    if (--groupsDepsCount[gDep] == 0) {
+                        groupsQueue.push(gDep);
+                    }
+                }
+
+                auto& q = queues[g];
+
+                while (!q.empty()) {
+                    int sz = q.size();
+
+                    while (sz--) {
+                        int item = q.front();
+                        q.pop();
+
+                        result.push_back(item);
+
+                        for (int iDep : itemsDependents[item]) {
+                            if (--itemsDepsCount[iDep] == 0) {
+                                int g = group[iDep] + 1;
+
+                                queues[g].push(iDep);
+                                if (g == 0) { groupsQueue.push(g); }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return result.size() == n ? result : vector<int>();
     }
 };
 
