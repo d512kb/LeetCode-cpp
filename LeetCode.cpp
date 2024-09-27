e24a637c81a77dd368e110fa59c417e2ff318cc1
Danil Pinchuk <512kb@proton.me>
2024-09-27 07:15:04 -0400

    217. Contains Duplicate

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,20 +7,14 @@ using namespace std;
 
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        int sz = nums.size();
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> st;
 
-        int dp1[2]{ 0, 0 };
-        int dp2[2]{ 0, 0 };
-
-        dp1[1] = nums[0];
-        for (int i = 1; i < sz - 1; ++i) {
-            dp1[0] = exchange(dp1[1], max(nums[i] + dp1[0], dp1[1]));
-            dp2[0] = exchange(dp2[1], max(nums[i] + dp2[0], dp2[1]));
+        for (int n : nums) {
+            if (!st.insert(n).second) { return true; }
         }
-        dp2[1] = max(nums.back() + dp2[0], dp2[1]);
 
-        return max(dp1[1], dp2[1]);
+        return false;
     }
 };
 
