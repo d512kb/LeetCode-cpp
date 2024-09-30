693a73e4625c186a40fa6b4532795862c5430dc0
Danil Pinchuk <512kb@proton.me>
2024-09-30 02:49:34 -0400

    973. K Closest Points to Origin

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,22 +7,29 @@ using namespace std;
 
 class Solution {
 public:
-    int firstMissingPositive(vector<int>& nums) {
-        int sz = nums.size();
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        vector<vector<int>> result;
 
-        for (int i = 1; i <= sz; ++i) {
-            int& n = nums[i - 1];
+        int i = 0;
+        for (; i < k; ++i) {
+            result.push_back(move(points[i]));
+        }
 
-            while (n != i && n > 0 && n <= sz && nums[n - 1] != n) {
-                swap(n, nums[n - 1]);
+        auto lmb = [](auto& v1, auto& v2) {
+            return sqrt(v1[0] * v1[0] + v1[1] * v1[1]) < sqrt(v2[0] * v2[0] + v2[1] * v2[1]);
+        };
+
+        make_heap(begin(result), end(result), lmb);
+
+        for (; i < points.size(); ++i) {
+            if (lmb(points[i], result.front())) {
+                pop_heap(begin(result), end(result), lmb);
+                swap(result.back(), points[i]);
+                push_heap(begin(result), end(result), lmb);
             }
         }
 
-        for (int i = 1; i <= sz; ++i) {
-            if (nums[i - 1] != i) { return i; }
-        }
-
-        return sz + 1;
+        return result;
     }
 };
 
