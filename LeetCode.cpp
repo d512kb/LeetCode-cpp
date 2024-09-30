b715228bac2112ee19e0bf8566d8e06b7cc3b37a
Danil Pinchuk <512kb@proton.me>
2024-09-30 00:27:07 -0400

    31. Next Permutation

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,32 +7,13 @@ using namespace std;
 
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        int sz = heights.size();
-        vector<pair<int, int>> stack;
-        int result = 0;
-
-        for (int i = 0; i < sz; ++i) {
-            int h = heights[i];
-            int newStart = i;
-
-            while (!stack.empty() && stack.back().first >= h) {
-                auto& prevH = stack.back();
-                result = max(result, prevH.first * (i - prevH.second));
-                newStart = prevH.second;
-                stack.pop_back();
-            }
-
-            stack.emplace_back(h, newStart);
-        };
-
-        while (!stack.empty()) {
-            auto& prevH = stack.back();
-            result = max(result, prevH.first * (sz - prevH.second));
-            stack.pop_back();
+    void nextPermutation(vector<int>& nums) {
+        auto seqEdge = is_sorted_until(rbegin(nums), rend(nums));
+        if (seqEdge != rend(nums)) {
+            iter_swap(seqEdge, upper_bound(rbegin(nums), seqEdge, *seqEdge));
         }
 
-        return result;
+        reverse(seqEdge.base(), end(nums));
     }
 };
 
