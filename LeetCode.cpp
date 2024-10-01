354e9dc154ab30ab0f9351afcc93adeb466cbbd2
Danil Pinchuk <512kb@proton.me>
2024-10-01 10:26:13 -0400

    463. Island Perimeter

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,13 +7,30 @@ using namespace std;
 
 class Solution {
 public:
-    int getSum(int a, int b) {
-        while (b != 0) {
-            int carry = a & b;
-            a ^= b;
-            b = carry << 1;
+    int islandPerimeter(vector<vector<int>>& grid) {
+        char dirs[]{ -1, 0, 1, 0, -1 };
+        int rows = grid.size();
+        int cols = grid[0].size();
+        int result = 0;
+
+        for (int row = 0; row < rows; ++row) {
+            for (int col = 0; col < cols; ++col) {
+                if (grid[row][col] == 1) {
+                    for (int i = 0; i < 4; ++i) {
+                        int newRow = row + dirs[i];
+                        int newCol = col + dirs[i + 1];
+
+                        if (newRow < 0 || newRow >= rows || newCol < 0 || newCol >= cols) {
+                            ++result;
+                        } else if (grid[newRow][newCol] == 0) {
+                            ++result;
+                        }
+                    }
+                }
+            }
         }
-        return a;
+
+        return result;
     }
 };
 
