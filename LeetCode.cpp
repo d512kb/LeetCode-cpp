d3bcf96332225330ce35d0dbe332318bef46d3e4
Danil Pinchuk <512kb@proton.me>
2024-10-04 16:02:09 -0400

    703. Kth Largest Element in a Stream

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -5,37 +5,26 @@
 
 using namespace std;
 
-class Solution {
+class KthLargest {
 public:
-    int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-        vector<vector<int>> paths(rows, vector<int>(cols));
-
-        int result = 0;
-        for (int row = 0; row < rows; ++row) {
-            for (int col = 0; col < cols; ++col) {
-                result = max(result, dfs(matrix, paths, row, col, -1));
-            }
+    KthLargest(int k, vector<int>& nums) {
+        for (int n : nums) {
+            minHeap.push(n);
+            if (minHeap.size() > k) { minHeap.pop(); }
         }
 
-        return result;
+        if (k > nums.size()) { minHeap.push(numeric_limits<int>::min()); }
+    }
+
+    int add(int val) {
+        if (val > minHeap.top()) {
+            minHeap.pop();
+            minHeap.push(val);
+        }
+        return minHeap.top();
     }
 private:
-    int dfs(vector<vector<int>>& matrix, vector<vector<int>>& paths, int row, int col, int prev) {
-        if (row < 0 || row == matrix.size() || col < 0 || col == matrix[0].size()) { return 0; }
-        if (matrix[row][col] <= prev) { return 0; }
-        if (paths[row][col] != 0) { return paths[row][col]; }
-
-        int currValue = matrix[row][col];
-
-        int len = max(max(max(dfs(matrix, paths, row - 1, col, currValue),
-                              dfs(matrix, paths, row, col + 1, currValue)),
-                          dfs(matrix, paths, row + 1, col, currValue)),
-                      dfs(matrix, paths, row, col - 1, currValue));
-
-        return paths[row][col] = len + 1;
-    }
+    priority_queue<int, vector<int>, greater<>> minHeap;
 };
 
 int main() {
