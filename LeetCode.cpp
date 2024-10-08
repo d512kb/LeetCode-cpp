06ee014ca11ce64336ce99217cd04c3322a3448c
Danil Pinchuk <512kb@proton.me>
2024-10-08 06:11:21 -0400

    572. Subtree of Another Tree

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -5,46 +5,29 @@
 
 using namespace std;
 
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
-    vector<string> findItinerary(vector<vector<string>>& tickets) {
-        auto hashAeroport = [](const string& s) -> size_t { return ((s[0] - 'A') << 10) + ((s[1] - 'A') << 5) + (s[2] - 'A'); };
-        auto hashTicket = [&hashAeroport](const string& from, const string& to) -> size_t {
-            return (hashAeroport(from) << 15) + hashAeroport(to);
-        };
-        unordered_map<string, set<string>, decltype(hashAeroport)> graph;
-        unordered_map<size_t, int> totalTickets; // we will use hashTicket manually
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (!root) { return false; }
 
-        for (auto& ticket : tickets) {
-            graph[ticket[0]].insert(ticket[1]);
-            ++totalTickets[hashTicket(ticket[0], ticket[1])];
-        }
+        return isIdentical(root, subRoot) || isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+    }
+private:
+    bool isIdentical(TreeNode* tree1, TreeNode* tree2) {
+        if (!tree1) { return !tree2; }
+        if (!tree2) { return false; }
 
-        vector<string> result;
-        auto dfs = [&](auto&& self, string from, vector<string>& path) -> void {
-            if (!result.empty()) { return; }
-            if (path.size() == tickets.size() + 1) {
-                result = path;
-                return;
-            }
-
-            for (auto& to : graph[from]) {
-                int& haveTickets = totalTickets[hashTicket(from, to)];
-
-                if (haveTickets) {
-                    path.push_back(to);
-                    --haveTickets;
-                    self(self, to, path);
-                    path.pop_back();
-                    ++haveTickets;
-                }
-            }
-        };
-
-        vector<string> path{ "JFK" };
-        dfs(dfs, "JFK", path);
-
-        return result;
+        return tree1->val == tree2->val && isIdentical(tree1->left, tree2->left) && isIdentical(tree1->right, tree2->right);
     }
 };
 
