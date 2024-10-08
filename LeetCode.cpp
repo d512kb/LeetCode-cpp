30bb6e77ba3b312a79debe831c4f1bf72fed5d86
Danil Pinchuk <512kb@proton.me>
2024-10-08 03:19:08 -0400

    332. Reconstruct Itinerary

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -5,26 +5,47 @@
 
 using namespace std;
 
-class KthLargest {
+class Solution {
 public:
-    KthLargest(int k, vector<int>& nums) {
-        for (int n : nums) {
-            minHeap.push(n);
-            if (minHeap.size() > k) { minHeap.pop(); }
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        auto hashAeroport = [](const string& s) -> size_t { return ((s[0] - 'A') << 10) + ((s[1] - 'A') << 5) + (s[2] - 'A'); };
+        auto hashTicket = [&hashAeroport](const string& from, const string& to) -> size_t {
+            return (hashAeroport(from) << 15) + hashAeroport(to);
+        };
+        unordered_map<string, set<string>, decltype(hashAeroport)> graph;
+        unordered_map<size_t, int> totalTickets; // we will use hashTicket manually
+
+        for (auto& ticket : tickets) {
+            graph[ticket[0]].insert(ticket[1]);
+            ++totalTickets[hashTicket(ticket[0], ticket[1])];
         }
 
-        if (k > nums.size()) { minHeap.push(numeric_limits<int>::min()); }
-    }
+        vector<string> result;
+        auto dfs = [&](auto&& self, string from, vector<string>& path) -> void {
+            if (!result.empty()) { return; }
+            if (path.size() == tickets.size() + 1) {
+                result = path;
+                return;
+            }
 
-    int add(int val) {
-        if (val > minHeap.top()) {
-            minHeap.pop();
-            minHeap.push(val);
-        }
-        return minHeap.top();
+            for (auto& to : graph[from]) {
+                int& haveTickets = totalTickets[hashTicket(from, to)];
+
+                if (haveTickets) {
+                    path.push_back(to);
+                    --haveTickets;
+                    self(self, to, path);
+                    path.pop_back();
+                    ++haveTickets;
+                }
+            }
+        };
+
+        vector<string> path{ "JFK" };
+        dfs(dfs, "JFK", path);
+
+        return result;
     }
-private:
-    priority_queue<int, vector<int>, greater<>> minHeap;
 };
 
 int main() {
