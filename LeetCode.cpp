17c2111ebc30c1df930afa1a679e2065126531fb
Danil Pinchuk <512kb@proton.me>
2024-10-23 03:10:04 -0400

    3324. Find the Sequence of Strings Appeared on the Screen

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,59 +7,21 @@ using namespace std;
 
 class Solution {
 public:
-    vector<long long> findXSum(vector<int>& nums, int k, int x) {
-        set<pair<int, int>, greater<>> freqMaxSet;
-        set<pair<int, int>> freqMinSet;
-        unordered_map<int, int> freqs;
-        int64_t sum = 0;
+    vector<string> stringSequence(string target) {
+        vector<string> result;
+        string str;
 
-        for (int i = 0; i < k; ++i) { ++freqs[nums[i]]; }
-        for (const auto& s : freqs) { freqMaxSet.emplace(s.second, s.first); }
-        for (int i = 0; i < x && !freqMaxSet.empty(); ++i) {
-            auto node = freqMaxSet.extract(freqMaxSet.begin());
-            sum += static_cast<int64_t>(node.value().first) * node.value().second;
-            freqMinSet.insert(std::move(node));
-        }
+        for (char targetChar : target) {
+            str.push_back('a');
 
-        vector<long long> result;
-        result.push_back(sum);
+            for (char c = 'a'; c <= 'z'; ++c) {
+                str.back() = c;
+                result.push_back(str);
 
-        for (int prev = 0, next = k; next < nums.size(); ++prev, ++next) {
-            auto prevNum = make_pair(freqs[nums[prev]]--, nums[prev]);
-            auto nextNum = make_pair(freqs[nums[next]]++, nums[next]);
-
-            auto iter = freqMinSet.find(prevNum);
-            if (iter != freqMinSet.end()) {
-                sum -= static_cast<int64_t>(prevNum.first) * prevNum.second;
-                freqMinSet.erase(iter);
-            } else {
-                freqMaxSet.erase(prevNum);
+                if (c == targetChar) {
+                    break;
+                }
             }
-
-            freqMaxSet.emplace(prevNum.first - 1, prevNum.second);
-            if (freqMinSet.size() < x) {
-                auto node = freqMaxSet.extract(freqMaxSet.begin());
-                sum += static_cast<int64_t>(node.value().first) * node.value().second;
-                freqMinSet.insert(std::move(node));
-            }
-
-            iter = freqMinSet.find(nextNum);
-            if (iter != freqMinSet.end()) {
-                sum -= static_cast<int64_t>(nextNum.first) * nextNum.second;
-                freqMinSet.erase(iter);
-            } else {
-                freqMaxSet.erase(nextNum);
-            }
-
-            freqMinSet.emplace(nextNum.first + 1, nextNum.second);
-            sum += static_cast<int64_t>(nextNum.first + 1) * nextNum.second;
-            if (freqMinSet.size() > x) {
-                auto node = freqMinSet.extract(freqMinSet.begin());
-                sum -= static_cast<int64_t>(node.value().first) * node.value().second;
-                freqMaxSet.insert(std::move(node));
-            }
-
-            result.push_back(sum);
         }
 
         return result;
