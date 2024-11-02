08de53d8efd88b9cd7ec92a9bd34af8f41bfa757
Danil Pinchuk <512kb@proton.me>
2024-11-02 02:17:52 -0400

    3327. Check if DFS Strings Are Palindromes

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,42 +6,54 @@
 using namespace std;
 
 class Solution {
-    unordered_map<int, int> divisors;
 public:
-    int minOperations(vector<int>& nums) {
-        int ans = 0;
-        int nextVal = nums.back();
+    vector<bool> findAnswer(vector<int>& parent, string s) {
+        int sz = parent.size();
+        vector<vector<int>> nodes(sz);
 
-        for (int i = nums.size() - 2; i >= 0; --i) {
-            int& n = nums[i];
+        for (int i = 1; i < sz; ++i) { nodes[parent[i]].push_back(i); }
 
-            while (n > nextVal) {
-                int div = getDivisor(n);
-                if (div == n) { return -1; }
+        string tmpStr;
+        vector<pair<int, int>> strIndexes(sz);
+        auto dfs = [&](auto& self, int x) -> void {
+            strIndexes[x].first = tmpStr.size();
+            for (int child : nodes[x]) { self(self, child); }
+            tmpStr += s[x];
+            strIndexes[x].second = tmpStr.size();
+        };
 
-                n /= div;
-                ++ans;
+        dfs(dfs, 0);
+
+        int uniSz = 2 * sz + 1;
+        string uniStr("^");
+        uniStr.reserve(uniSz + 2);
+
+        for (char c : tmpStr) { uniStr += '#'; uniStr += c; }
+        uniStr += "#$";
+
+        vector<int> wingSizes(uniSz);
+        int start = 0; int stop = 0;
+        for (int i = 2; i < uniSz; ++i) {
+            int pal = 1;
+            if (i < stop) { pal = min(wingSizes[2 * start - i], stop - i); }
+
+            while (uniStr[i - pal] == uniStr[i + pal]) { ++pal; }
+
+            if (i + pal > stop) {
+                start = i;
+                stop = i + pal;
             }
 
-            nextVal = n;
+            wingSizes[i] = pal;
         }
 
-        return ans;
-    }
-private:
-    int getDivisor(int n) {
-        if (divisors[n] == 0) {
-            divisors[n] = n;
-
-            for (int i = n / 2; i > 1; --i) {
-                if (n % i == 0) {
-                    divisors[n] = i;
-                    return i;
-                }
-            }
+        vector<bool> result;
+        result.reserve(sz);
+        for (auto& strIndex : strIndexes) {
+            result.push_back(wingSizes[strIndex.first + strIndex.second + 1] > strIndex.second - strIndex.first);
         }
 
-        return divisors[n];
+        return result;
     }
 };
 
