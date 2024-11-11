531beb058465f1d25944c137f9af48297d3de7b8
Danil Pinchuk <512kb@proton.me>
2024-11-11 00:03:22 -0500

    3340. Check Balanced String

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,32 +7,19 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthAfterTransformations(string s, int t) {
-        int mod = 1e9 + 7;
-        vector<int> counts(26);
-        vector<int> countsPrev(26);
+    bool isBalanced(string num) {
+        int evenSum = 0;
+        int oddSum = 0;
 
-        for (char c : s) { ++counts[c - 'a']; }
-        for (int i = 0; i < t; ++i) {
-            swap(counts, countsPrev);
-
-            for (int c = 0; c < 25; ++c) {
-                counts[c + 1] = (counts[c + 1] + countsPrev[c]) % mod;
-                countsPrev[c] = 0;
+        for (int i = 0; i < num.size(); ++i) {
+            if (i % 2) {
+                oddSum += num[i] - '0';
+            } else {
+                evenSum += num[i] - '0';
             }
-
-            counts[0] = (counts[0] + countsPrev[25]) % mod;
-            counts[1] = (counts[1] + countsPrev[25]) % mod;
-            countsPrev[25] = 0;
         }
 
-        int ans = 0;
-
-        for (int i = 0; i < 26; ++i) {
-            ans = (ans + counts[i]) % mod;
-        }
-
-        return ans;
+        return evenSum == oddSum;
     }
 };
 
