0e1b98c82028ed6bd91fb7395703ffaca000cc7c
Danil Pinchuk <512kb@proton.me>
2024-11-12 01:39:50 -0500

    3341. Find Minimum Time to Reach Last Room I

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,20 +6,49 @@
 using namespace std;
 
 class Solution {
-public:
-    bool isBalanced(string num) {
-        int evenSum = 0;
-        int oddSum = 0;
+    struct Cell {
+        int row;
+        int col;
+        int time;
 
-        for (int i = 0; i < num.size(); ++i) {
-            if (i % 2) {
-                oddSum += num[i] - '0';
-            } else {
-                evenSum += num[i] - '0';
+        friend bool operator>(const Cell& c1, const Cell& c2) {
+            if (c1.time != c2.time) { return c1.time > c2.time; }
+            if (c1.row != c2.row) { return c1.row > c2.row; }
+            return c1.col > c2.col;
+        }
+    };
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        int rows = moveTime.size();
+        int cols = moveTime[0].size();
+
+        priority_queue<Cell, vector<Cell>, greater<Cell>> q;
+        vector<vector<int>> cellTimes(rows, vector<int>(cols, numeric_limits<int>::max()));
+        char dirs[]{ 1, 0, -1, 0, 1 };
+        q.emplace(0, 0, 0);
+
+        while (!q.empty()) {
+            auto cell = q.top();
+            q.pop();
+
+            if (cell.row == rows - 1 && cell.col == cols - 1) { return cell.time; }
+
+            for (int i = 0; i < 4; ++i) {
+                int newRow = cell.row + dirs[i];
+                int newCol = cell.col + dirs[i + 1];
+
+                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) {
+                    int timeToVisit = 1 + max(cell.time, moveTime[newRow][newCol]);
+
+                    if (timeToVisit < cellTimes[newRow][newCol]) {
+                        cellTimes[newRow][newCol] = timeToVisit;
+                        q.emplace(newRow, newCol, timeToVisit);
+                    }
+                }
             }
         }
 
-        return evenSum == oddSum;
+        return -1;
     }
 };
 
