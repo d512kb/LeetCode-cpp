d4da94e98ae347e9adbbdd5c20c55ff467e9be69
Danil Pinchuk <512kb@proton.me>
2024-12-01 05:32:04 -0500

    743. Network Delay Time

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,22 +7,34 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        vector<int> points(1001);
-        iota(points.begin(), points.end(), 0);
-        vector<int> result;
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<pair<int, int>>> graph(n + 1);
 
-        for (auto& edge : edges) {
-            int p1 = edge[0];
-            int p2 = edge[1];
+        for (auto& time : times) { graph[time[0]].emplace_back(time[2], time[1]); }
 
-            while (p1 != points[p1]) { p1 = points[p1]; }
-            while (p2 != points[p2]) { p2 = points[p2]; }
+        vector<int> costs(n + 1, numeric_limits<int>::max());
+        costs[0] = 0; // unused
+        costs[k] = 0; // start
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+        pq.emplace(0, k);
 
-            if (p1 == p2) { result = edge; } else { points[max(p1, p2)] = min(p1, p2); }
+        while (!pq.empty()) {
+            auto p = pq.top();
+            pq.pop();
+
+            for (auto& n : graph[p.second]) {
+                int travelCost = p.first + n.first;
+
+                if (travelCost < costs[n.second]) {
+                    costs[n.second] = travelCost;
+                    pq.emplace(travelCost, n.second);
+                }
+            }
         }
 
-        return result;
+        int ans = *max_element(costs.begin(), costs.end());
+
+        return ans == numeric_limits<int>::max() ? -1 : ans;
     }
 };
 
