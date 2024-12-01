b09408b4a923b5fa1d569d0209798af6150ea4ca
Danil Pinchuk <512kb@proton.me>
2024-12-01 04:37:55 -0500

    684. Redundant Connection

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,51 +7,22 @@ using namespace std;
 
 class Solution {
 public:
-    int minCostConnectPoints(vector<vector<int>>& points) {
-        if (points.size() <= 1) { return 0; }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        vector<int> points(1001);
+        iota(points.begin(), points.end(), 0);
+        vector<int> result;
 
-        using Connection = pair<int, pair<int, int>>;
-        vector<Connection> graph;
-        graph.reserve((points.size() - 1) * points.size() / 2);
+        for (auto& edge : edges) {
+            int p1 = edge[0];
+            int p2 = edge[1];
 
-        for (int i = 0; i < points.size(); ++i) {
-            for (int j = i + 1; j < points.size(); ++j) {
-                graph.emplace_back(calcDistance(points[i], points[j]), make_pair(i, j));
-            }
+            while (p1 != points[p1]) { p1 = points[p1]; }
+            while (p2 != points[p2]) { p2 = points[p2]; }
+
+            if (p1 == p2) { result = edge; } else { points[max(p1, p2)] = min(p1, p2); }
         }
 
-        vector<int> pointsChains(points.size());
-        iota(pointsChains.begin(), pointsChains.end(), 0);
-
-        auto cmp = [](const Connection& conn1, const Connection& conn2) { return conn1.first > conn2.first; };
-        make_heap(graph.begin(), graph.end(), cmp);
-        int connections = 0;
-        int ans = 0;
-
-        while (connections < points.size() - 1) {
-            pop_heap(graph.begin(), graph.end(), cmp);
-
-            auto [p1, p2] = graph.back().second;
-            int cost = graph.back().first;
-            graph.pop_back();
-
-            while (p1 != pointsChains[p1]) { p1 = pointsChains[p1]; }
-            while (p2 != pointsChains[p2]) { p2 = pointsChains[p2]; }
-
-            if (p1 != p2) {
-                if (p1 > p2) { swap(p1, p2); }
-
-                pointsChains[p2] = p1;
-                ans += cost;
-                ++connections;
-            }
-        }
-
-        return ans;
-    }
-private:
-    inline int calcDistance(const auto& p1, const auto& p2) {
-        return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]);
+        return result;
     }
 };
 
