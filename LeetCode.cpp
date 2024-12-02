d8edac49162348480c6d91b3db80ad874300bffc
Danil Pinchuk <512kb@proton.me>
2024-12-02 03:46:23 -0500

    355. Design Twitter

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -5,40 +5,65 @@
 
 using namespace std;
 
-class Solution {
-    struct Travel {
-        int place;
-        int cost;
-        int stops;
+class Twitter {
+    struct FeedCompare {
+        bool operator()(const auto& tweet1, const auto& tweet2) {
+            return tweet1.second > tweet2.second;
+        }
     };
-public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int, int>>> graph(n);
-        for (auto& f : flights) { graph[f[0]].emplace_back(f[1], f[2]); }
 
-        vector<int> costs(n, numeric_limits<int>::max());
-        queue<Travel> pq;
-        pq.emplace(src, 0, 0);
+    static FeedCompare feedCompare;
 
-        while (!pq.empty()) {
-            auto [place, cost, stops] = pq.front();
-            pq.pop();
-
-            if (place == dst) { continue; }
-            if (stops++ > k) { continue; }
-
-            for (auto& dest : graph[place]) {
-                int destCost = cost + dest.second;
-
-                if (destCost < costs[dest.first]) {
-                    costs[dest.first] = destCost;
-                    pq.emplace(dest.first, destCost, stops);
-                }
+    void fillFeedWithNewTweets(int userId, vector<pair<int, int>>& feed) {
+        for (auto& tweet : m_tweets[userId]) {
+            if (tweet.second > feed.front().second) {
+                pop_heap(feed.begin(), feed.end(), feedCompare);
+                feed.back() = tweet;
+                push_heap(feed.begin(), feed.end(), feedCompare);
+            } else {
+                break;
             }
         }
-
-        return costs[dst] != numeric_limits<int>::max() ? costs[dst] : -1;
     }
+public:
+    Twitter() : m_time(0) {
+
+    }
+
+    void postTweet(int userId, int tweetId) {
+        auto& tweetsQueue = m_tweets[userId];
+
+        tweetsQueue.emplace_front(tweetId, ++m_time);
+        if (tweetsQueue.size() > 10) { tweetsQueue.pop_back(); }
+    }
+
+    vector<int> getNewsFeed(int userId) {
+        vector<pair<int, int>> feed(10);
+
+        fillFeedWithNewTweets(userId, feed);
+        for (int followee : m_followees[userId]) { fillFeedWithNewTweets(followee, feed); }
+
+        sort(feed.begin(), feed.end(), [](const auto& tweet1, const auto& tweet2) { return tweet1.second > tweet2.second; });
+
+        vector<int> result;
+        for (auto& tweet : feed) {
+            if (tweet.second > 0) { result.push_back(tweet.first); } else { break; }
+        }
+
+        return result;
+    }
+
+    void follow(int followerId, int followeeId) {
+        m_followees[followerId].insert(followeeId);
+    }
+
+    void unfollow(int followerId, int followeeId) {
+        m_followees[followerId].erase(followeeId);
+    }
+private:
+    unordered_map<int, deque<pair<int, int>>> m_tweets;
+    unordered_map<int, set<int>> m_followees;
+    int m_time;
 };
 
 int main()
