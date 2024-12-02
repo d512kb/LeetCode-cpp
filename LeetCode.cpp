dc111eb436b01fb9f0116b9229e154f7f419a01c
Danil Pinchuk <512kb@proton.me>
2024-12-02 02:34:43 -0500

    787. Cheapest Flights Within K Stops

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,35 +6,38 @@
 using namespace std;
 
 class Solution {
+    struct Travel {
+        int place;
+        int cost;
+        int stops;
+    };
 public:
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int, int>>> graph(n + 1);
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int, int>>> graph(n);
+        for (auto& f : flights) { graph[f[0]].emplace_back(f[1], f[2]); }
 
-        for (auto& time : times) { graph[time[0]].emplace_back(time[2], time[1]); }
-
-        vector<int> costs(n + 1, numeric_limits<int>::max());
-        costs[0] = 0; // unused
-        costs[k] = 0; // start
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-        pq.emplace(0, k);
+        vector<int> costs(n, numeric_limits<int>::max());
+        queue<Travel> pq;
+        pq.emplace(src, 0, 0);
 
         while (!pq.empty()) {
-            auto p = pq.top();
+            auto [place, cost, stops] = pq.front();
             pq.pop();
 
-            for (auto& n : graph[p.second]) {
-                int travelCost = p.first + n.first;
+            if (place == dst) { continue; }
+            if (stops++ > k) { continue; }
 
-                if (travelCost < costs[n.second]) {
-                    costs[n.second] = travelCost;
-                    pq.emplace(travelCost, n.second);
+            for (auto& dest : graph[place]) {
+                int destCost = cost + dest.second;
+
+                if (destCost < costs[dest.first]) {
+                    costs[dest.first] = destCost;
+                    pq.emplace(dest.first, destCost, stops);
                 }
             }
         }
 
-        int ans = *max_element(costs.begin(), costs.end());
-
-        return ans == numeric_limits<int>::max() ? -1 : ans;
+        return costs[dst] != numeric_limits<int>::max() ? costs[dst] : -1;
     }
 };
 
