3f64dd6b628fdfeb5d37ad7adefc53a0bb6c65a5
Danil Pinchuk <512kb@proton.me>
2024-12-06 10:37:38 -0500

    449. Serialize and Deserialize BST

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -13,14 +13,51 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-
-class Solution {
+class Codec {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (p == root || q == root) { return root; }
 
-        if (root->val > p->val && root->val > q->val) { return lowestCommonAncestor(root->left, p, q); }
-        if (root->val < p->val && root->val < q->val) { return lowestCommonAncestor(root->right, p, q); }
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        string serialData;
+
+        serialize(root, serialData);
+
+        return serialData;
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        int index = 0;
+        return deserialize(data, index);
+    }
+
+private:
+    void serialize(TreeNode* node, string& to) {
+        if (!node) {
+            to.push_back(static_cast<char>(0xFF));
+            return;
+        }
+
+        to.push_back(node->val >> 8);
+        to.push_back(node->val);
+
+        serialize(node->left, to);
+        serialize(node->right, to);
+    }
+
+    TreeNode* deserialize(const string& from, int& index) {
+        if (index >= from.size()) { return nullptr; }
+        if (from[index] == static_cast<char>(0xFF)) {
+            ++index;
+            return nullptr;
+        }
+
+        int a = from[index++];
+        int b = static_cast<unsigned char>(from[index++]);
+        TreeNode* root = new TreeNode((a << 8) + b);
+
+        root->left = deserialize(from, index);
+        root->right = deserialize(from, index);
 
         return root;
     }
