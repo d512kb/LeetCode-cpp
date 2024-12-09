24418979429b295abe8a150a7aa76f9f37ba13e0
Danil Pinchuk <512kb@proton.me>
2024-12-09 11:40:11 -0500

    2035. Partition Array Into Two Arrays to Minimize Sum Difference

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,52 +7,68 @@ using namespace std;
 
 class Solution {
 public:
-    int minAbsDifference(vector<int>& nums, int goal) {
-        if (nums.size() == 1) { return min(abs(goal), abs(goal - nums.front())); }
+    int minimumDifference(vector<int>& nums) {
+        int midSize = nums.size() / 2;
+        vector<vector<int>> leftSums(midSize + 1), rightSums(midSize + 1);
 
-        int mid = nums.size() / 2;
-        vector<int> halfNums(nums.begin(), nums.begin() + mid);
+        vector<int> halfNums(nums.begin(), nums.begin() + midSize);
         sort(halfNums.begin(), halfNums.end());
-
-        vector<int> sums;
-        generateSubsetsSums(halfNums, 0, 0, sums);
-
-        sort(sums.begin(), sums.end());
-        sums.erase(unique(sums.begin(), sums.end()), sums.end());
-
-        halfNums.assign(nums.begin() + mid, nums.end());
+        generateSubsetsSums(halfNums, 0, 0, 0, leftSums);
+        halfNums.assign(nums.begin() + midSize, nums.end());
         sort(halfNums.begin(), halfNums.end());
-        vector<int> rightSums;
-        generateSubsetsSums(halfNums, 0, 0, rightSums);
+        generateSubsetsSums(halfNums, 0, 0, 0, rightSums);
 
+        for (auto& sums : rightSums) { sort(sums.begin(), sums.end()); }
+
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+        int targetSum = totalSum / 2.;
         int ans = numeric_limits<int>::max();
 
-        for (int sum : rightSums) {
-            auto iter = lower_bound(sums.begin(), sums.end(), goal - sum);
+        for (int i = 0; i <= midSize; ++i) {
+            const auto& otherSums = rightSums[midSize - i];
 
-            if (iter != sums.end()) {
-                ans = min(ans, abs(goal - sum - *iter));
-            }
+            for (int sum : leftSums[i]) {
+                int neededSum = targetSum - sum;
+                int left = 0; int right = otherSums.size() - 1;
+                int otherSum = otherSums[left];
 
-            if (iter != sums.begin()) {
-                ans = min(ans, abs(goal - sum - *prev(iter)));
+                while (left < right) {
+                    int mid = (left + right) / 2;
+                    int midSum = otherSums[mid];
+
+                    if (midSum < neededSum) {
+                        left = mid + 1;
+                    } else if (midSum > neededSum) {
+                        right = mid - 1;
+                    } else {
+                        left = right = mid;
+                    }
+                }
+
+                ans = min(ans, abs(totalSum - 2 * (sum + otherSums[left])));
+
+                if (left > 0) { // if we have 2 potential sums we have to check which gives us lesser difference
+                    // between PotentialSum and its complement (TotalSum - PotentialSum). The diffence
+                    // is abs(TotalSum-PotentialSum - PotentialSum) which is abs(TotalSum - 2*PotentialSum)
+                    ans = min(ans, abs(totalSum - 2 * (sum + otherSums[left - 1])));
+                }
             }
         }
 
         return ans;
     }
-private:
-    void generateSubsetsSums(const vector<int>& nums, int index, int sum, vector<int>& sums) {
-        if (index == nums.size()) { return sums.push_back(sum); return; }
 
-        generateSubsetsSums(nums, index + 1, sum + nums[index], sums);
+private:
+    void generateSubsetsSums(vector<int>& nums, int index, int usedItems, int sum, vector<vector<int>>& sums) {
+        if (index == nums.size()) { sums[usedItems].push_back(sum); return; }
+
+        generateSubsetsSums(nums, index + 1, usedItems + 1, sum + nums[index], sums);
 
         while (index < nums.size() - 1 && nums[index] == nums[index + 1]) { ++index; }
 
-        generateSubsetsSums(nums, index + 1, sum, sums);
+        generateSubsetsSums(nums, index + 1, usedItems, sum, sums);
     }
 };
-
 
 int main()
 {
