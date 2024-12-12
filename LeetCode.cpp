e41868851cfc2d4329b519ca7123fd6d16e2e062
Danil Pinchuk <512kb@proton.me>
2024-12-11 21:22:26 -0500

    1547. Minimum Cost to Cut a Stick

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,72 +6,30 @@
 using namespace std;
 
 class Solution {
-    struct Compare {
-        bool operator()(const auto& p1, const auto& p2) const {
-            return p1.second > p2.second;
-        }
-    };
 public:
-    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        int wordSize = beginWord.size();
-        unordered_map<string, pair<int, vector<string>>> distances;
+    int minCost(int n, vector<int>& cuts) {
+        cuts.push_back(0);
+        cuts.push_back(n);
+        sort(cuts.begin(), cuts.end());
+        vector<vector<int>> cache(cuts.size(), vector<int>(cuts.size(), -1));
 
-        for (auto& word : wordList) { distances.emplace(word, make_pair(numeric_limits<int>::max(), vector<string>())); }
-        distances[beginWord].first = 0;
+        auto cutStick = [&](auto&& self, int i, int j) -> int {
+            if (i + 1 == j) { return 0; }
 
-        priority_queue<pair<string, int>, vector<pair<string, int>>, Compare> pq;
-        pq.emplace(beginWord, 0);
+            int& cachedValue = cache[i][j];
+            if (cachedValue != -1) { return cachedValue; }
 
-        while (!pq.empty()) {
-            auto currentNode = pq.top();
-            auto word = currentNode.first;
-            pq.pop();
+            int minCut = numeric_limits<int>::max();
+            int cutCost = cuts[j] - cuts[i];
 
-            for (int i = 0; i < wordSize; ++i) {
-                char prevC = word[i];
-
-                for (int c = 'a'; c <= 'z'; ++c) {
-                    word[i] = c;
-                    auto iter = distances.find(word);
-
-                    if (iter != distances.end()) {
-                        int newCost = currentNode.second + 1;
-
-                        if (newCost < iter->second.first) {
-                            iter->second = make_pair(newCost, vector<string>(1, currentNode.first));
-                            pq.emplace(word, newCost);
-                        } else if (newCost == iter->second.first) {
-                            iter->second.second.push_back(currentNode.first);
-                        }
-                    }
-                }
-
-                word[i] = prevC;
+            for (int k = i + 1; k < j; ++k) {
+                minCut = min(minCut, self(self, i, k) + self(self, k, j) + cutCost);
             }
-        }
 
-        vector<vector<string>> result;
+            return cachedValue = minCut;
+        };
 
-        if (!distances[endWord].second.empty()) {
-            vector<string> path(1, endWord);
-            buildPaths(distances, endWord, path, result);
-
-            for (auto& path : result) {
-                reverse(path.begin(), path.end());
-            }
-        }
-
-        return result;
-    }
-private:
-    void buildPaths(unordered_map<string, pair<int, vector<string>>>& distances, string word, vector<string>& path, vector<vector<string>>& result) {
-        if (distances[word].second.empty()) { result.push_back(path); return; }
-
-        for (auto& fromWord : distances[word].second) {
-            path.push_back(fromWord);
-            buildPaths(distances, fromWord, path, result);
-            path.pop_back();
-        }
+        return cutStick(cutStick, 0, cuts.size() - 1);
     }
 };
 
