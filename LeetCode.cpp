03c0a3f66797fae8a1b73d6d38cfdfbf3e001a1f
Danil Pinchuk <512kb@proton.me>
2024-12-16 22:25:46 -0500

    1487. Making File Names Unique

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,26 +7,29 @@ using namespace std;
 
 class Solution {
 public:
-    int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
-        vector<int> prefixSums(nums.size() + 1);
+    vector<string> getFolderNames(vector<string>& names) {
+        map<string, int> assignedCounts;
+        vector<string> result;
+        result.reserve(names.size());
 
-        for (int i = 1; i < prefixSums.size(); ++i) { prefixSums[i] = nums[i - 1] + prefixSums[i - 1]; }
+        for (auto& name : names) {
+            auto ins = assignedCounts.emplace(name, 1);
 
-        auto calcMax = [&](int len1, int len2) {
-            int maxSum = 0;
+            if (ins.second) {
+                result.push_back(name);
+            } else {
+                int counter = ins.first->second;
 
-            for (int i = len1; i < prefixSums.size() - len2; ++i) {
-                int sum1 = prefixSums[i] - prefixSums[i - len1];
-
-                for (int j = i + len2; j < prefixSums.size(); ++j) {
-                    maxSum = max(maxSum, sum1 + prefixSums[j] - prefixSums[j - len2]);
+                while (!assignedCounts.emplace(std::format("{0}({1})", name, counter), 1).second) {
+                    ++counter;
                 }
+
+                result.push_back(std::format("{0}({1})", name, counter));
+                assignedCounts[name] = ++counter;
             }
+        }
 
-            return maxSum;
-        };
-
-        return max(calcMax(firstLen, secondLen), calcMax(secondLen, firstLen));
+        return result;
     }
 };
 
