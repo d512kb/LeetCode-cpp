295ebb8a3d4f86c572d5323eecc38196c26cd8b5
Danil Pinchuk <512kb@proton.me>
2024-12-17 18:16:48 -0500

    2028. Find Missing Observations

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,31 +7,16 @@ using namespace std;
 
 class Solution {
 public:
-    int compareVersion(string version1, string version2) {
-        string::size_type a = 0;
-        string::size_type b = 0;
+    vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+        int rollsSum = accumulate(rolls.begin(), rolls.end(), 0);
+        int targetSum = (rolls.size() + n) * mean - rollsSum;
 
-        while (a != version1.size() || b != version2.size()) {
-            int aEnd = version1.find('.', a);
-            int bEnd = version2.find('.', b);
+        if (targetSum < n || targetSum > 6 * n) { return {}; }
 
-            if (aEnd == string::npos) { aEnd = version1.size(); }
-            if (bEnd == string::npos) { bEnd = version2.size(); }
+        vector<int> result(n, targetSum / n);
+        fill_n(result.begin(), targetSum % n, 1 + (targetSum / n));
 
-            int aVer(0), bVer(0);
-            from_chars(&version1[a], &version1[aEnd], aVer);
-            from_chars(&version2[b], &version2[bEnd], bVer);
-
-            if (aVer < bVer) { return -1; }
-            if (aVer > bVer) { return 1; }
-
-            a = aEnd;
-            b = bEnd;
-            if (a < version1.size()) { ++a; }
-            if (b < version2.size()) { ++b; }
-        }
-
-        return 0;
+        return result;
     }
 };
 
