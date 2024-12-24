d58662dfe7ba543efa866f81c398feb29f0a28bc
Danil Pinchuk <512kb@proton.me>
2024-12-24 01:18:16 -0500

    1202. Smallest String With Swaps

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,32 +7,46 @@ using namespace std;
 
 class Solution {
 public:
-    string longestDiverseString(int a, int b, int c) {
-        using CharCount = pair<int, char>;
-        vector<CharCount> chars{ {a, 'a'}, {b, 'b'}, {c, 'c'} };
+    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+        vector<int> parent(s.size(), -1);
+        for (auto& p : pairs) { join(parent, p[0], p[1]); }
 
-        string s("  ");
-
-        while (true) {
-            sort(chars.begin(), chars.end());
-            int sz = s.size();
-
-            for (int i = 2; i >= 0; --i) {
-                auto& [cnt, c] = chars[i];
-
-                if (cnt > 0 && (s[sz - 1] != c || s[sz - 2] != c)) {
-                    --cnt;
-                    s.push_back(c);
-                    break;
-                }
-            }
-
-            if (sz == s.size()) {
-                break;
-            }
+        unordered_map<int, vector<char>> sortedParts;
+        for (int i = 0; i < parent.size(); ++i) {
+            sortedParts[find(parent, i)].push_back(s[i]);
         }
 
-        return s.erase(0, 2);
+        for (auto& ch : sortedParts) { sort(ch.second.begin(), ch.second.end(), greater<>()); }
+
+        for (int i = 0; i < s.size(); ++i) {
+            auto& sortedPart = sortedParts[find(parent, i)];
+
+            s[i] = sortedPart.back();
+            sortedPart.pop_back();
+        }
+
+        return s;
+    }
+private:
+    int find(vector<int>& parent, int x) {
+        if (parent[x] < 0) { return x; }
+        return parent[x] = find(parent, parent[x]);
+    }
+
+    void join(vector<int>& parent, int a, int b) {
+        a = find(parent, a);
+        b = find(parent, b);
+
+        if (a != b) {
+            if (parent[a] < parent[b]) { // a has a higher (negative) rank
+                parent[b] = a;
+            } else if (parent[b] < parent[a]) {
+                parent[a] = b;
+            } else {
+                parent[a] = b;
+                --parent[b];
+            }
+        }
     }
 };
 
