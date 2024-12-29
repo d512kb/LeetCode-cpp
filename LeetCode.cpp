572b4891205042a7e505e31b49d8af7af9c939c0
Danil Pinchuk <512kb@proton.me>
2024-12-29 03:26:37 -0500

    231. Power of Two

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,32 +7,9 @@ using namespace std;
 
 class Solution {
 public:
-    int minOperations(vector<int>& nums1, vector<int>& nums2) {
-        int diff = accumulate(nums2.begin(), nums2.end(), 0) - accumulate(nums1.begin(), nums1.end(), 0);
-
-        if (diff == 0) { return 0; }
-        if (diff < 0) {
-            swap(nums1, nums2);
-            diff = -diff;
-        }
-
-        vector<int> count(6);
-        for (int n : nums1) { ++count[6 - n]; }
-        for (int n : nums2) { ++count[n - 1]; }
-
-        int result = 0;
-
-        for (int i = 5; i > 0; --i) {
-            result += count[i];
-            diff -= i * count[i];
-
-            if (diff <= 0) {
-                result -= -diff / i;
-                return result;
-            }
-        }
-
-        return -1;
+    bool isPowerOfTwo(int n) {
+        if (n < 0) { return false; }
+        return bitset<32>(n).count() == 1;
     }
 };
 
