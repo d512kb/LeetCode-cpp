9b71622f0208287f12d7f653c68200644a04a985
Danil Pinchuk <512kb@proton.me>
2025-01-05 03:20:01 -0500

    2405. Optimal Partition of String

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,29 +7,19 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        const int maxSize = rows * cols;
-        char dirs[]{ 0, 1, 0, -1, 0 };
-        char dir = 0; // east
-        vector<int> pos{ rStart, cStart };
-        vector<vector<int>> result{ {rStart, cStart} };
-        int sideSize = 0;
+    int partitionString(string s) {
+        bool charsUsed[26]{};
+        int ans = 1;
 
-        while (result.size() < maxSize) {
-            if (dir == 0 || dir == 2) { ++sideSize; }
-
-            for (int i = 0; i < sideSize; ++i) {
-                pos[0] += dirs[dir];
-                pos[1] += dirs[dir + 1];
-
-                if (pos[0] < 0 || pos[0] >= rows || pos[1] < 0 || pos[1] >= cols) { continue; }
-                result.push_back(pos);
+        for (char c : s) {
+            if (charsUsed[c - 'a']) {
+                memset(charsUsed, 0, sizeof(charsUsed));
+                ++ans;
             }
-
-            dir = (dir + 1) % 4;
+            charsUsed[c - 'a'] = 1;
         }
 
-        return result;
+        return ans;
     }
 };
 
