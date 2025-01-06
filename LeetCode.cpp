d51d10dbfc0cdfe8f11164ab67b8cfd5903e177d
Danil Pinchuk <512kb@proton.me>
2025-01-06 03:53:08 -0500

    1304. Find N Unique Integers Sum up to Zero

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,20 +7,16 @@ using namespace std;
 
 class Solution {
 public:
-    string tictactoe(vector<vector<int>>& moves) {
-        vector<char> aLines{ 0, 0, 0, 0, 0, 0, 0, 0, 'A' }; // 3 horizontal top to bottom, 3 vertical left to right ...
-        vector<char> bLines{ 0, 0, 0, 0, 0, 0, 0, 0, 'B' }; // ... and 2 diagonals (left and right) and player ID
+    vector<int> sumZero(int n) {
+        vector<int> result(n);
+        if (n % 2) { --n; }
 
-        for (auto& move : moves) {
-            if (++aLines[move[0]] == 3) { return { aLines.back() }; }
-            if (++aLines[3 + move[1]] == 3) { return { aLines.back() }; }
-            if (move[0] == move[1] && ++aLines[6] == 3) { return { aLines.back() }; }
-            if (move[0] + move[1] == 2 && ++aLines[7] == 3) { return { aLines.back() }; }
-
-            swap(aLines, bLines);
+        for (int i = 1; i < n; i += 2) {
+            result[i] = i;
+            result[i - 1] = -i;
         }
 
-        return moves.size() == 9 ? "Draw" : "Pending";
+        return result;
     }
 };
 
