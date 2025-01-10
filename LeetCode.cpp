cd18fd11281c0a79f1583eb21701d077b43bbdb6
Danil Pinchuk <512kb@proton.me>
2025-01-10 07:23:49 -0500

    2262. Total Appeal of A String

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,49 +7,17 @@ using namespace std;
 
 class Solution {
 public:
-    long long minimumCost(vector<int>& nums, int k, int dist) {
-        multiset<int, greater<>> pqMax;
-        multiset<int> pqMin;
-        int64_t runningSum = 0;
+    long long appealSum(string s) {
+        vector<int> lastPos(26, -1);
+        int64_t ans = 0;
+        int64_t appeal = 0;
 
-        for (int i = 1; i <= dist + 1; ++i) { pqMin.insert(nums[i]); }
-
-        while (pqMax.size() < k - 1) {
-            pqMax.insert(*pqMin.begin());
-            runningSum += *pqMin.begin();
-            pqMin.erase(pqMin.begin());
+        for (int i = 0; i < s.size(); ++i) {
+            appeal += i - exchange(lastPos[s[i] - 'a'], i);
+            ans += appeal;
         }
 
-        int64_t result = runningSum;
-        for (int a = 1, b = dist + 2; b < nums.size(); ++a, ++b) {
-            if (pqMin.extract(nums[a]).empty()) {
-                pqMax.extract(nums[a]);
-                runningSum -= nums[a];
-            }
-
-            if (nums[b] < *pqMax.begin()) {
-                pqMax.insert(nums[b]);
-                runningSum += nums[b];
-            } else {
-                pqMin.insert(nums[b]);
-            }
-
-            if (pqMax.size() < k - 1) {
-                pqMax.insert(*pqMin.begin());
-                runningSum += *pqMin.begin();
-                pqMin.extract(*pqMin.begin());
-            }
-
-            if (pqMax.size() > k - 1) {
-                pqMin.insert(*pqMax.begin());
-                runningSum -= *pqMax.begin();
-                pqMax.extract(*pqMax.begin());
-            }
-
-            result = min(result, runningSum);
-        }
-
-        return result + nums[0];
+        return ans;
     }
 };
 
