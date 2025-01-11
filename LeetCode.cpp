d92c1848c9a1926b43fb3be5d80e09b903da4352
Danil Pinchuk <512kb@proton.me>
2025-01-11 03:48:37 -0500

    460. LFU Cache

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -5,34 +5,68 @@
 
 using namespace std;
 
-class FreqStack {
+class LFUCache {
+    using FrequencyItems = list<pair<int, int>>;
+    struct Frequency {
+        int freq;
+        FrequencyItems::iterator iter;
+    };
 public:
-    FreqStack() {
+    LFUCache(int capacity) : m_capacity(capacity), m_minFreq(0) {
 
     }
 
-    void push(int val) {
-        int freq = m_freqMap[val]++;
+    int get(int key) {
+        auto value = findValue(key);
+        if (value) { return *value; }
 
-        if (m_freqToValues.size() == freq) {
-            m_freqToValues.emplace_back(1, val);
+        return -1;
+    }
+
+    void put(int key, int value) {
+        auto val = findValue(key);
+
+        if (val) {
+            val->get() = value;
             return;
         }
 
-        m_freqToValues[freq].push_back(val);
-    }
+        if (m_keyToFreq.size() == m_capacity) {
+            auto& freqList = m_freqToItems[m_minFreq];
+            m_keyToFreq.erase(freqList.front().first);
+            freqList.pop_front();
+            if (freqList.empty()) { m_freqToItems.erase(m_minFreq); }
+        }
 
-    int pop() {
-        if (m_freqToValues.back().empty()) { m_freqToValues.pop_back(); }
-
-        int val = m_freqToValues.back().back();
-        m_freqToValues.back().pop_back();
-        --m_freqMap[val];
-        return val;
+        m_minFreq = 0;
+        auto& freqList = m_freqToItems[0];
+        m_keyToFreq[key].iter = freqList.emplace(freqList.end(), key, value);
     }
 private:
-    unordered_map<int, int> m_freqMap;
-    deque<vector<int>> m_freqToValues;
+    int m_capacity;
+    int m_minFreq;
+    unordered_map<int, Frequency> m_keyToFreq;
+    unordered_map<int, FrequencyItems> m_freqToItems;
+
+    std::optional<reference_wrapper<int>> findValue(int key) {
+        auto keyIter = m_keyToFreq.find(key);
+        if (keyIter == m_keyToFreq.end()) { return std::nullopt; }
+
+        auto& [freq, iter] = keyIter->second;
+        auto& curList = m_freqToItems[freq];
+        auto& nextList = m_freqToItems[freq + 1];
+
+        nextList.splice(nextList.end(), curList, iter);
+        iter = --nextList.end();
+
+        if (curList.empty()) {
+            m_freqToItems.erase(freq);
+            if (m_minFreq == freq) { ++m_minFreq; }
+        }
+
+        ++freq;
+        return iter->second;
+    }
 };
 
 int main()
