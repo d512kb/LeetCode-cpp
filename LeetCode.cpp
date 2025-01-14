0cebc9b0e8c08683e82f7c3dc1c3ba7b26239d23
Danil Pinchuk <512kb@proton.me>
2025-01-14 06:36:20 -0500

    2971. Find Polygon With the Largest Perimeter

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,30 +7,15 @@ using namespace std;
 
 class Solution {
 public:
-    int maxOperations(vector<int>& nums) {
-        vector<vector<int>> memo(nums.size(), vector<int>(nums.size(), -1));
+    long long largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
 
-        auto calcOperations = [&](auto&& self, int left, int right, int score) -> int {
-            if (left >= right) { return 0; }
+        int64_t sidesSum = accumulate(nums.begin(), nums.end() - 1, 0l);
+        for (int lastSide = nums.size() - 1; lastSide >= 2; --lastSide, sidesSum -= nums[lastSide]) {
+            if (sidesSum > nums[lastSide]) { return sidesSum + nums[lastSide]; }
+        }
 
-            int& ans = memo[left][right];
-            if (ans != -1) { return ans; }
-
-            ans = 0;
-            if (nums[left] + nums[left + 1] == score) { ans = max(ans, 1 + self(self, left + 2, right, score)); }
-            if (nums[left] + nums[right] == score) { ans = max(ans, 1 + self(self, left + 1, right - 1, score)); }
-            if (nums[right - 1] + nums[right] == score) { ans = max(ans, 1 + self(self, left, right - 2, score)); }
-
-            return ans;
-        };
-
-        int ans = 0;
-
-        ans = max(ans, 1 + calcOperations(calcOperations, 2, nums.size() - 1, nums[0] + nums[1]));
-        ans = max(ans, 1 + calcOperations(calcOperations, 1, nums.size() - 2, nums.front() + nums.back()));
-        ans = max(ans, 1 + calcOperations(calcOperations, 0, nums.size() - 3, nums.back() + nums[nums.size() - 2]));
-
-        return ans;
+        return -1;
     }
 };
 
