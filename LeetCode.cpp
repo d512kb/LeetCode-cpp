6003a2db53b3507d3c12533a5f4a3befbd9e0401
Danil Pinchuk <512kb@proton.me>
2025-01-14 06:29:42 -0500

    3040. Maximum Number of Operations With the Same Score II

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,26 +7,28 @@ using namespace std;
 
 class Solution {
 public:
-    int maxRepOpt1(string text) {
-        vector<int> charCount(26);
-        for (char c : text) { ++charCount[c - 'a']; }
+    int maxOperations(vector<int>& nums) {
+        vector<vector<int>> memo(nums.size(), vector<int>(nums.size(), -1));
+
+        auto calcOperations = [&](auto&& self, int left, int right, int score) -> int {
+            if (left >= right) { return 0; }
+
+            int& ans = memo[left][right];
+            if (ans != -1) { return ans; }
+
+            ans = 0;
+            if (nums[left] + nums[left + 1] == score) { ans = max(ans, 1 + self(self, left + 2, right, score)); }
+            if (nums[left] + nums[right] == score) { ans = max(ans, 1 + self(self, left + 1, right - 1, score)); }
+            if (nums[right - 1] + nums[right] == score) { ans = max(ans, 1 + self(self, left, right - 2, score)); }
+
+            return ans;
+        };
+
         int ans = 0;
 
-        for (char c = 'a'; c <= 'z'; ++c) {
-            if (charCount[c - 'a'] == 0) { continue; }
-
-            int start = 0;
-            int stop = 0;
-            char filler = 0;
-
-            for (stop; stop < text.size(); ++stop) {
-                if (text[stop] == c && filler < 2) { continue; }
-                if (text[stop] != c && ++filler < 2) { continue; }
-                if (text[start++] != c) { --filler; }
-            }
-
-            ans = max(ans, min(charCount[c - 'a'], stop - start));
-        }
+        ans = max(ans, 1 + calcOperations(calcOperations, 2, nums.size() - 1, nums[0] + nums[1]));
+        ans = max(ans, 1 + calcOperations(calcOperations, 1, nums.size() - 2, nums.front() + nums.back()));
+        ans = max(ans, 1 + calcOperations(calcOperations, 0, nums.size() - 3, nums.back() + nums[nums.size() - 2]));
 
         return ans;
     }
