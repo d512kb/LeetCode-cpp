e18dcd332dede067d2a8a51129d9b43b29a5601a
Danil Pinchuk <512kb@proton.me>
2025-01-14 10:42:40 -0500

    2477. Minimum Fuel Cost to Report to the Capital

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,12 +7,32 @@ using namespace std;
 
 class Solution {
 public:
-    double angleClock(int hour, int minutes) {
-        double minutesAngle = minutes * 6;
-        double hourAngle = hour * 30 + minutesAngle / 12;
-        double angle = abs(hourAngle - minutesAngle);
+    long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
+        vector<vector<int>> graph(roads.size() + 1);
 
-        return angle > 180 ? 360 - angle : angle;
+        for (auto& road : roads) {
+            graph[road[0]].push_back(road[1]);
+            graph[road[1]].push_back(road[0]);
+        }
+        int64_t cost = 0;
+
+        auto calcSubtree = [&](auto&& self, int prev, int from) -> int {
+            int subcitiesCount = 0;
+
+            for (int city : graph[from]) {
+                if (city == prev) { continue; }
+                subcitiesCount += self(self, from, city);
+            }
+
+            cost += ceil((1.0 + subcitiesCount) / seats);
+            return 1 + subcitiesCount;
+        };
+
+        for (int city : graph[0]) {
+            calcSubtree(calcSubtree, 0, city);
+        }
+
+        return cost;
     }
 };
 
