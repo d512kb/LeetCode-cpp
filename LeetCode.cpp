39f27b602e7966510a589c90cc5c997901f6f09d
Danil Pinchuk <512kb@proton.me>
2025-01-21 23:51:50 -0500

    767. Reorganize String

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,27 +7,39 @@ using namespace std;
 
 class Solution {
 public:
-    int longestValidSubstring(string word, vector<string>& forbidden) {
-        unordered_set<string> fbWords(forbidden.begin(), forbidden.end());
+    string reorganizeString(string s) {
+        vector<int> charCount(26);
+        for (char c : s) { ++charCount[c - 'a']; }
 
-        int ans = 0;
-        int validEnd = word.size();
-
-        for (int i = word.size() - 1; i >= 0; --i) {
-            string str;
-
-            for (int j = i; j < validEnd && j - i < 10; ++j) {
-                str.push_back(word[j]);
-
-                if (fbWords.contains(str)) {
-                    ans = max(ans, validEnd - i - 1); // we count all the chars between two forbidden words not including the first char
-                    validEnd = j;
-                    break;
-                }
+        priority_queue<pair<int, char>> pq;
+        for (int i = 0; i < 26; ++i) {
+            if (charCount[i] > 0) {
+                pq.emplace(charCount[i], i + 'a');
             }
         }
 
-        return max(ans, validEnd);
+        string result;
+        result.reserve(s.size());
+
+        while (pq.size() > 1) {
+            auto p1 = pq.top();
+            pq.pop();
+            auto p2 = pq.top();
+            pq.pop();
+
+            result.push_back(p1.second);
+            result.push_back(p2.second);
+            if (--p1.first > 0) { pq.push(std::move(p1)); };
+            if (--p2.first > 0) { pq.push(std::move(p2)); };
+        }
+
+        if (!pq.empty()) {
+            if (pq.top().first > 1) { return ""; }
+
+            result.push_back(pq.top().second);
+        }
+
+        return result;
     }
 };
 
