7b209b937950549979878107722dafad7d8874a0
Danil Pinchuk <512kb@proton.me>
2025-01-23 18:50:01 -0500

    496. Next Greater Element I

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,37 +7,23 @@ using namespace std;
 
 class Solution {
 public:
-    string reorganizeString(string s) {
-        vector<int> charCount(26);
-        for (char c : s) { ++charCount[c - 'a']; }
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> mapping;
+        stack<int> monoStack;
 
-        priority_queue<pair<int, char>> pq;
-        for (int i = 0; i < 26; ++i) {
-            if (charCount[i] > 0) {
-                pq.emplace(charCount[i], i + 'a');
+        for (int n : nums2) {
+            while (!monoStack.empty() && monoStack.top() < n) {
+                mapping[monoStack.top()] = n;
+                monoStack.pop();
             }
+
+            mapping[n] = -1;
+            monoStack.push(n);
         }
 
-        string result;
-        result.reserve(s.size());
+        vector<int> result;
 
-        while (pq.size() > 1) {
-            auto p1 = pq.top();
-            pq.pop();
-            auto p2 = pq.top();
-            pq.pop();
-
-            result.push_back(p1.second);
-            result.push_back(p2.second);
-            if (--p1.first > 0) { pq.push(std::move(p1)); };
-            if (--p2.first > 0) { pq.push(std::move(p2)); };
-        }
-
-        if (!pq.empty()) {
-            if (pq.top().first > 1) { return ""; }
-
-            result.push_back(pq.top().second);
-        }
+        for (int n : nums1) { result.push_back(mapping[n]); }
 
         return result;
     }
