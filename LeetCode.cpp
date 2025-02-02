ea64c115a5e0e4fca43b19993ff7d10cf2e28e62
Danil Pinchuk <512kb@proton.me>
2025-02-02 02:22:12 -0500

    2385. Amount of Time for Binary Tree to Be Infected

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,17 +7,31 @@ using namespace std;
 
 class Solution {
 public:
-    bool isPathCrossing(string path) {
-        set<pair<int, int>> visitedPoints{ {0, 0} };
-        pair<int, int> point;
+    int amountOfTime(TreeNode* root, int start) {
+        int ans = 0;
 
-        for (char c : path) {
-            if (c == 'N') { ++point.second; } else if (c == 'E') { ++point.first; } else if (c == 'S') { --point.second; } else { --point.first; }
+        dfs(root, start, ans);
 
-            if (!visitedPoints.insert(point).second) { return true; }
+        return ans;
+    }
+private:
+    int dfs(TreeNode* node, int start, int& result) {
+        if (!node) { return 0; }
+
+        int left = dfs(node->left, start, result);
+        int right = dfs(node->right, start, result);
+
+        if (node->val == start) {
+            result = max(left, right);
+            return -1;
         }
 
-        return false;
+        if (left >= 0 && right >= 0) {
+            return max(left, right) + 1;
+        }
+
+        result = max(result, abs(left) + abs(right));
+        return min(left, right) - 1;
     }
 };
 
