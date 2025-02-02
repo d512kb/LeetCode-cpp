dc6bb9d90d7ace20d4c5129973b80d10f975f9e6
Danil Pinchuk <512kb@proton.me>
2025-02-02 00:27:09 -0500

    2357. Make Array Zero by Subtracting Equal Amounts

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,33 +7,11 @@ using namespace std;
 
 class Solution {
 public:
-    int pseudoPalindromicPaths(TreeNode* root) {
-        array<char, 10> numCounts{};
-        int pathCount = 0;
+    int minimumOperations(vector<int>& nums) {
+        unordered_set<int> st(nums.begin(), nums.end());
 
-        traverseTree(root, numCounts, pathCount);
-
-        return pathCount;
-    }
-private:
-    void traverseTree(TreeNode* node, array<char, 10>& numCounts, int& pathCount) {
-        if (!node) { return; }
-
-        ++numCounts[node->val];
-
-        if (node->left || node->right) {
-            traverseTree(node->left, numCounts, pathCount);
-            traverseTree(node->right, numCounts, pathCount);
-        } else {
-            int oddCount = 0;
-            for (char c : numCounts) {
-                if (c % 2) { ++oddCount; }
-            }
-
-            if (oddCount <= 1) { ++pathCount; }
-        }
-
-        --numCounts[node->val];
+        st.erase(0);
+        return st.size();
     }
 };
 
