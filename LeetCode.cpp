ee825bef1b704244dc5f8ca54f43361e8002d822
Danil Pinchuk <512kb@proton.me>
2025-02-08 04:25:49 -0500

    946. Validate Stack Sequences

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,80 +6,28 @@
 using namespace std;
 
 class Solution {
-    class DSet {
-    public:
-        explicit DSet(size_t size) : m_parent(size, -1) {
-
-        }
-
-        int find(int value) {
-            if (m_parent[value] < 0) { return value; }
-            return m_parent[value] = find(m_parent[value]);
-        }
-
-        void erase(int value) {
-            m_parent[value] = -1;
-        }
-
-        void join(int a, int b) {
-            a = find(a);
-            b = find(b);
-
-            if (a == b) { return; }
-
-            auto& parentA = m_parent[a];
-            auto& parentB = m_parent[b];
-
-            if (parentA < parentB) { // size, negative
-                parentA += parentB;
-                parentB = a;
-            } else {
-                parentB += parentA;
-                parentA = b;
-            }
-        }
-    private:
-        vector<int> m_parent;
-    };
 public:
-    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-        auto cmp = [](const auto& m1, const auto& m2) { return m1[2] < m2[2]; };
-        sort(meetings.begin(), meetings.end(), cmp);
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> st;
+        st.push(-1);
 
-        DSet dset(n);
-        dset.join(0, firstPerson);
+        auto pushIter = pushed.begin();
+        auto popIter = popped.begin();
 
-        auto startTime = meetings.begin();
-        while (startTime != meetings.end()) {
-            int currentTime = (*startTime)[2];
-            auto endTime = startTime;
-
-            for (endTime; endTime != meetings.end() && (*endTime)[2] == currentTime; ++endTime) {
-                const auto& met = *endTime;
-                dset.join(met[0], met[1]);
+        while (popIter != popped.end()) {
+            while (st.top() != *popIter && pushIter != pushed.end()) {
+                st.push(*pushIter++);
             }
 
-            int secretKeeper = dset.find(0);
-
-            for (startTime; startTime != endTime; ++startTime) {
-                int part1 = (*startTime)[0];
-                int part2 = (*startTime)[1];
-
-                if (dset.find(part1) != secretKeeper) {
-                    dset.erase(part1);
-                    dset.erase(part2);
-                }
+            if (st.top() == *popIter) {
+                st.pop();
+                ++popIter;
+            } else {
+                return false;
             }
         }
 
-        int secretKeeper = dset.find(0);
-        vector<int> result;
-
-        while (--n >= 0) {
-            if (dset.find(n) == secretKeeper) { result.push_back(n); }
-        }
-
-        return result;
+        return true;
     }
 };
 
