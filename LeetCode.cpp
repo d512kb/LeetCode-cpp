09f38e33d422bfc99e53922a7427d8c41773d8da
Danil Pinchuk <512kb@proton.me>
2025-02-08 20:35:48 -0500

    1007. Minimum Domino Rotations For Equal Row

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,14 +7,24 @@ using namespace std;
 
 class Solution {
 public:
-    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
-        if (!root1 && !root2) { return true; }
-        if (!root1 || !root2) { return false; }
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+        const int sz = tops.size();
+        array<int, 7> top{}, bottom{}, both{};
 
-        if (root1->val != root2->val) { return false; }
+        for (int i = 0; i < sz; ++i) {
+            if (tops[i] != bottoms[i]) {
+                ++top[tops[i]];
+                ++bottom[bottoms[i]];
+            } else {
+                ++both[tops[i]];
+            }
+        }
 
-        return (flipEquiv(root1->left, root2->left) && flipEquiv(root1->right, root2->right)) || 
-            (flipEquiv(root1->left, root2->right) && flipEquiv(root1->right, root2->left));
+        for (int i = 1; i <= 6; ++i) {
+            if (top[i] + bottom[i] == sz - both[i]) { return min(top[i], bottom[i]); }
+        }
+
+        return -1;
     }
 };
 
