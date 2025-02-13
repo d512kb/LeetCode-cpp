774227128309a004a38ace9e9449dc544cb1e5e4
Danil Pinchuk <512kb@proton.me>
2025-02-12 23:58:41 -0500

    2316. Count Unreachable Pairs of Nodes in an Undirected Graph

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,37 +7,42 @@ using namespace std;
 
 class Solution {
 public:
-    string findReplaceString(string s, vector<int>& indices, vector<string>& sources, vector<string>& targets) {
-        const int k = indices.size();
-        const int sz = s.size();
-        map<int, int> replaces;
+    long long countPairs(int n, vector<vector<int>>& edges) {
+        vector<int> parent(n, -1);
 
-        for (int ind = 0; ind < k; ++ind) {
-            int targetIndex = indices[ind];
-            auto newReplace = replaces.emplace(targetIndex, ind);
+        for (auto& edge : edges) {
+            join(parent, edge[0], edge[1]);
+        }
 
-            if (newReplace.second) {
-                for (char sourceChar : sources[ind]) {
-                    if (targetIndex == sz || s[targetIndex] != sourceChar) {
-                        replaces.erase(newReplace.first);
-                        break;
-                    }
-                    ++targetIndex;
-                }
+        int64_t allPairs = static_cast<int64_t>(n) * (n - 1) / 2;
+        for (int par : parent) {
+            if (par < 0) {
+                allPairs -= static_cast<int64_t>(par) * (par + 1) / 2; // par is negative, so +
             }
         }
 
-        string result;
-        int sourceIndex = 0;
+        return allPairs;
+    }
+private:
+    void join(vector<int>& parent, int a, int b) {
+        a = find(parent, a);
+        b = find(parent, b);
 
-        for (auto& replace : replaces) {
-            result.append(s, sourceIndex, replace.first - sourceIndex);
-            result.append(targets[replace.second]);
-            sourceIndex = replace.first + sources[replace.second].size();
+        if (a == b) { return; }
+        if (a < b) {
+            parent[a] += parent[b];
+            parent[b] = a;
+        } else {
+            parent[b] += parent[a];
+            parent[a] = b;
         }
+    }
 
-        result.append(s, sourceIndex);
-        return result;
+    int find(vector<int>& parent, int value) {
+        auto& par = parent[value];
+        if (par < 0) { return value; }
+
+        return par = find(parent, par);
     }
 };
 
