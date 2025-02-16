c72153fdae802116ffca95ad74bdce76ea8c22f3
Danil Pinchuk <512kb@proton.me>
2025-02-16 05:57:58 -0500

    470. Implement Rand10() Using Rand7()

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,49 +6,13 @@
 using namespace std;
 
 class Solution {
-    struct Task {
-        int id{};
-        int startTime{};
-        int processingTime{};
-    };
+    int rand7() { return 0; }
 public:
-    vector<int> getOrder(vector<vector<int>>& tasks) {
-        const int sz = tasks.size();
-        vector<Task> tasksToDo;
+    int rand10() {
+        int val = 40;
+        while (val >= 40) { val = (rand7() - 1) * 7 + rand7() - 1; }
 
-        for (int i = 0; i < sz; ++i) { tasksToDo.emplace_back(i, tasks[i][0], tasks[i][1]); }
-        sort(tasksToDo.begin(), tasksToDo.end(), [](const Task& a, const Task& b) { return a.startTime < b.startTime; });
-
-        auto pqCompare = [](const Task& a, const Task& b) {
-            if (a.processingTime == b.processingTime) { return a.id > b.id; }
-            return a.processingTime > b.processingTime;
-        };
-
-        priority_queue<Task, vector<Task>, decltype(pqCompare)> pq;
-
-        size_t currentTime = 0;
-        auto iter = tasksToDo.begin();
-        vector<int> result;
-
-        while (iter != tasksToDo.end()) {
-            if (pq.empty() && currentTime < iter->startTime) { currentTime = iter->startTime; }
-
-            while (iter != tasksToDo.end() && currentTime >= iter->startTime) {
-                pq.emplace(iter->id, iter->startTime, iter->processingTime);
-                ++iter;
-            }
-
-            result.push_back(pq.top().id);
-            currentTime += pq.top().processingTime;
-            pq.pop();
-        }
-
-        while (!pq.empty()) {
-            result.push_back(pq.top().id);
-            pq.pop();
-        }
-
-        return result;
+        return val % 10 + 1;
     }
 };
 
