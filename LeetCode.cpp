006e7285b2028931b206ca9bb6c821b0a94f7d6e
Danil Pinchuk <512kb@proton.me>
2025-02-22 23:05:56 -0500

    3462. Maximum Sum With at Most K Elements

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,16 +7,18 @@ using namespace std;
 
 class Solution {
 public:
-    bool hasSameDigits(string s) {
-        while (s.size() > 2) {
-            string sChanged;
-            for (int i = 0; i < s.size() - 1; ++i) {
-                sChanged.push_back((s[i] - 'a' + s[i + 1] - 'a') % 10 + 'a');
-            }
-            s = sChanged;
+    long long maxSum(vector<vector<int>>& grid, vector<int>& limits, int k) {
+        const int rows = grid.size();
+        vector<int> allItems;
+
+        for (int row = 0; row < rows; ++row) {
+            auto& gridRow = grid[row];
+            partial_sort(gridRow.begin(), gridRow.begin() + limits[row], gridRow.end(), greater<>{});
+            copy(gridRow.begin(), gridRow.begin() + limits[row], back_inserter(allItems));
         }
 
-        return s[0] == s[1];
+        sort(allItems.begin(), allItems.end(), greater<>{});
+        return accumulate(allItems.begin(), allItems.begin() + k, 0l);
     }
 };
 
