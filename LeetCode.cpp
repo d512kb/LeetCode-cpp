a85254cd7311920b5e1ea78ccb8488c51178724f
Danil Pinchuk <512kb@proton.me>
2025-02-23 03:29:24 -0500

    1014. Best Sightseeing Pair

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,33 +7,17 @@ using namespace std;
 
 class Solution {
 public:
-    int getKth(int lo, int hi, int k) {
-        vector<int> powers(1001);
+    int maxScoreSightseeingPair(vector<int>& values) {
+        const int sz = values.size();
+        int currMax = values.front();
+        int ans = 0;
 
-        // for higher numbers it't better to use recursion + map to avoid recomputations
-        for (int i = 0; i <= 1000; ++i) {
-            int p = 0;
-            int val = i;
-
-            while (val > 1) {
-                ++p;
-
-                if (val % 2) { val = val * 3 + 1; } else { val /= 2; }
-            }
-
-            powers[i] = p;
+        for (int i = 1; i < sz; ++i) {
+            currMax = max(currMax, values[i - 1]) - 1;
+            ans = max(ans, currMax + values[i]);
         }
 
-        vector<int> values(hi - lo + 1);
-        iota(values.begin(), values.end(), lo);
-
-        auto cmp = [&powers](int a, int b) {
-            if (powers[a] == powers[b]) { return a < b; }
-            return powers[a] < powers[b];
-        };
-
-        nth_element(values.begin(), values.begin() + k - 1, values.end(), cmp);
-        return values[k - 1];
+        return ans;
     }
 };
 
