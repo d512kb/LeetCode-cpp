249c3ffca67e3cb2457f5480dc9fe3d339ef339a
Danil Pinchuk <512kb@proton.me>
2025-02-24 00:05:54 -0500

    652. Find Duplicate Subtrees

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,48 +6,28 @@
 using namespace std;
 
 class Solution {
-    struct LogRecord {
-        int id{ -1 };
-        bool start{ false };
-        int timestamp{ 0 };
-    };
+    using TreeHash = string;
 public:
-    vector<int> exclusiveTime(int n, vector<string>& logs) {
-        vector<int> result(n);
-        stack<int> callStack;
-        int markTime = 0;
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        unordered_map<TreeHash, int> hashCount;
+        vector<TreeNode*> result;
 
-        for (const auto& log : logs) {
-            auto currentCall = parseCall(log);
-
-            if (currentCall.start) {
-                if (!callStack.empty()) {
-                    result[callStack.top()] += currentCall.timestamp - markTime;
-                }
-
-                callStack.push(currentCall.id);
-                markTime = currentCall.timestamp;
-            } else {
-                result[currentCall.id] += currentCall.timestamp - markTime + 1;
-                callStack.pop();
-                markTime = currentCall.timestamp + 1;
-            }
-        }
-
+        calcTreeHashes(root, hashCount, result);
         return result;
     }
 private:
-    LogRecord parseCall(const string& callRecord) {
-        size_t firstColon = callRecord.find(':');
-        size_t secondColon = callRecord.find(':', firstColon + 1);
+    TreeHash calcTreeHashes(TreeNode* node, unordered_map<TreeHash, int>& hashCount, vector<TreeNode*>& result) {
+        if (!node) { return "#"; }
 
-        LogRecord record;
+        auto leftSubtree(std::move(calcTreeHashes(node->left, hashCount, result)));
+        auto rightSubtree(std::move(calcTreeHashes(node->right, hashCount, result)));
 
-        from_chars(&callRecord[0], &callRecord[firstColon], record.id);
-        from_chars(&callRecord[secondColon + 1], &callRecord.back() + 1, record.timestamp);
-        record.start = callRecord[firstColon + 1] == 's';
+        auto treeHash = to_string(node->val) + ',' + leftSubtree + ',' + rightSubtree;
+        if (++hashCount[treeHash] == 2) {
+            result.push_back(node);
+        }
 
-        return record;
+        return treeHash;
     }
 };
 
