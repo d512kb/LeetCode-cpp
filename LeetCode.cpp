6c926f75bf475b92329c076c0214b31bb3f1bc69
Danil Pinchuk <512kb@proton.me>
2025-02-24 20:10:20 -0500

    896. Monotonic Array

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,35 +7,8 @@ using namespace std;
 
 class Solution {
 public:
-    string addStrings(string num1, string num2) {
-        string result;
-
-        if (num1.size() > num2.size()) {
-            swap(num1, num2);
-        }
-
-        int num1pos = num1.size() - 1;
-        int num2pos = num2.size() - 1;
-        char carry = 0;
-
-        for (; num1pos >= 0; --num1pos, --num2pos) {
-            char add = num1[num1pos] - '0' + num2[num2pos] - '0' + carry;
-            result += (add % 10) + '0';
-            carry = add / 10;
-        }
-
-        for (; num2pos >= 0; --num2pos) {
-            char add = num2[num2pos] - '0' + carry;
-            result += (add % 10) + '0';
-            carry = add / 10;
-        }
-
-        if (carry == 1) {
-            result += '1';
-        }
-
-        reverse(result.begin(), result.end());
-        return result;
+    bool isMonotonic(vector<int>& nums) {
+        return is_sorted(nums.begin(), nums.end()) || is_sorted(nums.rbegin(), nums.rend());
     }
 };
 
