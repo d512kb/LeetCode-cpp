1ec0d41664500ece9f467e606e8495fc48e47c2b
Danil Pinchuk <512kb@proton.me>
2025-02-24 19:22:17 -0500

    2190. Most Frequent Number Following Key In an Array

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,18 +7,21 @@ using namespace std;
 
 class Solution {
 public:
-    bool kLengthApart(vector<int>& nums, int k) {
-        int len = k;
-        for (int n : nums) {
-            if (n == 1) {
-                if (len < k) { return false; }
-                len = 0;
-            } else {
-                ++len;
+    int mostFrequent(vector<int>& nums, int key) {
+        unordered_map<int, int> counts;
+        int result = 0;
+        int maxCount = 0;
+
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            if (nums[i] == key) {
+                if (++counts[nums[i + 1]] > maxCount) {
+                    maxCount = counts[nums[i + 1]];
+                    result = nums[i + 1];
+                };
             }
         }
 
-        return true;
+        return result;
     }
 };
 
