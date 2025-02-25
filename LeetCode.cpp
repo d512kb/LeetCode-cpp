d6b6516e2babdf1079f0ff6407879ccca613c927
Danil Pinchuk <512kb@proton.me>
2025-02-24 19:13:17 -0500

    1437. Check If All 1's Are at Least Length K Places Away

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,51 +6,19 @@
 using namespace std;
 
 class Solution {
-    struct Node {
-        bool w{ false };
-        array<unique_ptr<Node>, 26> nodes{};
-    };
 public:
-    Solution() : m_root(new Node()) {};
-
-    string longestWord(vector<string>& words) {
-        for (const string& word : words) { addWord(word); }
-
-        string word;
-        string longestWord;
-
-        findLongestWords(m_root.get(), word, longestWord);
-
-        return longestWord;
-    }
-private:
-    unique_ptr<Node> m_root;
-
-    void addWord(const string& word) {
-        auto node = m_root.get();
-
-        for (char c : word) {
-            int idx = c - 'a';
-
-            if (!node->nodes[idx]) { node->nodes[idx] = make_unique<Node>(); }
-            node = node->nodes[idx].get();
-        }
-
-        node->w = true;
-    }
-
-    void findLongestWords(Node* node, string& word, string& longestWord) {
-        for (int i = 0; i < 26; ++i) {
-            if (node->nodes[i] && node->nodes[i]->w) {
-                word.push_back(i + 'a');
-                findLongestWords(node->nodes[i].get(), word, longestWord);
-                word.pop_back();
+    bool kLengthApart(vector<int>& nums, int k) {
+        int len = k;
+        for (int n : nums) {
+            if (n == 1) {
+                if (len < k) { return false; }
+                len = 0;
+            } else {
+                ++len;
             }
         }
 
-        if (word.size() > longestWord.size() || (word.size() == longestWord.size() && word < longestWord)) {
-            longestWord = word;
-        }
+        return true;
     }
 };
 
