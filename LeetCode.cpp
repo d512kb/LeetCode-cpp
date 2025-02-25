d8c7a9c952c27f93d7633e6fd4ec760eb472f1d5
Danil Pinchuk <512kb@proton.me>
2025-02-24 20:36:47 -0500

    680. Valid Palindrome II

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,8 +7,35 @@ using namespace std;
 
 class Solution {
 public:
-    bool isMonotonic(vector<int>& nums) {
-        return is_sorted(nums.begin(), nums.end()) || is_sorted(nums.rbegin(), nums.rend());
+    bool validPalindrome(string s) {
+        int left = 0;
+        int right = s.size() - 1;
+        int diffs = 0;
+
+        while (left < right) {
+            if (s[left] != s[right]) {
+                for (int l = left + 1, r = right; l < r; ++l, --r) {
+                    if (s[l] != s[r]) {
+                        ++diffs;
+                        break;
+                    }
+                }
+
+                for (int l = left, r = right - 1; l < r; ++l, --r) {
+                    if (s[l] != s[r]) {
+                        ++diffs;
+                        break;
+                    }
+                }
+
+                break;
+            }
+
+            ++left;
+            --right;
+        }
+
+        return diffs < 2;
     }
 };
 
