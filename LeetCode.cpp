f5de893ba5f4c6f606d36ba78687f58fa02ddad9
Danil Pinchuk <512kb@proton.me>
2025-02-24 20:07:02 -0500

    415. Add Strings

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,16 +7,35 @@ using namespace std;
 
 class Solution {
 public:
-    int countElements(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        auto firstNum = upper_bound(nums.begin(), nums.end(), nums.front());
-        auto lastNum = upper_bound(nums.rbegin(), nums.rend(), nums.back(), greater<>{}).base();
+    string addStrings(string num1, string num2) {
+        string result;
 
-        int result = nums.size();
-        result -= distance(nums.begin(), firstNum);
-        result -= distance(lastNum, nums.end());
+        if (num1.size() > num2.size()) {
+            swap(num1, num2);
+        }
 
-        return max(result, 0);
+        int num1pos = num1.size() - 1;
+        int num2pos = num2.size() - 1;
+        char carry = 0;
+
+        for (; num1pos >= 0; --num1pos, --num2pos) {
+            char add = num1[num1pos] - '0' + num2[num2pos] - '0' + carry;
+            result += (add % 10) + '0';
+            carry = add / 10;
+        }
+
+        for (; num2pos >= 0; --num2pos) {
+            char add = num2[num2pos] - '0' + carry;
+            result += (add % 10) + '0';
+            carry = add / 10;
+        }
+
+        if (carry == 1) {
+            result += '1';
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
     }
 };
 
