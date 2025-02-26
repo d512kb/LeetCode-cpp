c9223ad653035e5c1f0fd0e9e909872b275d8220
Danil Pinchuk <512kb@proton.me>
2025-02-26 06:58:29 -0500

    2178. Maximum Split of Positive Even Integers

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,49 +7,19 @@ using namespace std;
 
 class Solution {
 public:
-    int minCostSetTime(int startAt, int moveCost, int pushCost, int targetSeconds) {
-        int minutes = targetSeconds / 60;
-        int seconds = targetSeconds % 60;
+    vector<long long> maximumEvenSplit(long long finalSum) {
+        if (finalSum % 2) { return {}; }
 
-        if (minutes == 100) {
-            --minutes;
-            seconds += 60;
+        vector<long long> result;
+        int subtract = 2;
+
+        while (finalSum >= subtract) {
+            result.push_back(subtract);
+            finalSum -= subtract;
+            subtract += 2;
         }
 
-        int result = calcCost(startAt, moveCost, pushCost, minutes, seconds);
-        if (seconds + 60 <= 99 && minutes > 0) {
-            result = min(result, calcCost(startAt, moveCost, pushCost, --minutes, seconds + 60));
-        }
-
-        return result;
-    }
-private:
-    int calcCost(int startAt, int moveCost, int pushCost, int minutes, int seconds) {
-        vector<int> digits;
-
-        while (seconds) {
-            digits.push_back(seconds % 10);
-            seconds /= 10;
-        }
-
-        while (minutes > 0 && digits.size() < 2) { digits.push_back(0); } // seconds are always 2 digits if we have minutes too
-
-        while (minutes) {
-            digits.push_back(minutes % 10);
-            minutes /= 10;
-        }
-
-        reverse(digits.begin(), digits.end());
-        int result = 0;
-
-        for (int n : digits) {
-            if (startAt != n) {
-                startAt = n;
-                result += moveCost;
-            }
-
-            result += pushCost;
-        }
+        result.back() += finalSum;
 
         return result;
     }
