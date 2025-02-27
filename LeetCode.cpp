7734c50173230a8627bc36569b5332c5edd47ca3
Danil Pinchuk <512kb@proton.me>
2025-02-27 05:30:54 -0500

    1828. Queries on Number of Points Inside a Circle

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,15 +7,32 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> findArray(vector<int>& pref) {
-        vector<int> result(pref.size());
-        result[0] = pref[0];
+    vector<int> countPoints(vector<vector<int>>& points, vector<vector<int>>& queries) {
+        vector<array<int, 2>> sortedPoints;
 
-        for (int i = 1; i < pref.size(); ++i) {
-            result[i] = pref[i] ^ pref[i - 1];
+        for (const auto& p : points) { sortedPoints.push_back({ p[0], p[1] }); }
+        sort(sortedPoints.begin(), sortedPoints.end());
+
+        vector<int> result;
+
+        for (const auto& query : queries) {
+            auto from = lower_bound(sortedPoints.begin(), sortedPoints.end(), array<int, 2>{query[0] - query[2], 0});
+            int number = 0;
+
+            for (from; from != sortedPoints.end() && (*from)[0] <= query[0] + query[2]; ++from) {
+                if (calcDistance(query, *from) <= query[2]) {
+                    ++number;
+                }
+            }
+
+            result.push_back(number);
         }
 
         return result;
+    }
+private:
+    inline double calcDistance(const auto& p1, const auto& p2) const {
+        return sqrt(pow(p1[0] - p2[0], 2) + pow(p1[1] - p2[1], 2));
     }
 };
 
