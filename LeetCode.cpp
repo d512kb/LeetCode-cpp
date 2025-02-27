669ca9745d8206f0520f1824640d412387259625
Danil Pinchuk <512kb@proton.me>
2025-02-27 01:20:37 -0500

    393. UTF-8 Validation

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,53 +7,34 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthLongestPath(string input) {
-        int index = 0;
-        int targetLevel = 0;
-        int longestPath = 0;
+    bool validUtf8(vector<int>& data) {
+        const int sz = data.size();
+        int bytesToCheck = 0;
 
-        traverse(input, index, 0, targetLevel, 0, longestPath);
+        for (int i = 0; i < sz; ++i) {
+            if (bytesToCheck > 0) {
+                if (!checkFollowingByte(data[i])) { return false; }
+                --bytesToCheck;
+                continue;
+            }
 
-        return longestPath;
+            bytesToCheck = calcBytesToCheck(data[i]);
+            if (bytesToCheck < 0) { return false; }
+        }
+
+        return bytesToCheck == 0;
     }
 private:
-    void traverse(const string& path, int& index, int level, int& targetLevel, int pathLen, int& longestPath) {
-        const int sz = path.size();
-        int localNameLen = 0;
+    int calcBytesToCheck(int val) const {
+        if (val >> 3 == 0b11110) { return 3; }
+        if (val >> 4 == 0b1110) { return 2; }
+        if (val >> 5 == 0b110) { return 1; }
+        if (val >> 7 == 0b0) { return 0; }
+        return -1;
+    }
 
-
-        while (index < sz) {
-            if (level < targetLevel) {
-                traverse(path, index, level + 1, targetLevel, pathLen + 1 + localNameLen, longestPath);
-            }
-
-            while (level > targetLevel) {
-                return;
-            }
-
-            if (path[index] == '\n') {
-                index += 1; // skip \n
-                int nextLevel = 0;
-
-                while (index < sz && path[index] == '\t') { // count tabs
-                    index += 1;
-                    ++nextLevel;
-                }
-
-                targetLevel = nextLevel;
-            } else {
-                bool file = false;
-                localNameLen = 0;
-
-                for (index; index < sz && path[index] != '\n'; ++index, ++localNameLen) {
-                    if (path[index] == '.') { file = true; }
-                }
-
-                if (file) {
-                    longestPath = max(longestPath, pathLen + localNameLen);
-                }
-            }
-        }
+    inline bool checkFollowingByte(int val) const {
+        return val >> 6 == 0b10;
     }
 };
 
