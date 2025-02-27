da1e300c2a3fea4d79de2133e9a7f385a641111c
Danil Pinchuk <512kb@proton.me>
2025-02-27 04:28:36 -0500

    2807. Insert Greatest Common Divisors in Linked List

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,34 +7,16 @@ using namespace std;
 
 class Solution {
 public:
-    bool validUtf8(vector<int>& data) {
-        const int sz = data.size();
-        int bytesToCheck = 0;
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        ListNode* node = head;
 
-        for (int i = 0; i < sz; ++i) {
-            if (bytesToCheck > 0) {
-                if (!checkFollowingByte(data[i])) { return false; }
-                --bytesToCheck;
-                continue;
-            }
-
-            bytesToCheck = calcBytesToCheck(data[i]);
-            if (bytesToCheck < 0) { return false; }
+        while (node->next) {
+            ListNode* gcdNode = new ListNode(gcd(node->val, node->next->val), node->next);
+            node->next = gcdNode;
+            node = gcdNode->next;
         }
 
-        return bytesToCheck == 0;
-    }
-private:
-    int calcBytesToCheck(int val) const {
-        if (val >> 3 == 0b11110) { return 3; }
-        if (val >> 4 == 0b1110) { return 2; }
-        if (val >> 5 == 0b110) { return 1; }
-        if (val >> 7 == 0b0) { return 0; }
-        return -1;
-    }
-
-    inline bool checkFollowingByte(int val) const {
-        return val >> 6 == 0b10;
+        return head;
     }
 };
 
--- LeetCode.h
+++ LeetCode.h
@@ -19,6 +19,15 @@
 #include <unordered_set>
 #include <vector>
 
+// Definition for singly - linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
 //Definition for a binary tree node.
 struct TreeNode {
     int val;
