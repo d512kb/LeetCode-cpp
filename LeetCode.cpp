c7266cf13cfea52c3a3adcd6b99b8fcaaf9900a2
Danil Pinchuk <512kb@proton.me>
2025-03-05 12:56:22 -0500

    2966. Divide Array Into Arrays With Max Difference

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,26 +7,18 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        unordered_map<int, pair<int, int>> playerStats;
+    vector<vector<int>> divideArray(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
 
-        for (const auto& match : matches) {
-            ++playerStats[match[0]].first; // actually we don't need win count
-            ++playerStats[match[1]].second;
+        for (int i = 0; i < nums.size(); i += 3) {
+            if (nums[i + 2] - nums[i] > k) { return {}; }
         }
 
-        vector<vector<int>> result(2);
-
-        for (const auto& [playerId, stats] : playerStats) {
-            if (stats.second == 0) {
-                result[0].push_back(playerId);
-            } else if (stats.second == 1) {
-                result[1].push_back(playerId);
-            }
+        vector<vector<int>> result;
+        for (int i = 0; i < nums.size(); i += 3) {
+            result.push_back({ nums[i], nums[i + 1], nums[i + 2] });
         }
 
-        sort(result[0].begin(), result[0].end());
-        sort(result[1].begin(), result[1].end());
 
         return result;
     }
