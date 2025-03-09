3f7635a17dd07d771da1386f556381b91fbac84c
Danil Pinchuk <512kb@proton.me>
2025-03-08 23:02:21 -0500

    3477. Fruits Into Baskets II

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,59 +7,18 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<char>> updateBoard(vector<vector<char>>& board, vector<int>& click) {
-        for (const auto& c : click) {
-            int row = click[0];
-            int col = click[1];
-
-            if (board[row][col] == 'M') {
-                board[row][col] = 'X';
-                break;
-            }
-
-            reveal(board, row, col);
-        }
-
-        return board;
-    }
-private:
-    void reveal(vector<vector<char>>& board, int row, int col) {
-        if (row < 0 || row == board.size() || col < 0 || col == board.front().size()) { return; }
-        char& currCell = board[row][col];
-        if (currCell == 'B' || (currCell >= '1' && currCell <= '8')) { return; }
-
-        int minesAround = countMinesAround(board, row, col);
-
-        if (minesAround) {
-            currCell = '0' + minesAround;
-        } else {
-            currCell = 'B';
-
-            for (int i = -1; i <= 1; ++i) {
-                for (int j = -1; j <= 1; ++j) {
-                    if (i == 0 && j == 0) { continue; }
-
-                    reveal(board, row + i, col + j);
-                }
-            }
-        }
-    }
-
-    int countMinesAround(const vector<vector<char>>& board, int row, int col) {
-        int result = 0;
-
-        for (int i = -1; i <= 1; ++i) {
-            for (int j = -1; j <= 1; ++j) {
-                int newRow = row + i;
-                int newCol = col + j;
-
-                if (newRow >= 0 && newRow < board.size() && newCol >= 0 && newCol < board.front().size()) {
-                    result += board[newRow][newCol] == 'M';
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        for (int i = 0; i < fruits.size(); ++i) {
+            for (int j = 0; j < baskets.size(); ++j) {
+                if (fruits[i] <= baskets[j]) {
+                    fruits[i] = 0;
+                    baskets[j] = 0;
+                    break;
                 }
             }
         }
 
-        return result;
+        return count_if(fruits.begin(), fruits.end(), [](int val) { return val > 0; });
     }
 };
 
