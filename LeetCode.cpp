148cc0ea660206bf21a70db23d0b850c03da5a4a
Danil Pinchuk <512kb@proton.me>
2025-03-10 09:00:35 -0400

    113. Path Sum II

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,37 +7,28 @@ using namespace std;
 
 class Solution {
 public:
-    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
-        int sz = 1;
-        while (sz < baskets.size()) { sz *= 2; }
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> paths;
+        vector<int> path;
 
-        vector<int> segmentTree(2 * sz);
-        copy(baskets.begin(), baskets.end(), segmentTree.begin() + sz);
-        for (int i = sz - 1; i > 0; --i) { segmentTree[i] = max(segmentTree[2 * i], segmentTree[2 * i + 1]); }
+        buildPathSum(root, paths, path, 0, targetSum);
 
-        int ans = 0;
-        for (int f : fruits) {
-            if (segmentTree[1] < f) {
-                ++ans;
-                continue;
-            }
+        return paths;
+    }
+private:
+    void buildPathSum(TreeNode* node, vector<vector<int>>& paths, vector<int>& path, int currentSum, int targetSum) {
+        if (!node) { return; }
 
-            int index = 1;
-            while (index < sz) {
-                if (segmentTree[2 * index] >= f) { index *= 2; } else { index = 2 * index + 1; }
-            }
+        currentSum += node->val;
+        path.push_back(node->val);
+        buildPathSum(node->left, paths, path, currentSum, targetSum);
+        buildPathSum(node->right, paths, path, currentSum, targetSum);
 
-            segmentTree[index] = 0;
-
-            while (index > 1) {
-                index /= 2;
-                int newVal = max(segmentTree[2 * index], segmentTree[2 * index + 1]);
-
-                if (newVal != segmentTree[index]) { segmentTree[index] = newVal; } else { break; }
-            }
+        if (!node->left && !node->right && currentSum == targetSum) {
+            paths.push_back(path);
         }
 
-        return ans;
+        path.pop_back();
     }
 };
 
