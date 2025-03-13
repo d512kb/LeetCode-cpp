933cec3d3dd2f2d8ffeb2a337a46192077f34697
Danil Pinchuk <512kb@proton.me>
2025-03-13 04:18:05 -0400

    2221. Find Triangular Sum of an Array

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,27 +7,14 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> sequentialDigits(int low, int high) {
-        vector<int> result;
-
-        for (int width = 2; width < 10; ++width) {
-            int num = 0;
-            int inc = 0;
-
-            for (int s = 1; s <= width; ++s) {
-                num = num * 10 + s;
-                inc = inc * 10 + 1;
+    int triangularSum(vector<int>& nums) {
+        for (int j = nums.size() - 1; j > 0; --j) {
+            for (int i = 0; i < j; ++i) {
+                nums[i] = (nums[i] + nums[i + 1]) % 10;
             }
-
-            while (num <= high && num % 10 != 0) {
-                if (num >= low) { result.push_back(num); }
-
-                num += inc;
-            }
-
         }
 
-        return result;
+        return nums[0];
     }
 };
 
