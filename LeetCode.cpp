e60c476963bf2bb454515650a198028af073f854
Danil Pinchuk <512kb@proton.me>
2025-03-14 03:06:52 -0400

    2222. Number of Ways to Select Buildings

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,63 +7,27 @@ using namespace std;
 
 class Solution {
 public:
-    int calculate(string s) {
-        vector<int> calc{ 0 }; // first item is a placeholder
-        int num = 0;
+    long long numberOfWays(string s) {
+        long long ans = 0;
+
+        int zeroesBefore = 0;
+        int onesBefore = 0;
+        long long zeroChains = 0;
+        long long oneChains = 0;
 
         for (char c : s) {
-            if (c == ' ') { continue; }
-
-            if (c >= '0' && c <= '9') {
-                num = num * 10 + (c - '0');
-            } else { // it's time to use num
-                int op = calc.back();
-
-                if (op == '/' || op == '*') {
-                    calc.pop_back();
-
-                    if (op == '/') {
-                        calc.back() /= num;
-                    } else {
-                        calc.back() *= num;
-                    }
-                } else {
-                    calc.push_back(num);
-                }
-
-                num = 0;
-                calc.push_back(c);
-            }
-        }
-
-        int op = calc.back();
-        if (op == '/' || op == '*') {
-            calc.pop_back();
-
-            if (op == '/') {
-                calc.back() /= num;
+            if (c == '1') {
+                ans += zeroChains;
+                oneChains += zeroesBefore;
+                ++onesBefore;
             } else {
-                calc.back() *= num;
-            }
-        } else {
-            calc.push_back(num);
-        }
-
-        const int calcSz = calc.size();
-
-        for (int i = 1; i + 2 < calcSz; i += 2) {
-            int left = calc[i];
-            int op = calc[i + 1];
-            int& right = calc[i + 2];
-
-            if (op == '+') {
-                right = left + right;
-            } else {
-                right = left - right;
+                ans += oneChains;
+                zeroChains += onesBefore;
+                ++zeroesBefore;
             }
         }
 
-        return calc.back();
+        return ans;
     }
 };
 
