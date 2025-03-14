a3ca152ef13fe7cfcd4b621da34c67e1d694f2d0
Danil Pinchuk <512kb@proton.me>
2025-03-14 02:43:13 -0400

    227. Basic Calculator II

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,14 +7,63 @@ using namespace std;
 
 class Solution {
 public:
-    int triangularSum(vector<int>& nums) {
-        for (int j = nums.size() - 1; j > 0; --j) {
-            for (int i = 0; i < j; ++i) {
-                nums[i] = (nums[i] + nums[i + 1]) % 10;
+    int calculate(string s) {
+        vector<int> calc{ 0 }; // first item is a placeholder
+        int num = 0;
+
+        for (char c : s) {
+            if (c == ' ') { continue; }
+
+            if (c >= '0' && c <= '9') {
+                num = num * 10 + (c - '0');
+            } else { // it's time to use num
+                int op = calc.back();
+
+                if (op == '/' || op == '*') {
+                    calc.pop_back();
+
+                    if (op == '/') {
+                        calc.back() /= num;
+                    } else {
+                        calc.back() *= num;
+                    }
+                } else {
+                    calc.push_back(num);
+                }
+
+                num = 0;
+                calc.push_back(c);
             }
         }
 
-        return nums[0];
+        int op = calc.back();
+        if (op == '/' || op == '*') {
+            calc.pop_back();
+
+            if (op == '/') {
+                calc.back() /= num;
+            } else {
+                calc.back() *= num;
+            }
+        } else {
+            calc.push_back(num);
+        }
+
+        const int calcSz = calc.size();
+
+        for (int i = 1; i + 2 < calcSz; i += 2) {
+            int left = calc[i];
+            int op = calc[i + 1];
+            int& right = calc[i + 2];
+
+            if (op == '+') {
+                right = left + right;
+            } else {
+                right = left - right;
+            }
+        }
+
+        return calc.back();
     }
 };
 
