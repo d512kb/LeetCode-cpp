612518ab25897222059fa630dc3d7d1c4a499b76
Danil Pinchuk <512kb@proton.me>
2025-03-16 02:12:01 -0400

    3488. Closest Equal Element Queries

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,16 +7,34 @@ using namespace std;
 
 class Solution {
 public:
-    int maxSum(vector<int>& nums) {
-        int result = *max_element(nums.begin(), nums.end());
-        if (result < 0) { return result; }
-        result = 0;
+    vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
+        const int sz = nums.size();
 
-        unordered_set<int> nset;
+        unordered_map<int, vector<int>> positions;
 
-        for (int n : nums) {
-            if (n > 0 && nset.insert(n).second) {
-                result += n;
+        for (int i = 0; i < sz; ++i) {
+            positions[nums[i]].push_back(i);
+        }
+
+        vector<int> result;
+        for (int q : queries) {
+            const auto& pos = positions[nums[q]];
+
+            if (pos.size() < 2) {
+                result.push_back(-1);
+            } else {
+                int idx = distance(pos.begin(), lower_bound(pos.begin(), pos.end(), q));
+                int val = 0;
+
+                if (idx == 0) {
+                    val = min(pos[idx + 1] - q, sz - pos.back() + q);
+                } else if (idx == pos.size() - 1) {
+                    val = min(q - pos[idx - 1], sz - pos.back() + pos.front());
+                } else {
+                    val = min(q - pos[idx - 1], pos[idx + 1] - q);
+                }
+
+                result.push_back(val);
             }
         }
 
