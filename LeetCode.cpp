984fc1f5700d73bbcd24dcff9a0bef0f28025bd4
Danil Pinchuk <512kb@proton.me>
2025-03-15 21:54:48 -0400

    284. Peeking Iterator

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -5,21 +5,48 @@
 
 using namespace std;
 
-class Solution {
+class Iterator {
 public:
-    int repeatedStringMatch(string a, string b) {
-        string text = a + a;
+	Iterator(const vector<int>& nums) {};
+	
+	// Returns the next element in the iteration.
+	int next() {};
+	
+	// Returns true if the iteration has more elements.
+	bool hasNext() const {};
+	
+};
 
-        while (text.size() < b.size() * 2) {
-            text += text;
-        }
+class PeekingIterator : public Iterator {
+public:
+	PeekingIterator(const vector<int>& nums) : Iterator(nums), m_peekIterator(nums) {
+		// Initialize any member here.
+		// **DO NOT** save a copy of nums and manipulate it directly.
+		// You should only use the Iterator interface methods.
 
-        int pos = text.find(b);
-        if (pos == string::npos) { return -1; }
+		m_peekValue = m_peekIterator.next();
+	}
 
-        float len = pos + b.size();
-        return ceil(len / a.size());
-    }
+	// Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+		return m_peekValue;
+	}
+
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+		if (m_peekIterator.hasNext()) {
+			m_peekValue = m_peekIterator.next();
+		}
+		return Iterator::next();
+	}
+
+	bool hasNext() const {
+		return Iterator::hasNext();
+	}
+private:
+	Iterator m_peekIterator;
+	int m_peekValue;
 };
 
 int main()
