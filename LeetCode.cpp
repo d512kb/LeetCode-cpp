b7ed89c2e5e2637d7a8ce50825218ea2466ab8e4
Danil Pinchuk <512kb@proton.me>
2025-03-16 02:25:42 -0400

    844. Backspace String Compare

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,38 +7,30 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
-        const int sz = nums.size();
+    bool backspaceCompare(string s, string t) {
+        int sIndex = s.size() - 1;
+        int tIndex = t.size() - 1;
+        int sSkip = 0;
+        int tSkip = 0;
 
-        unordered_map<int, vector<int>> positions;
-
-        for (int i = 0; i < sz; ++i) {
-            positions[nums[i]].push_back(i);
-        }
-
-        vector<int> result;
-        for (int q : queries) {
-            const auto& pos = positions[nums[q]];
-
-            if (pos.size() < 2) {
-                result.push_back(-1);
-            } else {
-                int idx = distance(pos.begin(), lower_bound(pos.begin(), pos.end(), q));
-                int val = 0;
-
-                if (idx == 0) {
-                    val = min(pos[idx + 1] - q, sz - pos.back() + q);
-                } else if (idx == pos.size() - 1) {
-                    val = min(q - pos[idx - 1], sz - pos.back() + pos.front());
-                } else {
-                    val = min(q - pos[idx - 1], pos[idx + 1] - q);
-                }
-
-                result.push_back(val);
+        while (true) {
+            while (sIndex >= 0 && (s[sIndex] == '#' || sSkip > 0)) {
+                sSkip += s[sIndex] == '#' ? 1 : -1;
+                --sIndex;
             }
+
+            while (tIndex >= 0 && (t[tIndex] == '#' || tSkip > 0)) {
+                tSkip += t[tIndex] == '#' ? 1 : -1;
+                --tIndex;
+            }
+
+            if (sIndex < 0 && tIndex < 0) { return true; }
+            if (sIndex < 0 || tIndex < 0 || s[sIndex] != t[tIndex]) { return false; }
+            --sIndex;
+            --tIndex;
         }
 
-        return result;
+        return true;
     }
 };
 
