8574714c4b67aa30ca404480833bfdf5af21ebc3
Danil Pinchuk <512kb@proton.me>
2025-03-16 01:58:56 -0400

    3489. Zero Array Transformation IV

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,19 +7,35 @@ using namespace std;
 
 class Solution {
 public:
-    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
-        const int rows = matrix.size();
-        const int cols = matrix.front().size();
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int ans = 0;
+        const int qsz = queries.size();
 
-        for (int row = 1; row < rows; ++row) {
-            for (int col = 1; col < cols; ++col) {
-                if (matrix[row - 1][col - 1] != matrix[row][col]) {
-                    return false;
+        for (int numIndex = 0; numIndex < nums.size(); ++numIndex) {
+            if (nums[numIndex] == 0) { continue; }
+
+            int n = nums[numIndex];
+            vector<char> dp(n + 1);
+            dp[0] = 1;
+
+            for (int i = 0; i < qsz; ++i) {
+                if (queries[i][0] > numIndex || queries[i][1] < numIndex) { continue; }
+                int qVal = queries[i][2];
+
+                for (int sum = n - qVal; sum >= 0; --sum) {
+                    dp[sum + qVal] |= dp[sum];
+                }
+
+                if (dp[n] == 1) {
+                    ans = max(ans, i + 1);
+                    break;
                 }
             }
+
+            if (dp[n] == 0) { return -1; }
         }
 
-        return true;
+        return ans;
     }
 };
 
