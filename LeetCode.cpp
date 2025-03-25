63c88556f791c8ae85f127d157f5c826403054fd
Danil Pinchuk <512kb@proton.me>
2025-03-25 18:49:25 -0400

    2772. Apply Operations to Make All Array Elements Equal to Zero

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,8 +7,17 @@ using namespace std;
 
 class Solution {
 public:
-    int maxContainers(int n, int w, int maxWeight) {
-        return min(n * n, maxWeight / w);
+    bool checkArray(vector<int>& nums, int k) {
+        const int sz = nums.size();
+        int corr = 0;
+
+        for (int i = 0; i < sz; ++i) {
+            if ((nums[i] -= corr) < 0) { return false; }
+            corr += nums[i];
+            if (i - k + 1 >= 0) { corr -= nums[i - k + 1]; }
+        }
+
+        return corr == 0;
     }
 };
 
