cddb15ef67a934026f6fb2c16e5362d140ed12e0
Danil Pinchuk <512kb@proton.me>
2025-04-13 23:10:05 -0400

    2800. Shortest String That Contains Three Strings

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,113 +6,40 @@ using namespace std;
 
 class Solution {
 public:
-    int countNumbers(string l, string r, int b) {
-        constexpr int modulo = 1e9 + 7;
+    string minimumString(string a, string b, string c) {
+        array<reference_wrapper<string>, 3> strings{ a, b, c };
+        array<char, 3> indexes{ 0, 1, 2 };
+        string result = a + b + c;
 
-        l = convertToBase(l, b);
-        r = convertToBase(r, b);
+        do {
+            string nextResult = generateString(strings, indexes);
 
-        // find next lower increasing number
-        for (int i = 1; i < l.size(); ++i) {
-            if (l[i - 1] > l[i]) {
-                for (i; i < l.size(); ++i) {
-                    l[i] = l[i - 1];
-                }
-
-                break;
+            if (nextResult.size() == result.size()) {
+                result = min(result, nextResult);
+            } else if (nextResult.size() < result.size()) {
+                result = move(nextResult);
             }
-        }
+        } while (next_permutation(indexes.begin(), indexes.end()));
 
-        // find prev upper increasing number
-        for (int i = 1; i < r.size(); ++i) {
-            if (r[i - 1] > r[i]) { // found point of decreasing, decrease left side of the number
-                while (i > 0 && r[i - 1] > r[i]) {
-                    --r[--i];
-                }
-
-                // fill the right part of the number with max values for the base
-                for (++i; i < r.size(); ++i) {
-                    r[i] = b - 1 + '0';
-                }
-
-                r.erase(r.begin(), r.begin() + r.find_first_not_of('0')); // remove leading zeros
-
-                break;
-            }
-        }
-
-        if (l.size() > r.size()) { return 0; }
-
-        // pair->first for unrestricted digit, pair->second for restricted
-        vector<vector<pair<int, int>>> dpLeft(l.size() + 1, vector<pair<int, int>>(b));
-        vector<vector<pair<int, int>>> dpRight(r.size() + 1, vector<pair<int, int>>(b));
-        fill(dpLeft[0].begin(), dpLeft[0].end(), make_pair(1, 1));
-        fill(dpRight[0].begin(), dpRight[0].end(), make_pair(1, 1));
-
-        for (int n = 1, li = l.size() - 1; n <= l.size(); ++n, --li) {
-            int val = 0;
-
-            // fill unrestricted values
-            for (int j = b - 1; j >= 0; --j) {
-                val = (val + dpLeft[n - 1][j].first) % modulo;
-                dpLeft[n][j].first = val;
-            }
-
-            // fill restricted values
-            val = dpLeft[n - 1][l[li] - '0'].second;
-            dpLeft[n][l[li] - '0'].second = val;
-
-            for (int j = l[li] - '0' - 1; j >= 0; --j) {
-                val = (val + dpLeft[n - 1][j].first) % modulo;
-                dpLeft[n][j].second = val;
-            }
-        }
-
-        for (int n = 1, ri = r.size() - 1; n <= r.size(); ++n, --ri) {
-            int val = 0;
-
-            // fill unrestricted values
-            for (int j = b - 1; j >= 0; --j) {
-                val = (val + dpRight[n - 1][j].first) % modulo;
-                dpRight[n][j].first = val;
-            }
-
-            // fill restricted values
-            val = dpRight[n - 1][r[ri] - '0'].second;
-            dpRight[n][r[ri] - '0'].second = val;
-
-            for (int j = r[ri] - '0' - 1; j >= 0; --j) {
-                val = (val + dpRight[n - 1][j].first) % modulo;
-                dpRight[n][j].second = val;
-            }
-        }
-
-        return (dpRight.back()[0].second - dpLeft.back()[0].second + 1 + modulo) % modulo;
+        return result;
     }
 private:
-    string convertToBase(string num, int base) {
-        if (num.empty() || num == "0") { return "0"; }
+    string generateString(const array<reference_wrapper<string>, 3>& strings, const array<char, 3>& indexes) {
+        string result = strings[indexes[0]];
 
-        string result;
+        for (int i = 1; i < indexes.size(); ++i) {
+            if (result.find(strings[indexes[i]]) != string::npos) { continue; }
 
-        while (!num.empty()) {
-            string quotient;
-            int n = 0;
+            const auto& nextString = strings[indexes[i]].get();
 
-            for (char c : num) {
-                n = n * 10 + c - '0';
-
-                if (!quotient.empty() || n >= base) {
-                    quotient.push_back((n / base) + '0');
-                    n = n % base;
+            for (int len = min(nextString.size(), result.size()); len >= 0; --len) {
+                if (equal(result.end() - len, result.end(), nextString.begin())) {
+                    result.append(nextString, len);
+                    break;
                 }
             }
-
-            num = std::move(quotient);
-            result.push_back(n + '0');
         }
 
-        reverse(result.begin(), result.end());
         return result;
     }
 };
