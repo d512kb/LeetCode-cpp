d05485e4e305c4f55338089d248793924b9fd078
Danil Pinchuk <512kb@proton.me>
2025-04-23 01:15:47 -0400

    2779. Maximum Beauty of an Array After Applying Operation

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,41 +6,26 @@ using namespace std;
 
 class Solution {
 public:
-    string minimumString(string a, string b, string c) {
-        array<reference_wrapper<string>, 3> strings{ a, b, c };
-        array<char, 3> indexes{ 0, 1, 2 };
-        string result = a + b + c;
+    int maximumBeauty(vector<int>& nums, int k) {
+        int maxVal = *max_element(nums.begin(), nums.end());
+        vector<int> values(maxVal + 1);
 
-        do {
-            string nextResult = generateString(strings, indexes);
-
-            if (nextResult.size() == result.size()) {
-                result = min(result, nextResult);
-            } else if (nextResult.size() < result.size()) {
-                result = move(nextResult);
-            }
-        } while (next_permutation(indexes.begin(), indexes.end()));
-
-        return result;
-    }
-private:
-    string generateString(const array<reference_wrapper<string>, 3>& strings, const array<char, 3>& indexes) {
-        string result = strings[indexes[0]];
-
-        for (int i = 1; i < indexes.size(); ++i) {
-            if (result.find(strings[indexes[i]]) != string::npos) { continue; }
-
-            const auto& nextString = strings[indexes[i]].get();
-
-            for (int len = min(nextString.size(), result.size()); len >= 0; --len) {
-                if (equal(result.end() - len, result.end(), nextString.begin())) {
-                    result.append(nextString, len);
-                    break;
-                }
+        for (int n : nums) {
+            ++values[max(0, n - k)];
+            if (n + k + 1 <= maxVal) {
+                --values[n + k + 1];
             }
         }
 
-        return result;
+        int ans = 0;
+        int currentSum = 0;
+
+        for (int val : values) {
+            currentSum += val;
+            ans = max(ans, currentSum);
+        }
+
+        return ans;
     }
 };
 
