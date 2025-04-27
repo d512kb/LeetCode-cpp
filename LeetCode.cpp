20a9b8fdfe92949e0ce03af09c604d3816914796
Danil Pinchuk <512kb@proton.me>
2025-04-27 03:53:24 -0400

    2981. Find Longest Special Substring That Occurs Thrice I

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,24 +6,50 @@ using namespace std;
 
 class Solution {
 public:
-    int maxSubarrayLength(vector<int>& nums, int k) {
-        int left = 0;
-        int ans = 0;
+    int maximumLength(string s) {
+        using SubstringLengths = priority_queue<int, vector<int>, greater<>>;
+        const SubstringLengths defaultLengths(SubstringLengths::value_compare(), { 0, 0, 0 });
+        vector<SubstringLengths> substrings(26, defaultLengths);
+        int substringLen = 1;
 
-        unordered_map<int, int> numsCount;
+        for (int i = 1; i < s.size(); ++i) {
+            if (s[i] == s[i - 1]) {
+                ++substringLen;
+            } else {
+                auto& substr = substrings[s[i - 1] - 'a'];
 
-        for (int right = 0; right < nums.size(); ++right) {
-            int& count = numsCount[nums[right]];
-            ++count;
+                if (substringLen > substr.top()) {
+                    substr.pop();
+                    substr.push(substringLen);
+                }
 
-            while (count > k) {
-                --numsCount[nums[left++]];
+                substringLen = 1;
             }
-
-            ans = max(ans, right - left + 1);
         }
 
-        return ans;
+        if (substringLen > substrings[s.back() - 'a'].top()) {
+            substrings[s.back() - 'a'].pop();
+            substrings[s.back() - 'a'].push(substringLen);
+        }
+
+        int ans = 0;
+
+        for (auto& substr : substrings) {
+            ans = max(ans, substr.top());
+            substr.pop();
+
+            int a = substr.top();
+            substr.pop();
+            int b = substr.top();
+
+            if (a == b) {
+                ans = max(ans, a - 1);
+            } else {
+                ans = max(ans, max(a, b - 2));
+            }
+        }
+
+        return ans == 0 ? -1 : ans;
     }
 };
 
