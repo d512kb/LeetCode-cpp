94d6676af3671dde24e96320a0441c73765f541f
Danil Pinchuk <512kb@proton.me>
2025-04-27 04:27:06 -0400

    3101. Count Alternating Subarrays

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,50 +6,22 @@ using namespace std;
 
 class Solution {
 public:
-    int maximumLength(string s) {
-        using SubstringLengths = priority_queue<int, vector<int>, greater<>>;
-        const SubstringLengths defaultLengths(SubstringLengths::value_compare(), { 0, 0, 0 });
-        vector<SubstringLengths> substrings(26, defaultLengths);
-        int substringLen = 1;
+    long long countAlternatingSubarrays(vector<int>& nums) {
+        const size_t sz = nums.size();
+        long long totalSubarrays = 1;
+        int alternatingLen = 1;
 
-        for (int i = 1; i < s.size(); ++i) {
-            if (s[i] == s[i - 1]) {
-                ++substringLen;
+        for (int i = 1; i < sz; ++i) {
+            if (nums[i - 1] == nums[i]) {
+                alternatingLen = 1;
             } else {
-                auto& substr = substrings[s[i - 1] - 'a'];
-
-                if (substringLen > substr.top()) {
-                    substr.pop();
-                    substr.push(substringLen);
-                }
-
-                substringLen = 1;
+                ++alternatingLen;
             }
+
+            totalSubarrays += alternatingLen;
         }
 
-        if (substringLen > substrings[s.back() - 'a'].top()) {
-            substrings[s.back() - 'a'].pop();
-            substrings[s.back() - 'a'].push(substringLen);
-        }
-
-        int ans = 0;
-
-        for (auto& substr : substrings) {
-            ans = max(ans, substr.top());
-            substr.pop();
-
-            int a = substr.top();
-            substr.pop();
-            int b = substr.top();
-
-            if (a == b) {
-                ans = max(ans, a - 1);
-            } else {
-                ans = max(ans, max(a, b - 2));
-            }
-        }
-
-        return ans == 0 ? -1 : ans;
+        return totalSubarrays;
     }
 };
 
