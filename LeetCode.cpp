6d53bd535dd044f908969f7d14ce0bf722166e14
Danil Pinchuk <512kb@proton.me>
2025-04-29 05:06:10 -0400

    3016. Minimum Number of Pushes to Type Word II

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,30 +6,18 @@ using namespace std;
 
 class Solution {
 public:
-    int rangeSum(vector<int>& nums, int n, int left, int right) {
-        auto cmp = [](const auto& p1, const auto& p2) { return p1.first > p2.first; };
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> pq;
+    int minimumPushes(string word) {
+        array<int, 26> charCount{};
 
-        for (int i = 0; i < n; ++i) {
-            pq.emplace(nums[i], i + 1); // put next index
+        for (char c : word) {
+            ++charCount[c - 'a'];
         }
 
-        constexpr int modulo = 1e9 + 7;
+        sort(charCount.rbegin(), charCount.rend());
         int ans = 0;
-        --left; // make it 0-based
 
-        for (int i = 0; i < right; ++i) {
-            auto val = pq.top();
-            pq.pop();
-
-            if (i >= left) {
-                ans = (ans + val.first) % modulo;
-            }
-
-            if (val.second < n) {
-                val.first += nums[val.second++];
-                pq.push(val);
-            }
+        for (int i = 0; i < 26; ++i) {
+            ans += charCount[i] * (i / 8 + 1);
         }
 
         return ans;
