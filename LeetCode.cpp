1b5293ec9c1140abcfb8cdce5d99eb28b614e897
Danil Pinchuk <512kb@proton.me>
2025-05-24 19:03:14 -0400

    3558. Number of Ways to Assign Edge Weights I

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,20 +6,47 @@ using namespace std;
 
 class Solution {
 public:
-    int maxSubstrings(string word) {
-        const auto sz = word.size();
-        array<int, 26> prevPos;
-        fill(prevPos.begin(), prevPos.end(), -1);
-        int ans = 0;
+    int assignEdgeWeights(vector<vector<int>>& edges) {
+        constexpr int modulo = 1e9 + 7;
+        vector<vector<int>> graph(edges.size() + 2);
 
-        for (int i = 0; i < sz; ++i) {
-            auto& pos = prevPos[word[i] - 'a'];
-            if (pos == -1) {
-                pos = i;
-            } else if (i - pos >= 3) {
-                fill(prevPos.begin(), prevPos.end(), -1);
-                ++ans;
+        for (const auto& edge : edges) {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+
+        vector<bool> visited(graph.size());
+        visited[1] = true;
+
+        int maxPath = getMaxPathLength(graph, visited, 1);
+
+        return fastExp(2, maxPath - 1, modulo);
+    }
+
+private:
+    int getMaxPathLength(const vector<vector<int>>& graph, vector<bool>& visited, int node) {
+        int maxPath = 0;
+
+        for (int child : graph[node]) {
+            if (visited[child]) { continue; }
+            visited[child] = true;
+
+            maxPath = max(maxPath, 1 + getMaxPathLength(graph, visited, child));
+        }
+
+        return maxPath;
+    }
+
+    int fastExp(int64_t base, int64_t exp, int modulo) {
+        int64_t ans = 1;
+
+        while (exp > 0) {
+            if (exp % 2 == 1) {
+                ans = (ans * base) % modulo;
             }
+
+            base = (base * base) % modulo;
+            exp /= 2;
         }
 
         return ans;
