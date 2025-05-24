05c24526644379a56e35d9d1e3494b9816f2fe5c
Danil Pinchuk <512kb@proton.me>
2025-05-24 17:13:15 -0400

    3557. Find Maximum Number of Non Intersecting Substrings

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,39 +6,23 @@ using namespace std;
 
 class Solution {
 public:
-    long long sumOfLargestPrimes(string s) {
-        const auto sz = s.size();
-        set<int64_t, greater<>> primes;
+    int maxSubstrings(string word) {
+        const auto sz = word.size();
+        array<int, 26> prevPos;
+        fill(prevPos.begin(), prevPos.end(), -1);
+        int ans = 0;
 
         for (int i = 0; i < sz; ++i) {
-            int64_t val = 0;
-
-            for (int j = i; j < sz; ++j) {
-                val *= 10;
-                val += s[j] - '0';
-
-                if (isPrime(val))
-                    primes.insert(val);
+            auto& pos = prevPos[word[i] - 'a'];
+            if (pos == -1) {
+                pos = i;
+            } else if (i - pos >= 3) {
+                fill(prevPos.begin(), prevPos.end(), -1);
+                ++ans;
             }
         }
 
-        if (primes.size() < 3) {
-            return accumulate(primes.begin(), primes.end(), 0ll);
-        }
-
-        return accumulate(primes.begin(), next(primes.begin(), 3), 0ll);
-    }
-private:
-    bool isPrime(int64_t val) {
-        if (val == 1) { return false; }
-
-        for (int64_t i = 2; i * i <= val; ++i) {
-            if (val % i == 0) {
-                return false;
-            }
-        }
-
-        return true;
+        return ans;
     }
 };
 
