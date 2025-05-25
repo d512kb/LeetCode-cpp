6884bec18db7b0b8e074b15750fac7a61a58b703
Danil Pinchuk <512kb@proton.me>
2025-05-25 00:04:48 -0400

    3560. Find Minimum Log Transportation Cost

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,50 +6,12 @@ using namespace std;
 
 class Solution {
 public:
-    int assignEdgeWeights(vector<vector<int>>& edges) {
-        constexpr int modulo = 1e9 + 7;
-        vector<vector<int>> graph(edges.size() + 2);
-
-        for (const auto& edge : edges) {
-            graph[edge[0]].push_back(edge[1]);
-            graph[edge[1]].push_back(edge[0]);
+    long long minCuttingCost(int n, int m, int k) {
+        if (n <= k && m <= k) { return 0; }
+        if (n > k) {
+            return static_cast<int64_t>(k) * (n - k);
         }
-
-        vector<bool> visited(graph.size());
-        visited[1] = true;
-
-        int maxPath = getMaxPathLength(graph, visited, 1);
-
-        return fastExp(2, maxPath - 1, modulo);
-    }
-
-private:
-    int getMaxPathLength(const vector<vector<int>>& graph, vector<bool>& visited, int node) {
-        int maxPath = 0;
-
-        for (int child : graph[node]) {
-            if (visited[child]) { continue; }
-            visited[child] = true;
-
-            maxPath = max(maxPath, 1 + getMaxPathLength(graph, visited, child));
-        }
-
-        return maxPath;
-    }
-
-    int fastExp(int64_t base, int64_t exp, int modulo) {
-        int64_t ans = 1;
-
-        while (exp > 0) {
-            if (exp % 2 == 1) {
-                ans = (ans * base) % modulo;
-            }
-
-            base = (base * base) % modulo;
-            exp /= 2;
-        }
-
-        return ans;
+        return static_cast<int64_t>(k) * (m - k);
     }
 };
 
