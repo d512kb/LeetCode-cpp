972b32829014966f497d5d5b78d0604bea9bfeb6
Danil Pinchuk <512kb@proton.me>
2025-05-27 00:38:05 -0400

    641. Design Circular Deque

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -4,25 +4,76 @@
 
 using namespace std;
 
-class Solution {
+class MyCircularDeque {
 public:
-    string resultingString(string s) {
-        string result;
+    MyCircularDeque(int k) : m_size(k), m_capacity(k), m_begin(0), m_end(0), m_data(k) {
 
-        for (char c : s) {
-            if (!result.empty() && adjacent(result.back(), c)) {
-                result.pop_back();
-            } else {
-                result.push_back(c);
-            }
-        }
-
-        return result;
     }
+
+    bool insertFront(int value) {
+        if (isFull()) { return false; }
+        --m_capacity;
+
+        m_begin = (m_begin - 1 + m_size) % m_size;
+        m_data[m_begin] = value;
+
+        return true;
+    }
+
+    bool insertLast(int value) {
+        if (isFull()) { return false; }
+        --m_capacity;
+
+        m_data[m_end] = value;
+        m_end = (m_end + 1) % m_size;
+
+        return true;
+    }
+
+    bool deleteFront() {
+        if (isEmpty()) { return false; }
+        ++m_capacity;
+
+        m_begin = (m_begin + 1) % m_size;
+
+        return true;
+    }
+
+    bool deleteLast() {
+        if (isEmpty()) { return false; }
+        ++m_capacity;
+
+        m_end = (m_end - 1 + m_size) % m_size;
+
+        return true;
+    }
+
+    int getFront() const {
+        if (isEmpty()) { return -1; }
+
+        return m_data[m_begin];
+    }
+
+    int getRear() const {
+        if (isEmpty()) { return -1; }
+
+        return m_data[(m_end - 1 + m_size) % m_size];
+    }
+
+    inline bool isEmpty() const {
+        return m_capacity == m_size;
+    }
+
+    inline bool isFull() const {
+        return m_capacity == 0;
+    }
+
 private:
-    inline bool adjacent(char a, char b) const {
-        return abs(a - b) == 1 || abs(a - b) == 25;
-    }
+    const size_t m_size;
+    int m_capacity;
+    int m_begin;
+    int m_end;
+    vector<int> m_data;
 };
 
 int main()
