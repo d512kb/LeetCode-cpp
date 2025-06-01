ebdd231ae9a0f9e45f9a5ee0063f38755e424e74
Danil Pinchuk <512kb@proton.me>
2025-06-01 00:05:45 -0400

    3566. Partition Array into Two Equal Product Subsets

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,54 +6,36 @@ using namespace std;
 
 class Solution {
 public:
-    int maximumInvitations(vector<int>& favorite) {
-        const auto sz = favorite.size();
-        vector<int> inDegree(sz);
-
-        for (int i = 0; i < sz; ++i) {
-            ++inDegree[favorite[i]];
+    bool checkEqualPartitions(vector<int>& nums, long long target) {
+        for (int n : nums) {
+            if (target % n != 0) { return false; }
         }
 
-        queue<int> q;
-        for (int i = 0; i < sz; ++i) {
-            if (inDegree[i] == 0) {
-                q.push(i);
+        return distribute(nums, target, target);
+    }
+
+private:
+    bool distribute(vector<int>& values, int64_t value, int64_t target) {
+        if (value == 1) {
+            return accumulate(values.begin(), values.end(), 1ll, multiplies<>{}) == target;
+        }
+
+        for (auto iter = values.begin(); iter != values.end(); ++iter) {
+            if (*iter == 1) { continue; }
+
+            if (value % *iter == 0) {
+                auto val = *iter;
+                *iter = 1;
+
+                if (distribute(values, value / val, target)) {
+                    return true;
+                }
+
+                *iter = val;
             }
         }
 
-        vector<int> depth(sz, 1);
-
-        while (!q.empty()) {
-            int node = q.front();
-            int nextNode = favorite[node];
-            q.pop();
-
-            depth[nextNode] = max(depth[nextNode], 1 + depth[node]);
-            if (--inDegree[nextNode] == 0) {
-                q.push(nextNode);
-            }
-        }
-
-        int peopleInChains = 0;
-        int peopleInCycles = 0;
-
-        for (int i = 0; i < sz; ++i) {
-            if (inDegree[i] == 0) { continue; }
-
-            int cycleLength = 0;
-            for (int current = i; inDegree[current] > 0; current = favorite[current]) {
-                inDegree[current] = 0;
-                ++cycleLength;
-            }
-
-            if (cycleLength == 2) {
-                peopleInChains += depth[i] + depth[favorite[i]];
-            } else {
-                peopleInCycles = max(peopleInCycles, cycleLength);
-            }
-        }
-
-        return max(peopleInChains, peopleInCycles);
+        return false;
     }
 };
 
