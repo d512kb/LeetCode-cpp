95b4394e7ff1816a64a8264f841cb15e550ca391
Daniel Pinchuk <512kb@proton.me>
2025-10-16 19:49:29 -0400

    111. Minimum Depth of Binary Tree

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,21 +6,29 @@ using namespace std;
 
 class Solution {
 public:
-    ListNode* deleteDuplicates(ListNode* head) {
-        ListNode* node = head;
+    int minDepth(TreeNode* root) {
+        if (!root) { return 0; }
 
-        while (node && node->next) {
-            if (node->val == node->next->val) {
-                ListNode* n = node->next;
-                node->next = node->next->next;
-                delete n;
+        queue<TreeNode*> q;
+        q.push(root);
+        int ans = 1;
+
+        while (!q.empty()) {
+            int sz = q.size();
+
+            while (sz--) {
+                auto node = q.front();
+                q.pop();
+
+                if (!node->left && !node->right) { return ans; }
+                if (node->left) { q.push(node->left); }
+                if (node->right) { q.push(node->right); }
             }
-            else {
-                node = node->next;
-            }
+
+            ++ans;
         }
 
-        return head;
+        return ans;
     }
 };
 
