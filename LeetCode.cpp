6e8a0e5fe458e9a685329c690fad6c5a8daf7920
Daniel Pinchuk <512kb@proton.me>
2025-10-16 21:49:39 -0400

    2769. Find the Maximum Achievable Number

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,14 +6,8 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> buildArray(vector<int>& nums) {
-        vector<int> ans(nums.size());
-
-        for (int i = 0; i < nums.size(); ++i) {
-            ans[i] = nums[nums[i]];
-        }
-
-        return ans;
+    int theMaximumAchievableX(int num, int t) {
+        return num + t * 2;
     }
 };
 
