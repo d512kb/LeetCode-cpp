a924a0d4069b3e3679b9e520b2ed3b3aec196e5b
Daniel Pinchuk <512kb@proton.me>
2025-10-16 21:52:32 -0400

    2942. Find Words Containing Character

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,10 +6,12 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> getConcatenation(vector<int>& nums) {
-        vector<int> result(nums);
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> result;
 
-        copy(nums.begin(), nums.end(), back_inserter(result));
+        for (int i = 0; i < words.size(); ++i) {
+            if (words[i].contains(x)) { result.push_back(i); }
+        }
 
         return result;
     }
