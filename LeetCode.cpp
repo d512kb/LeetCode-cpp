38108c1cf3a6d74b668735bd5a93ca42ec898dfe
Daniel Pinchuk <512kb@proton.me>
2025-10-18 00:56:54 -0400

    2574. Left and Right Sum Differences

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,8 +6,19 @@ using namespace std;
 
 class Solution {
 public:
-    int minBitFlips(int start, int goal) {
-        return popcount(static_cast<size_t>(start ^ goal));
+    vector<int> leftRightDifference(vector<int>& nums) {
+        vector<int> rightSums(nums.size());
+        partial_sum(nums.rbegin(), nums.rend() - 1, rightSums.rbegin() + 1);
+        int leftSum = 0;
+
+        vector<int> result;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            result.push_back(abs(leftSum - rightSums[i]));
+            leftSum += nums[i];
+        }
+
+        return result;
     }
 };
 
