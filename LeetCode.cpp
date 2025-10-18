6ec8a0b0ceb21410dc3789911d2a584f39dca55b
Daniel Pinchuk <512kb@proton.me>
2025-10-18 00:25:11 -0400

    2798. Number of Employees Who Met the Target

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,27 +6,8 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
-        const int n = grid.size();
-        vector<vector<int>> result;
-
-        for (int row = 1; row < n - 1; ++row) {
-            result.emplace_back();
-
-            for (int col = 1; col < n - 1; ++col) {
-                int maxVal = 0;
-
-                for (int i = -1; i <= 1; ++i) {
-                    for (int j = -1; j <= 1; ++j) {
-                        maxVal = max(maxVal, grid[row + i][col + j]);
-                    }
-                }
-
-                result.back().push_back(maxVal);
-            }
-        }
-
-        return result;
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        return count_if(hours.begin(), hours.end(), [target](int val) { return val >= target; });
     }
 };
 
