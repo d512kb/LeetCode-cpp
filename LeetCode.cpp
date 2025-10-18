087d5500ce08b8ec4c0ce3c247daa6d4616e2ccb
Daniel Pinchuk <512kb@proton.me>
2025-10-18 00:12:33 -0400

    3498. Reverse Degree of a String

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,9 +6,14 @@ using namespace std;
 
 class Solution {
 public:
-    int smallestEvenMultiple(int n) {
-        if (n % 2)  return 2 * n;
-        return n;
+    int reverseDegree(string s) {
+        int ans = 0;
+
+        for (int i = 0; i < s.size(); ++i) {
+            ans += ('z' - s[i] + 1) * (i + 1);
+        }
+
+        return ans;
     }
 };
 
