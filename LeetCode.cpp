21361c2e1ff77fdfbdeb56f9c85a96a42fbd8963
Daniel Pinchuk <512kb@proton.me>
2025-10-20 15:25:00 -0400

    2652. Sum Multiples

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,8 +6,16 @@ using namespace std;
 
 class Solution {
 public:
-    bool isStrictlyPalindromic(int n) {
-        return false;
+    int sumOfMultiples(int n) {
+        int ans = 0;
+
+        for (int i = 1; i <= n; ++i) {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+                ans += i;
+            }
+        }
+
+        return ans;
     }
 };
 
