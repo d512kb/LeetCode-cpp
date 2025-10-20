37baad4b129c7f922042afc642574163ba949f87
Daniel Pinchuk <512kb@proton.me>
2025-10-20 15:32:40 -0400

    1534. Count Good Triplets

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,12 +6,16 @@ using namespace std;
 
 class Solution {
 public:
-    int sumOfMultiples(int n) {
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
         int ans = 0;
 
-        for (int i = 1; i <= n; ++i) {
-            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
-                ans += i;
+        for (int i = 0; i < arr.size() - 2; ++i) {
+            for (int j = i + 1; j < arr.size() - 1; ++j) {
+                for (int k = j + 1; k < arr.size(); ++k) {
+                    if (abs(arr[i] - arr[j]) <= a && abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c) {
+                        ++ans;
+                    }
+                }
             }
         }
 
