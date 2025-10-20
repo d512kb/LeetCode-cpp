6df53f6c307eb7298b99fb9090ec2fdbbfcbc9c1
Daniel Pinchuk <512kb@proton.me>
2025-10-19 20:17:23 -0400

    2000. Reverse Prefix of Word

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,16 +6,14 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> stableMountains(vector<int>& height, int threshold) {
-        vector<int> result;
+    string reversePrefix(string word, char ch) {
+        auto iter = find(word.begin(), word.end(), ch);
 
-        for (int i = 1; i < height.size(); ++i) {
-            if (height[i - 1] > threshold) {
-                result.push_back(i);
-            }
+        if (iter != word.end()) {
+            reverse(word.begin(), iter + 1);
         }
 
-        return result;
+        return word;
     }
 };
 
