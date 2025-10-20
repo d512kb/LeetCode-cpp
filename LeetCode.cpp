00eadff4a380e7447b5b3ea92cd883224bc859af
Daniel Pinchuk <512kb@proton.me>
2025-10-20 16:38:57 -0400

    3427. Sum of Variable Length Subarrays

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,17 +6,14 @@ using namespace std;
 
 class Solution {
 public:
-    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+    int subarraySum(vector<int>& nums) {
         int ans = 0;
 
-        for (int i = 0; i < arr.size() - 2; ++i) {
-            for (int j = i + 1; j < arr.size() - 1; ++j) {
-                for (int k = j + 1; k < arr.size(); ++k) {
-                    if (abs(arr[i] - arr[j]) <= a && abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c) {
-                        ++ans;
-                    }
-                }
-            }
+        vector<int> prefixSum(nums.size() + 1);
+        partial_sum(nums.begin(), nums.end(), prefixSum.begin() + 1);
+
+        for (int i = 0; i < nums.size(); ++i) {
+            ans += prefixSum[i + 1] - prefixSum[max(0, i - nums[i])];
         }
 
         return ans;
