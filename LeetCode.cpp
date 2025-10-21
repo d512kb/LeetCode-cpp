1a5347712241854f357e7f65ae84516c07df7a71
Daniel Pinchuk <512kb@proton.me>
2025-10-20 23:50:58 -0400

    2956. Find Common Elements Between Two Arrays

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,21 +6,26 @@ using namespace std;
 
 class Solution {
 public:
-    int minElement(vector<int>& nums) {
-        int ans = numeric_limits<int>::max();
+    vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> nums1Set(nums1.begin(), nums1.end());
+        unordered_set<int> nums2Set(nums2.begin(), nums2.end());
 
-        for (int n : nums) {
-            int val = 0;
+        int ans1 = 0;
+        int ans2 = 0;
 
-            while (n) {
-                val += n % 10;
-                n /= 10;
+        for (int n : nums1) {
+            if (nums2Set.contains(n)) {
+                ++ans1;
             }
-
-            ans = min(ans, val);
         }
 
-        return ans;
+        for (int n : nums2) {
+            if (nums1Set.contains(n)) {
+                ++ans2;
+            }
+        }
+
+        return { ans1, ans2 };
     }
 };
 
