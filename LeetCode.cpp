3a702ab32f9fa8f2e9942a841769c0cf424249c3
Daniel Pinchuk <512kb@proton.me>
2025-10-20 23:25:06 -0400

    2236. Root Equals Sum of Children

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,18 +6,8 @@ using namespace std;
 
 class Solution {
 public:
-    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
-        unordered_map<string, size_t> rulesIndexes{ {"type", 0}, {"color", 1}, {"name", 2} };
-        const size_t ruleIndex = rulesIndexes[ruleKey];
-
-        int ans = 0;
-        for (int i = 0; i < items.size(); ++i) {
-            if (items[i][ruleIndex] == ruleValue) {
-                ++ans;
-            }
-        }
-
-        return ans;
+    bool checkTree(TreeNode* root) {
+        return root->val == root->left->val + root->right->val;
     }
 };
 
