8f15afc0dfc650e309612293f2e6d225d1ea6b43
Daniel Pinchuk <512kb@proton.me>
2025-10-20 23:43:56 -0400

    709. To Lower Case

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,17 +6,12 @@ using namespace std;
 
 class Solution {
 public:
-    int maximum69Number(int num) {
-        auto number = to_string(num);
-
-        for (char& c : number) {
-            if (c == '6') {
-                c = '9';
-                break;
-            }
+    string toLowerCase(string s) {
+        for (char& c : s) {
+            c = tolower(c);
         }
 
-        return stoi(number);
+        return s;
     }
 };
 
