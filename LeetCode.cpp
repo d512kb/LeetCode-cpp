19d81ca88b21c13d31541efe2c35d06a9913bcc2
Daniel Pinchuk <512kb@proton.me>
2025-10-26 03:40:50 -0400

    2315. Count Asterisks

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,53 +6,18 @@ using namespace std;
 
 class Solution {
 public:
-    int countSymmetricIntegers(int low, int high) {
+    int countAsterisks(string s) {
         int ans = 0;
+        bool inPair = false;
 
-        for (low; low <= high; ++low) {
-            int digits = countDigits(low);
-            if (digits % 2) {
-                int newStep = 0;
-
-                while (digits) {
-                    newStep = newStep * 10 + 9;
-                    --digits;
-                }
-                low = newStep;
-
-                continue;
+        for (char c : s) {
+            if (c == '|') {
+                inPair = !inPair;
             }
 
-            int n = low;
-            int leftSum = 0;
-            int rightSum = 0;
-
-            digits /= 2;
-
-            for (int i = 0; i < digits; ++i) {
-                rightSum += n % 10;
-                n /= 10;
-            }
-
-            for (int i = 0; i < digits; ++i) {
-                leftSum += n % 10;
-                n /= 10;
-            }
-
-            if (leftSum == rightSum) {
+            if (c == '*' && !inPair) {
                 ++ans;
             }
-        }
-
-        return ans;
-    }
-private:
-    char countDigits(int n) {
-        int ans = 0;
-
-        while (n > 0) {
-            ++ans;
-            n /= 10;
         }
 
         return ans;
