ee1d7e8ea8a371c5ccb59b31010cbfd42d22a863
Daniel Pinchuk <512kb@proton.me>
2025-10-27 04:46:17 -0400

    2553. Separate the Digits in an Array

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,21 +6,19 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> diStringMatch(string s) {
-        vector<int> result(s.size() + 1);
+    vector<int> separateDigits(vector<int>& nums) {
+        vector<int> result;
 
-        int decr = count(s.begin(), s.end(), 'D');
-        int incr = decr;
-        result[0] = incr;
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            int n = nums[i];
 
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == 'D') {
-                result[i + 1] = --decr;
-            }
-            else {
-                result[i + 1] = ++incr;
+            while (n) {
+                result.push_back(n % 10);
+                n /= 10;
             }
         }
+
+        reverse(result.begin(), result.end());
 
         return result;
     }
