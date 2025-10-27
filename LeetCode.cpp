8ecd0f0370278ba717f4456c8f1158b0806c2acc
Daniel Pinchuk <512kb@proton.me>
2025-10-27 02:27:13 -0400

    3304. Find the K-th Character in String Game I

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,35 +6,24 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
-        auto iter1 = nums1.begin();
-        auto iter2 = nums2.begin();
+    char kthCharacter(int k) {
+        int iterations = 0;
+        int stringLen = 1;
 
-        vector<vector<int>> result;
+        while (stringLen < k) {
+            stringLen *= 2;
+        }
 
-        while (iter1 != nums1.end() && iter2 != nums2.end()) {
-            if (iter1->front() < iter2->front()) {
-                result.push_back(std::move(*iter1++));
-            }
-            else if (iter1->front() > iter2->front()) {
-                result.push_back(std::move(*iter2++));
-            }
-            else {
-                result.push_back({ iter1->front(), iter1->back() + iter2->back() });
-                ++iter1;
-                ++iter2;
+        while (k > 1) {
+            stringLen /= 2;
+
+            if (k > stringLen) {
+                ++iterations;
+                k -= stringLen;
             }
         }
 
-        while (iter1 != nums1.end()) {
-            result.push_back(std::move(*iter1++));
-        }
-
-        while (iter2 != nums2.end()) {
-            result.push_back(std::move(*iter2++));
-        }
-
-        return result;
+        return iterations % 26 + 'a';
     }
 };
 
