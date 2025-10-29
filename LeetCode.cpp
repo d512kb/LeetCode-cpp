f219f3ac82b6651cbafb33552b1f63eaaa67aae0
Daniel Pinchuk <512kb@proton.me>
2025-10-29 00:43:03 -0400

    3370. Smallest Number With All Set Bits

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,25 +6,11 @@ using namespace std;
 
 class Solution {
 public:
-    int maxNumberOfBalloons(string text) {
-        array<int, 26> charCount{};
-        array<int, 26> targetCharCount{};
-        targetCharCount['b' - 'a'] = 1;
-        targetCharCount['a' - 'a'] = 1;
-        targetCharCount['l' - 'a'] = 2;
-        targetCharCount['o' - 'a'] = 2;
-        targetCharCount['n' - 'a'] = 1;
+    int smallestNumber(int n) {
+        int ans = 1;
 
-        for (char c : text) {
-            ++charCount[c - 'a'];
-        }
-
-        int ans = numeric_limits<int>::max();
-
-        for (int i = 0; i < targetCharCount.size(); ++i) {
-            if (targetCharCount[i] == 0) { continue; }
-
-            ans = min(ans, charCount[i] / targetCharCount[i]);
+        while (ans < n) {
+            ans = (ans << 1) | 1;
         }
 
         return ans;
