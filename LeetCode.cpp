396e213adeaaab0f6dcbc53258587a24c8bece63
Daniel Pinchuk <512kb@proton.me>
2025-11-03 02:51:37 -0500

    414. Third Maximum Number

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,34 +6,17 @@ using namespace std;
 
 class Solution {
 public:
-    int diagonalPrime(vector<vector<int>>& nums) {
-        const int sz = nums.size();
-        int ans = 0;
+    int thirdMax(vector<int>& nums) {
+        set<int> maxes;
 
-        for (int i = 0; i < sz; ++i) {
-            if (isPrime(nums[i][i])) {
-                ans = max(ans, nums[i][i]);
-            }
-
-            if (isPrime(nums[i][sz - i - 1])) {
-                ans = max(ans, nums[i][sz - i - 1]);
+        for (int n : nums) {
+            maxes.insert(n);
+            if (maxes.size() > 3) {
+                maxes.erase(maxes.begin());
             }
         }
 
-        return ans;
-    }
-
-private:
-    bool isPrime(int n) {
-        if (n == 1) { return false; }
-        if (n == 2) { return true; }
-        if (n % 2 == 0) { return false; }
-
-        for (int i = 3; i * i <= n; i += 2) {
-            if (n % i == 0) { return false; }
-        }
-
-        return true;
+        return maxes.size() >= 3 ? *maxes.begin() : *maxes.rbegin();
     }
 };
 
