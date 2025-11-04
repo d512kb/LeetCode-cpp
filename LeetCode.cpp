302a636f490eaf8785415879bcd42a8a37d8c8c3
Daniel Pinchuk <512kb@proton.me>
2025-11-04 02:02:07 -0500

    1784. Check if Binary String Has at Most One Segment of Ones

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,15 +6,11 @@ using namespace std;
 
 class Solution {
 public:
-    bool isBoomerang(vector<vector<int>>& points) {
-        const auto& p1 = points[0];
-        const auto& p2 = points[1];
-        const auto& p3 = points[2];
+    bool checkOnesSegment(string s) {
+        auto iter = find(s.begin(), s.end(), '0');
+        iter = find(iter, s.end(), '1');
 
-        if (p1 == p2 || p1 == p3 || p2 == p3) { return false; }
-
-        // division transformed into multiplication
-        return (p1[1] - p2[1]) * (p2[0] - p3[0]) != (p2[1] - p3[1]) * (p1[0] - p2[0]);
+        return iter == s.end();
     }
 };
 
