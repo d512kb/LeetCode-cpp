724f0b6d57b60a249d141a4ba69b4e18199ce382
Daniel Pinchuk <512kb@proton.me>
2025-11-05 01:42:34 -0500

    1668. Maximum Repeating Substring

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,18 +6,16 @@ using namespace std;
 
 class Solution {
 public:
-    bool checkStraightLine(vector<vector<int>>& coordinates) {
-        for (int i = 2; i < coordinates.size(); ++i) {
-            if (!shareOneLine(coordinates[0], coordinates[1], coordinates[i])) {
-                return false;
-            }
+    int maxRepeating(string sequence, string word) {
+        int ans = 0;
+        string substring = word;
+
+        while (sequence.find(substring) != string::npos) {
+            ++ans;
+            substring += word;
         }
 
-        return true;
-    }
-private:
-    inline bool shareOneLine(const vector<int>& p1, const vector<int>& p2, const vector<int>& p3) {
-        return (p2[1] - p1[1]) * (p3[0] - p2[0]) == (p3[1] - p2[1]) * (p2[0] - p1[0]);
+        return ans;
     }
 };
 
