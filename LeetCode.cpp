251d7fc97a011d7b31c807b9ae6f3f45c3bbe784
Daniel Pinchuk <512kb@proton.me>
2025-11-05 20:48:12 -0500

    1346. Check If N and Its Double Exist

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,29 +6,16 @@ using namespace std;
 
 class Solution {
 public:
-    int buttonWithLongestTime(vector<vector<int>>& events) {
-        map<int, int> pushTimes;
-        int prevTime = 0;
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> nums;
 
-        for (const auto& push : events) {
-            pushTimes[push[0]] = max(pushTimes[push[0]], push[1] - prevTime);
-            prevTime = push[1];
+        for (int n : arr) {
+            if (n % 2 == 0 && nums.contains(n / 2) || nums.contains(n * 2)) { return true; }
+
+            nums.insert(n);
         }
 
-        vector<int> longestPush(2);
-
-        for (const auto& [id, pushTime] : pushTimes) {
-            if (pushTime > longestPush[1]) {
-                longestPush[0] = id;
-                longestPush[1] = pushTime;
-            }
-            else if (pushTime == longestPush[1] && id < longestPush[0]) {
-                longestPush[0] = id;
-                longestPush[1] = pushTime;
-            }
-        }
-
-        return longestPush[0];
+        return false;
     }
 };
 
