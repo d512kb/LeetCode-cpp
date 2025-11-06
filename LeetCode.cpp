e66f90b1e854bf520a751e69e633811d5d060d75
Daniel Pinchuk <512kb@proton.me>
2025-11-05 21:27:53 -0500

    2383. Minimum Hours of Training to Win a Competition

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,18 +6,22 @@ using namespace std;
 
 class Solution {
 public:
-    bool canAliceWin(int n) {
-        bool ans = false;
-        int deduct = 10;
+    int minNumberOfHours(int initialEnergy, int initialExperience, vector<int>& energy, vector<int>& experience) {
+        int neededEnergy = 0;
+        int neededExperience = 0;
 
-        while (n >= deduct) {
-            n -= deduct;
-            --deduct;
+        for (int i = 0; i < energy.size(); ++i) {
+            neededEnergy += energy[i];
 
-            ans = !ans;
+            if (initialExperience <= experience[i]) {
+                neededExperience += experience[i] - initialExperience + 1;
+                initialExperience = experience[i] + 1;;
+            }
+
+            initialExperience += experience[i];
         }
 
-        return ans;
+        return max(0, neededEnergy + 1 - initialEnergy) + neededExperience;
     }
 };
 
