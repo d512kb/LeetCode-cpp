37466ddec2675c1d93556945d74f36f9c4d3197b
Daniel Pinchuk <512kb@proton.me>
2026-03-21 05:19:41 -0400

    1576. Replace All ?'s to Avoid Consecutive Repeating Characters

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,18 +6,20 @@ using namespace std;
 
 class Solution {
 public:
-    bool satisfiesConditions(vector<vector<int>>& grid) {
-        const int rows = grid.size();
-        const int cols = grid.front().size();
+    string modifyString(string s) {
+        const int sz = s.size();
 
-        for (int row = rows - 1; row >= 0; --row) {
-            for (int col = cols - 1; col >= 0; --col) {
-                if (row < rows - 1 && grid[row][col] != grid[row + 1][col]) { return false; }
-                if (col < cols - 1 && grid[row][col] == grid[row][col + 1]) { return false; }
+        for (int i = 0; i < sz; ++i) {
+            if (s[i] == '?') {
+                s[i] = 'a';
+
+                while ((i > 0 && s[i] == s[i - 1]) || (i < sz - 1 && s[i] == s[i + 1])) {
+                    ++s[i];
+                }
             }
         }
 
-        return true;
+        return s;
     }
 };
 
