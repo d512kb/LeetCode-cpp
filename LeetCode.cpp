96f3c22f635731b6ee30388ede810e0adcbb3f54
Daniel Pinchuk <512kb@proton.me>
2026-03-22 23:38:15 -0400

    3637. Trionic Array I

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,20 +6,23 @@ using namespace std;
 
 class Solution {
 public:
-    bool areAlmostEqual(string s1, string s2) {
-        vector<pair<char, char>> swapped;
+    bool isTrionic(vector<int>& nums) {
+        vector<char> peaks;
 
-        for (int i = 0; i < s1.size(); ++i) {
-            if (s1[i] != s2[i]) {
-                swapped.emplace_back(s1[i], s2[i]);
-                if (swapped.size() > 2) { return false; }
+        for (int i = 1; i < nums.size() - 1; ++i) {
+            if (nums[i - 1] == nums[i] || nums[i] == nums[i + 1]) { return false; }
+
+            if (nums[i - 1] < nums[i] && nums[i] > nums[i + 1]) {
+                peaks.push_back('^');
             }
+            else if (nums[i - 1] > nums[i] && nums[i] < nums[i + 1]) {
+                peaks.push_back('v');
+            }
+
+            if (peaks.size() > 2) { return false; }
         }
 
-        if (swapped.empty()) { return true; }
-        if (swapped.size() == 1) { return false; }
-
-        return swapped[0].first == swapped[1].second && swapped[0].second == swapped[1].first;
+        return peaks.size() == 2 && peaks[0] == '^' && peaks[1] == 'v';
     }
 };
 
