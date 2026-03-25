c1006d7b3094e4a94b33a96d67114d9f8fe19374
Daniel Pinchuk <512kb@proton.me>
2026-03-25 15:44:57 -0400

    1417. Reformat The String

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,10 +6,45 @@ using namespace std;
 
 class Solution {
 public:
-    bool isPowerOfFour(int n) {
-        int mask = 0x55555555;
+    string reformat(string s) {
+        vector<char> chars;
+        vector<char> digits;
 
-        return n > 0 && (n & n - 1) == 0 && (n & mask);
+        for (char c : s) {
+            if (isdigit(c)) {
+                digits.push_back(c);
+            }
+            else {
+                chars.push_back(c);
+            }
+        }
+
+        if (abs(static_cast<int>(digits.size()) - static_cast<int>(chars.size())) > 1) { return ""; }
+
+        string ans;
+
+        if (digits.size() > chars.size()) {
+            while (digits.size() > 1) {
+                ans.push_back(digits.back());
+                digits.pop_back();
+                ans.push_back(chars.back());
+                chars.pop_back();
+            }
+            ans.push_back(digits.back());
+        }
+        else {
+            while (!digits.empty()) {
+                ans.push_back(chars.back());
+                chars.pop_back();
+                ans.push_back(digits.back());
+                digits.pop_back();
+            }
+            if (!chars.empty()) {
+                ans.push_back(chars.back());
+            }
+        }
+
+        return ans;
     }
 };
 
