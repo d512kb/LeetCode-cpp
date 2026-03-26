440f23cb12c444c8e8ec74b13a8abb10d4634fdd
Daniel Pinchuk <512kb@proton.me>
2026-03-26 06:07:04 -0400

    2446. Determine if Two Events Have Conflict

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,19 +6,8 @@ using namespace std;
 
 class Solution {
 public:
-    bool canMakeSquare(vector<vector<char>>& grid) {
-        auto check = [&](int row, int col) {
-            int ans = 0;
-
-            if (grid[row][col] == 'W') { ++ans; }
-            if (grid[row][col + 1] == 'W') { ++ans; }
-            if (grid[row + 1][col] == 'W') { ++ans; }
-            if (grid[row + 1][col + 1] == 'W') { ++ans; }
-
-            return ans != 2;
-            };
-
-        return check(0, 0) || check(0, 1) || check(1, 0) || check(1, 1);
+    bool haveConflict(vector<string>& event1, vector<string>& event2) {
+        return (event1[0] <= event2[1]) && (event2[0] <= event1[1]);
     }
 };
 
