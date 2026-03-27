928fdb8a91472c49aa35057259af14178e4a02db
Daniel Pinchuk <512kb@proton.me>
2026-03-27 00:52:10 -0400

    2605. Form Smallest Number From Two Digit Arrays

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -5,44 +5,23 @@
 using namespace std;
 
 class Solution {
-private:
-    class DSet {
-        vector<int> m_parent;
-
-    public:
-        DSet(int n) : m_parent(n, -1) {};
-
-        int findParent(int n) {
-            if (m_parent[n] < 0) { return n; }
-            return m_parent[n] = findParent(m_parent[n]);
-        }
-
-        void join(int a, int b) {
-            int parentA = findParent(a);
-            int parentB = findParent(b);
-
-            if (parentA == parentB) { return; }
-
-            if (m_parent[parentA] < m_parent[parentB]) {
-                m_parent[parentA] += m_parent[parentB];
-                m_parent[parentB] = parentA;
-            }
-            else {
-                m_parent[parentB] += m_parent[parentA];
-                m_parent[parentA] = parentB;
-            }
-        }
-    };
-
 public:
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        DSet dset(n);
+    int minNumber(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
 
-        for (const auto& edge : edges) {
-            dset.join(edge[0], edge[1]);
+        vector<int> intersec;
+
+        set_intersection(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), back_inserter(intersec));
+
+        if (!intersec.empty()) { return intersec.front(); }
+
+        if (nums1.front() < nums2.front()) {
+            return nums1.front() * 10 + nums2.front();
         }
-
-        return dset.findParent(source) == dset.findParent(destination);
+        else {
+            return nums2.front() * 10 + nums1.front();
+        }
     }
 };
 
