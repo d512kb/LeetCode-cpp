f21d93d488df840498a38dbbdaa4e2d79d60cae3
Daniel Pinchuk <512kb@proton.me>
2026-03-28 01:43:53 -0400

    405. Convert a Number to Hexadecimal

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,22 +6,20 @@ using namespace std;
 
 class Solution {
 public:
-    bool strongPasswordCheckerII(string password) {
-        if (password.size() < 8) { return false; }
+    string toHex(int num) {
+        if (num == 0) { return "0"; }
 
-        string specials("!@#$%^&*()-+");
-        bitset<4> flags;
+        constexpr auto codeTable("0123456789abcdef");
+        string result;
+        uint32_t n = num;
 
-        for (int i = 0; i < password.size(); ++i) {
-            if (i > 0 && password[i - 1] == password[i]) { return false; }
-
-            if (islower(password[i])) { flags[0] = 1; }
-            if (isupper(password[i])) { flags[1] = 1; }
-            if (isdigit(password[i])) { flags[2] = 1; }
-            if (specials.contains(password[i])) { flags[3] = 1; }
+        while (n) {
+            result.push_back(codeTable[n & 0b1111]);
+            n >>= 4;
         }
 
-        return flags.all();
+        reverse(result.begin(), result.end());
+        return result;
     }
 };
 
