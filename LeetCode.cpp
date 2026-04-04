42e6d7fef0b8e411903131ea035a20adadc9b6a6
Daniel Pinchuk <512kb@proton.me>
2026-04-04 03:29:42 -0400

    697. Degree of an Array

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,17 +6,38 @@ using namespace std;
 
 class Solution {
 public:
-    int minimumMoves(string s) {
-        int ans = 0;
+    int findShortestSubArray(vector<int>& nums) {
+        struct Number {
+            int counter = 0;
+            int firstPos = 0;
+            int lastPos = 0;
+        };
 
-        for (int i = 0; i < s.size();) {
-            if (s[i] == 'X') {
-                ans += 1;
-                i += 3;
+        int maxDegree = 0;
+        unordered_map<int, Number> numbers;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            auto& numData = numbers[nums[i]];
+            if (numData.counter == 0) { numData.firstPos = i; }
+            numData.lastPos = i;
+
+            if (++numData.counter > maxDegree) {
+                maxDegree = numData.counter;
             }
-            else {
-                ++i;
+        }
+
+        unordered_set<int> maxNumbers;
+
+        for (const auto& [num, numData] : numbers) {
+            if (numData.counter == maxDegree) {
+                maxNumbers.insert(num);
             }
+        }
+
+        int ans = nums.size();
+
+        for (int n : maxNumbers) {
+            ans = min(ans, 1 + numbers[n].lastPos - numbers[n].firstPos);
         }
 
         return ans;
