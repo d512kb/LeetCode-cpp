4b46fbffea268b9e1bbb9d211a81812a765f5f4e
Daniel Pinchuk <512kb@proton.me>
2026-04-04 22:28:52 -0400

    292. Nim Game

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,21 +6,8 @@ using namespace std;
 
 class Solution {
 public:
-    bool isCousins(TreeNode* root, int x, int y) {
-        auto heightX = calcHeight(root, nullptr, x, 0);
-        auto heightY = calcHeight(root, nullptr, y, 0);
-
-        return heightX.first != heightY.first && heightX.second == heightY.second;
-    }
-private:
-    pair<TreeNode*, int> calcHeight(TreeNode* node, TreeNode* parent, int v, int h) {
-        if (!node) { return {}; }
-        if (node->val == v) { return { parent, h }; }
-
-        auto heightLeft = calcHeight(node->left, node, v, h + 1);
-        if (heightLeft.first) { return heightLeft; }
-
-        return calcHeight(node->right, node, v, h + 1);
+    bool canWinNim(int n) {
+        return n % 4;
     }
 };
 
