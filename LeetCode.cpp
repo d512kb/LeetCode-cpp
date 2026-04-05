adbde6a59aac53c2233ea6eee59ba281c253c08c
Daniel Pinchuk <512kb@proton.me>
2026-04-04 23:30:46 -0400

    2600. K Items With the Maximum Sum

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,27 +6,12 @@ using namespace std;
 
 class Solution {
 public:
-    int fillCups(vector<int>& amount) {
-        priority_queue<int> pq(amount.begin(), amount.end());
-        if (pq.top() == 0) { return 0; }
+    int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        int ans = min(k, numOnes);
+        k -= numOnes;
 
-        int ans = 0;
-
-        while (pq.size() > 1) {
-            int a = pq.top();
-            pq.pop();
-            int b = pq.top();
-            pq.pop();
-
-            ++ans;
-            --a;
-            --b;
-
-            if (a > 0) { pq.push(a); }
-            if (b > 0) { pq.push(b); }
-        }
-
-        if (!pq.empty()) { ans += pq.top(); }
+        if (k > 0) { k -= numZeros; }
+        if (k > 0) { ans -= k; }
 
         return ans;
     }
