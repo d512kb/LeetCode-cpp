3fca527009b63995694d3e96a1b3c40a64d662a7
Daniel Pinchuk <512kb@proton.me>
2026-04-08 01:31:44 -0400

    2451. Odd String Difference

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,9 +6,28 @@ using namespace std;
 
 class Solution {
 public:
-    int distinctIntegers(int n) {
-        if (n == 1) { return 1; }
-        return n - 1;
+    string oddString(vector<string>& words) {
+        const int wordSz = words.front().size();
+
+        sort(words.begin(), words.end(), [wordSz](const auto& word1, const auto& word2) {
+            for (int i = 1; i < wordSz; ++i) {
+                int a = word1[i] - word1[i - 1];
+                int b = word2[i] - word2[i - 1];
+
+                if (a != b) { return a < b; }
+            }
+
+            return false;
+        });
+
+        for (int i = 1; i < wordSz; ++i) {
+            int a = words[0][i] - words[0][i - 1];
+            int b = words[1][i] - words[1][i - 1];
+
+            if (a != b) { return words.front(); }
+        }
+
+        return words.back();
     }
 };
 
