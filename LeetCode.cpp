19ea8a75d3e9a0f80b6f69bb3848910c305fc387
Daniel Pinchuk <512kb@proton.me>
2026-04-10 23:08:46 -0400

    521. Longest Uncommon Subsequence I

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,26 +6,10 @@ using namespace std;
 
 class Solution {
 public:
-    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
-        int landEarliest = landStartTime[0] + landDuration[0];
-        int waterEarliest = waterStartTime[0] + waterDuration[0];
-        int ans = numeric_limits<int>::max();
+    int findLUSlength(string a, string b) {
+        if (a.size() > b.size()) { return a.size(); }
 
-        for (int i = 0; i < landStartTime.size(); ++i) {
-            landEarliest = min(landEarliest, landStartTime[i] + landDuration[i]);
-        }
-
-        for (int i = 0; i < waterStartTime.size(); ++i) {
-            ans = min(ans, max(landEarliest, waterStartTime[i]) + waterDuration[i]);
-
-            waterEarliest = min(waterEarliest, waterStartTime[i] + waterDuration[i]);
-        }
-
-        for (int i = 0; i < landStartTime.size(); ++i) {
-            ans = min(ans, max(waterEarliest, landStartTime[i]) + landDuration[i]);
-        }
-
-        return ans;
+        return a == b ? -1 : b.size();
     }
 };
 
