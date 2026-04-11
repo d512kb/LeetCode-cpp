d90facdb849088a3c5953328c238368531b21578
Daniel Pinchuk <512kb@proton.me>
2026-04-10 22:59:14 -0400

    3633. Earliest Finish Time for Land and Water Rides I

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,14 +6,26 @@ using namespace std;
 
 class Solution {
 public:
-    string largestEven(string s) {
-        while (!s.empty()) {
-            if (s.back() == '2') { return s; }
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+        int landEarliest = landStartTime[0] + landDuration[0];
+        int waterEarliest = waterStartTime[0] + waterDuration[0];
+        int ans = numeric_limits<int>::max();
 
-            s.pop_back();
+        for (int i = 0; i < landStartTime.size(); ++i) {
+            landEarliest = min(landEarliest, landStartTime[i] + landDuration[i]);
         }
 
-        return s;
+        for (int i = 0; i < waterStartTime.size(); ++i) {
+            ans = min(ans, max(landEarliest, waterStartTime[i]) + waterDuration[i]);
+
+            waterEarliest = min(waterEarliest, waterStartTime[i] + waterDuration[i]);
+        }
+
+        for (int i = 0; i < landStartTime.size(); ++i) {
+            ans = min(ans, max(waterEarliest, landStartTime[i]) + landDuration[i]);
+        }
+
+        return ans;
     }
 };
 
